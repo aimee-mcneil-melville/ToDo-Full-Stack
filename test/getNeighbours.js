e69e9431879785781dfe2ce8754a5e572e51791c
Piet Geursen <pietgeursen@gmail.com>
2015-12-12 17:48:17 +1300

    Improved neighbour tests

--- test/getNeighbours.js
+++ test/getNeighbours.js
@@ -2,9 +2,27 @@ var test = require('tape')
 var getNeighbours = require('../getNeighbours')
 var createBoard = require('../conway.js').createBoard
 
-test('getNeighbours', function (t) {
+test('getNeighbours in corner', function (t) {
   var board = createBoard(10)
-  t.equal(getNeighbours(2, 2, board).length, 8)
-  t.equal(getNeighbours(0, 0, board).length, 3)
+  board[0][1] = 3
+  board[1][0] = 5
+  board[1][1] = 11
+  var neighbours = getNeighbours(0, 0, board)
+  t.equal(neighbours.reduce(function (sum, val) { return sum + val }, 0), 19)
+  t.end()
+})
+test('getNeighbours in middle', function (t) {
+  var board = createBoard(10)
+  var total = 0
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (i === 1 && j === 1) continue
+      var rando = Math.random()
+      total += rando
+      board[i][j] = rando
+    }
+  }
+  var neighbours = getNeighbours(1, 1, board)
+  t.equal(neighbours.reduce(function (sum, val) { return sum + val }, 0), total)
   t.end()
 })
