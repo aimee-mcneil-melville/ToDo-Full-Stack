38714f61b03d39b2e19e680c965d74a8016973a6
mix irving <whimful@gmail.com>
2016-02-22 21:29:43 +1300

    make asserts more readable

--- main.js
+++ main.js
@@ -70,22 +70,24 @@ function splitStringByCommas (str) {
 }
 
 
-// TESTS 
+// TESTS
 
 var meaningOfLife = '42'
-var expectedType = 'string'
 
-assert(getType(meaningOfLife), expectedType, 'meaningOfLife is a ' + expectedType + ' data type')
-assert(getType(data), 'object', 'data is an object!?')
-assert(isNumber(meaningOfLife), false, 'meaningOfLife is not a number datatype')
-assert(isStringNumber(meaningOfLife), true, 'we can convert meaningOfLife to number')
-assert(isStringNumber('jsksk'), false, 'isStringNumber does not give a false positive')
-assert(toNumber(meaningOfLife), 42, 'toNumber can convert strings to number if possible') 
-assert(add(2, 3), 5, 'add() can add')
-assert(addStrings(meaningOfLife, '10'), '52', 'addStrings can add strings and convert them back to a string')
-assert(addStringsOrNumbers(2, 3), 5, 'addStringsOrNumbers can add numbers')
-assert(addStringsOrNumbers('1', '2'), '3', 'addStringsOrNumbers can add numbers')
-assert(addStringsOrNumbers('10', 10), '20', 'addStringsOrNumbers can add strings and numbers (returning a string)')
+assert( getType(meaningOfLife), 'string', 'meaningOfLife is a string data type' )
+assert( getType(data), 'object', 'data is an object!?' )
+
+assert( isNumber(meaningOfLife), false, 'meaningOfLife is not a number datatype' )
+assert( isStringNumber(meaningOfLife), true, 'we can convert meaningOfLife to number' )
+assert( isStringNumber('jsksk'), false, 'isStringNumber does not give a false positive' )
+assert( toNumber(meaningOfLife), 42, 'toNumber can convert strings to number if possible' )
+
+assert( add(2, 3), 5, 'add() can add' )
+assert( addStrings(meaningOfLife, '10'), '52', 'addStrings can add strings and convert them back to a string' )
+assert( addStringsOrNumbers(2, 3), 5, 'addStringsOrNumbers can add numbers' )
+assert( addStringsOrNumbers('1', '2'), '3', 'addStringsOrNumbers can add numbers' )
+assert( addStringsOrNumbers('10', 10), '20', 'addStringsOrNumbers can add strings and numbers (returning a string)' )
+
 
 var mixedArray = [1, '21', null, Date.now(), 5, meaningOfLife, 42]
 var expectedNumberCount = 4 // why are there 4 expected number data-types?  What are they?
@@ -93,23 +95,23 @@ var expectedStringCount = 2
 var numberCount = countIf(isNumber, mixedArray)
 var stringCount = countIf(function (x) { return typeof x === 'string' }, mixedArray)
 
-assert(numberCount, expectedNumberCount, 'countIf can count the numbers in an array' )
-assert(stringCount, expectedStringCount, 'countIf can count the strings in an array')
+assert( numberCount, expectedNumberCount, 'countIf can count the numbers in an array' )
+assert( stringCount, expectedStringCount, 'countIf can count the strings in an array' )
 
 var anISO8601String = "2015-11-15 04:30:11 +1300"
-assert(isEmail('thedonald@makeamericagreatagain.com'), true, 'isEmail detects an email')
-assert(isEmail('3333@'), false, 'isEmail does not give a false positive')
-assert(isDate(anISO8601String), true, 'isDate identifies ISO8601 Date and Time')
-assert(isDate('200E-ii:iE'), false, 'isDate does not give a false positive')
-assert(formatDate(anISO8601String), "15/11/2015", 'formatDate converts a date to dd/mm/yyyy format')
+assert( isEmail('thedonald@makeamericagreatagain.com'), true, 'isEmail detects an email' )
+assert( isEmail('3333@'), false, 'isEmail does not give a false positive' )
+assert( isDate(anISO8601String), true, 'isDate identifies ISO8601 Date and Time' )
+assert( isDate('200E-ii:iE'), false, 'isDate does not give a false positive' )
+assert( formatDate(anISO8601String), "15/11/2015", 'formatDate converts a date to dd/mm/yyyy format' )
 var emails = filter(isEmail, data)
-assert(emails.length, 44, 'filter and isEmail returns the correct number of emails' )
+assert( emails.length, 44, 'filter and isEmail returns the correct number of emails' )
 
 var stringsWithCommas = filter(filterStringsWithCommas, data)
-assert(stringsWithCommas.length, 62, 'filter and filterStringsWithCommas returns the correct number of commas')
+assert( stringsWithCommas.length, 62, 'filter and filterStringsWithCommas returns the correct number of commas' )
 
 var dates = filter(isDate, data)
-assert(dates.length, 51, 'filter and isDate return the correct number of dates from data')
+assert( dates.length, 51, 'filter and isDate return the correct number of dates from data' )
 
 var formattedDates = map(formatDate, dates)
 var matchesFormattedDates = formattedDates.every(function (d, i) {
@@ -117,7 +119,7 @@ var matchesFormattedDates = formattedDates.every(function (d, i) {
   return d === expectedFormattedDates[i]
 })
 
-assert(matchesFormattedDates, true, 'formatDate and map correctly format dates') 
+assert( matchesFormattedDates, true, 'formatDate and map correctly format dates' )
 
 var arrayOfArrays = map(splitStringByCommas, stringsWithCommas)
 var matchesArrayOfArrays = arrayOfArrays.every(function (arr, i) {
@@ -126,8 +128,6 @@ var matchesArrayOfArrays = arrayOfArrays.every(function (arr, i) {
   })
 })
 
-assert(matchesArrayOfArrays, true, 'the generated array of array of strings matches the expected array') 
-
-
+assert( matchesArrayOfArrays, true, 'the generated array of array of strings matches the expected array' )
 
 
