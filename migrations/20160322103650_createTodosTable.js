89812dd5fb68f795cce7e0f09639a0dba79d6255
Piet Geursen <pietgeursen@gmail.com>
2016-03-22 11:38:49 +1300

    It works

---
+++ migrations/20160322103650_createTodosTable.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  console.log('create table')
+
+  return knex.schema.createTableIfNotExists('todos', function(table) {
+    table.increments('id')
+      table.string('task')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('todos').then(function () {
+    console.log('todos table was dropped')
+  })
+};
