f5f659fa97ea33094e8b78cbe320db50007e0d97
Don Smith <locksmithdon@gmail.com>
2016-04-20 00:02:56 +1200

    An ES5 Node.js implementation

---
+++ game.js
@@ -0,0 +1,53 @@
+module.exports = {
+  scoreFrame: scoreFrame,
+  scoreGame: scoreGame
+}
+
+function scoreFrame (frame, nextFrame, anotherFrame, isTenth) {
+  var score = frame[0] + frame[1]
+  if (isTenth) {
+    return calculateTenth(frame)
+  }
+  if (isStrike(frame)) {
+    score = scoreStrikes(frame, nextFrame, anotherFrame)
+  } else if (isSpare(frame)) {
+    score += nextFrame[0]
+  }
+  return score
+}
+
+function scoreGame (frames) {
+  var score = 0
+  var isTenth = false
+  for (var i = 0; i < frames.length; i++) {
+    isTenth = i === frames.length - 1
+    score += this.scoreFrame(frames[i], frames[i + 1], frames[i + 2], isTenth)
+  }
+  return score
+}
+
+function calculateTenth (frame) {
+  var score = frame[0] + frame[1]
+  if (isStrike(frame) || isSpare(frame)) {
+    score += frame[2]
+  }
+  return score
+}
+
+function isStrike (frame) {
+  return frame[0] === 10
+}
+
+function isSpare (frame) {
+  return frame[0] + frame[1] === 10
+}
+
+function scoreStrikes (frame, nextFrame, anotherFrame) {
+  var score = frame[0]
+  if (nextFrame[0] === 10) {
+    score = score + nextFrame[0] + anotherFrame[0]
+  } else {
+    score = score + nextFrame[0] + nextFrame[1]
+  }
+  return score
+}
--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,73 @@
 var test = require('tape')
+var game = require('../game')
 
-test('initial', function (t) {
+test('test setup working', function (t) {
   t.plan(1)
   t.equal('a', 'a')
+})
+
+test('scores a gutterball frame', function (t) {
+  var frame = [0, 0]
+  var score = game.scoreFrame(frame)
+  t.equals(score, 0)
+  t.end()
+})
+
+test('scores a normal frame', function (t) {
+  var frame = [2, 3]
+  var score = game.scoreFrame(frame)
+  t.equals(score, 5)
+  t.end()
+})
+
+test('scores a spare frame', function (t) {
+  var frame = [2, 8]
+  var nextFrame = [6, 3]
+  var score = game.scoreFrame(frame, nextFrame)
+  t.equals(score, 16)
+  t.end()
+})
+
+test('scores a single strike frame', function (t) {
+  var frame = [10]
+  var nextFrame = [6, 3]
+  var score = game.scoreFrame(frame, nextFrame)
+  t.equals(score, 19)
+  t.end()
+})
+
+test('scores a double strike frame', function (t) {
+  var frame = [10]
+  var nextFrame = [10]
+  var anotherFrame = [8, 1]
+  var score = game.scoreFrame(frame, nextFrame, anotherFrame)
+  t.equals(score, 28)
+  t.end()
+})
+
+test('scores a game', function (t) {
+  var frames = [
+    [1, 2], [6, 4], [5, 4], [10], [7, 2], [10], [10], [5, 2], [7, 0], [4, 4]
+  ] // 3      18      27     46     55     80    97     104     111     119
+  var score = game.scoreGame(frames)
+  t.equals(score, 119)
+  t.end()
+})
+
+test('scores a spare in the 10th frame', function (t) {
+  var spareFrames = [
+    [1, 2], [6, 4], [5, 4], [10], [7, 2], [10], [10], [5, 2], [7, 0], [6, 4, 8]
+  ] // 3      18      27     46     55     80    97     104     111     129
+  var spareScore = game.scoreGame(spareFrames)
+  t.equals(spareScore, 129)
+  t.end()
+})
+
+test('scores a strike in the 10th frame', function (t) {
+  var strikeFrames = [
+    [1, 2], [6, 4], [5, 4], [10], [7, 2], [10], [10], [5, 2], [7, 0], [10, 4, 5]
+  ] // 3      18      27     46     55     80    97     104     111      130
+  var strikeScore = game.scoreGame(strikeFrames)
+  t.equals(strikeScore, 130)
+  t.end()
 })
