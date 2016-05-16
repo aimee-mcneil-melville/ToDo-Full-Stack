b262a3faa9ec9128e8991aa5f3151645cac91954
Don Smith <locksmithdon@gmail.com>
2016-05-17 10:52:26 +1200

    Refactoring

--- main.js
+++ main.js
@@ -67,20 +67,19 @@ function splitStringByCommas (str) {
 
 var meaningOfLife = '42'
 
-assert( getType(meaningOfLife), 'string', 'meaningOfLife is a string data type' )
-assert( getType(data), 'object', 'data is an object!?' )
+assert(getType(meaningOfLife), 'string', 'meaningOfLife is a string data type')
+assert(getType(data), 'object', 'data is an object!?')
 
-assert( isNumber(meaningOfLife), false, 'meaningOfLife is not a number datatype' )
-assert( isStringNumber(meaningOfLife), true, 'we can convert meaningOfLife to number' )
-assert( isStringNumber('jsksk'), false, 'isStringNumber does not give a false positive' )
-assert( toNumber(meaningOfLife), 42, 'toNumber can convert strings to number if possible' )
+assert(isNumber(meaningOfLife), false, 'meaningOfLife is not a number datatype')
+assert(isStringNumber(meaningOfLife), true, 'we can convert meaningOfLife to number')
+assert(isStringNumber('jsksk'), false, 'isStringNumber does not give a false positive')
+assert(toNumber(meaningOfLife), 42, 'toNumber can convert strings to number if possible')
 
-assert( add(2, 3), 5, 'add() can add' )
-assert( addStrings(meaningOfLife, '10'), '52', 'addStrings can add strings and convert them back to a string' )
-assert( addStringsOrNumbers(2, 3), 5, 'addStringsOrNumbers can add numbers' )
-assert( addStringsOrNumbers('1', '2'), '3', 'addStringsOrNumbers can add numbers' )
-assert( addStringsOrNumbers('10', 10), '20', 'addStringsOrNumbers can add strings and numbers (returning a string)' )
-
+assert(add(2, 3), 5, 'add() can add')
+assert(addStrings(meaningOfLife, '10'), '52', 'addStrings can add strings and convert them back to a string')
+assert(addStringsOrNumbers(2, 3), 5, 'addStringsOrNumbers can add numbers')
+assert(addStringsOrNumbers('1', '2'), '3', 'addStringsOrNumbers can add numbers')
+assert(addStringsOrNumbers('10', 10), '20', 'addStringsOrNumbers can add strings and numbers (returning a string)')
 
 var mixedArray = [1, '21', null, Date.now(), 5, meaningOfLife, 42]
 var expectedNumberCount = 4 // why are there 4 expected number data-types?  What are they?
@@ -88,17 +87,16 @@ var expectedStringCount = 2
 var numberCount = countIf(isNumber, mixedArray)
 var stringCount = countIf(function (x) { return typeof x === 'string' }, mixedArray)
 
-assert( numberCount, expectedNumberCount, 'countIf can count the numbers in an array' )
-assert( stringCount, expectedStringCount, 'countIf can count the strings in an array' )
+assert(numberCount, expectedNumberCount, 'countIf can count the numbers in an array')
+assert(stringCount, expectedStringCount, 'countIf can count the strings in an array')
 
 assert(isEmail('thedonald@makeamericagreatagain.com'), true, 'isEmail detects an email')
 assert(isEmail('3333@'), false, 'isEmail does not give a false positive')
 var emails = filter(isEmail, data)
-assert( emails.length, 44, 'filter and isEmail returns the correct number of emails' )
+assert(emails.length, 44, 'filter and isEmail returns the correct number of emails')
 
 var stringsWithCommas = filter(filterStringsWithCommas, data)
-assert( stringsWithCommas.length, 62, 'filter and filterStringsWithCommas returns the correct number of commas' )
-
+assert(stringsWithCommas.length, 62, 'filter and filterStringsWithCommas returns the correct number of commas')
 
 var arrayOfArrays = map(splitStringByCommas, stringsWithCommas)
 var matchesArrayOfArrays = arrayOfArrays.every(function (arr, i) {
@@ -107,6 +105,5 @@ var matchesArrayOfArrays = arrayOfArrays.every(function (arr, i) {
   })
 })
 
-assert( matchesArrayOfArrays, true, 'the generated array of array of strings matches the expected array' )
-
+assert(matchesArrayOfArrays, true, 'the generated array of array of strings matches the expected array')
 
--- utils/assert.js
+++ utils/assert.js
@@ -3,13 +3,13 @@ var emoji = require('node-emoji')
 var pretty = require('js-object-pretty-print').pretty
 
 module.exports = function (actual, expected, message) {
+  var greenCheck = colors.green(emoji.get('white_check_mark'))
+  var redCross = colors.red(emoji.get('negative_squared_cross_mark'))
   if (actual === expected) {
-    console.log(colors.green(emoji.get('white_check_mark')), colors.green(message))
+    console.log(greenCheck, colors.green(message))
   } else {
-    console.log(colors.red(emoji.get('negative_squared_cross_mark')), colors.red(message))
-
+    console.log(redCross, colors.red(message))
     console.log('actual: ', pretty(actual))
-    // console.log('...............')
     console.log('expected: ', pretty(expected))
   }
 }
