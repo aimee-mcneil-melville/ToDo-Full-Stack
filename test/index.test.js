6baa8ef9c7315a23182b6243594f69458ae4f230
Rich Churcher <rich.churcher@gmail.com>
2016-07-20 10:24:24 +1200

    Fix some test issues

--- test/index.test.js
+++ test/index.test.js
@@ -21,7 +21,7 @@ function getContacts (asArray) {
   }
 
   if (asArray) {
-    return contacts.keys().map(function (key) {
+    return Object.keys(contacts).map(function (key) {
       var id = { id: key }
       return Object.assign(id, contacts[key])
     })
@@ -33,12 +33,12 @@ test('getValue gets a nested object by key', function (t) {
   var contacts = getContacts()
   var expected = { address: '742 Evergreen Terrace', name: 'Marge Simpson', age: 47 }
   var actual = getValue(contacts, '123')
-  t.is(actual, expected)
+  t.deepEqual(actual, expected)
 })
 
-test('map and getAddress return the address property from objects in an array', function (t) {
+test.only('map and getAddress return the address property from objects in an array', function (t) {
   var contacts = getContacts(true)
-  var expected = [ '742 Evergreen Terrace', 'Bag End', 'Wayne Manor']
+  var expected = [ '742 Evergreen Terrace', 'Bag End', 'Wayne Manor', 'Skull Island', 'Wayne Manor']
   var actual = contacts.map(getAddress)
   t.deepEqual(actual, expected)
 })
