f3193abf9aa473b2db23a2bf9e450906706ded0e
Rich Churcher <rich.churcher@gmail.com>
2016-08-15 08:30:56 +1200

    Remove source files

--- src/components/Pic.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Pic = (props) => <img src={`images/${props.image}`} alt={props.alt} className="dog-img" />
-Pic.propTypes = { image: React.PropTypes.string.isRequired, alt: React.PropTypes.string }
-export default Pic
--- src/components/Subtitle.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const SubTitle = (props) => <h2>{props.text}</h2>
-SubTitle.propTypes = { text: React.PropTypes.string.isRequired }
-export default SubTitle
