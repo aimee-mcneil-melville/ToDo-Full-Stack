16bc637b95bc8df92f612bc4e957f31e328d9c41
Rich Churcher <rich.churcher@gmail.com>
2016-08-30 10:36:35 +1200

    Write a simple map

--- test/index.test.js
+++ test/index.test.js
@@ -66,3 +66,9 @@ test('functions.callsProperty accepts a parameter `obj` and calls obj.increment(
   functions.callsProperty(actual)
   t.is(actual.n, expected)
 })
+
+test.only('functions.map takes an array as the first parameter, a function as the second parameter, and returns the result of calling the function on each array element', function (t) {
+  var expected = [1, 2, 3]
+  var actual = functions.map([0, 1, 2], function (n) { return ++n })
+  t.deepEqual(actual, expected)
+})
