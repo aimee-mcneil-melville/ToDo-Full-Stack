4ff0c59e8220c30fc5a767d5b8b5e7d1037bddd6
Piet Geursen <pietgeursen@gmail.com>
2016-10-20 21:39:30 +1300

    Improve some tests.

--- test/nextCellState.js
+++ test/nextCellState.js
@@ -5,6 +5,7 @@ test('nextCellState', function (t) {
   t.true(nextCellState(true, 2), 'returns true if cell is alive and neighbour count is 2')
   t.true(nextCellState(true, 3), 'returns true if cell is alive and neighbour count is 3')
   t.false(nextCellState(true, 4), 'should return false if cell is alive and overpopulated')
+  t.false(nextCellState(true, 1), 'should return false if cell is alive and underpopulated')
   t.true(nextCellState(false, 3), 'should return true id cell is dead and ressurectable')
   t.end()
 })
