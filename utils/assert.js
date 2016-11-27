589b6b125f8acc5eb4c86e17f0779297479af26a
Don Smith <locksmithdon@gmail.com>
2016-11-27 16:50:11 +1300

    remove comment

--- utils/assert.js
+++ utils/assert.js
@@ -1,17 +1,15 @@
 var test = require('tape')
-//var htest = test.createHarness()
 var tapSpec = require('tap-spec')
 
 test.createStream()
   .pipe(tapSpec())
   .pipe(process.stdout);
 
-function assert( expect, actual, msg ) {
-  test( msg || "please add a message",  function (t) {
+function assert(expect, actual, msg) {
+  test(msg || "please add a message",  function (t) {
     t.equal(expect, actual)
     t.end()
   })
 }
 
 module.exports = assert
-
