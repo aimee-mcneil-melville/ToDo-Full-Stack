bc0e868fcac3ae285f6355b12aa5f7ccd4e9aacb
mix irving <whimful@gmail.com>
2016-11-29 21:29:36 +1300

    space out tests + add watch case

--- test/index.test.js
+++ test/index.test.js
@@ -36,6 +36,8 @@ test('getType returns the data type of its argument', function (t) {
   var expected = 'number'
   var actual = getType(123)
   t.is(actual, expected)
+
+  t.is(getType('123'), 'string')
 })
 
 test('getType returns the expected data types', function (t) {
@@ -66,13 +68,18 @@ test('getFirst & getLast', function (t) {
 })
 
 test('map and getAddress return the address property from objects in an array', function (t) {
+  // arrange
   var contacts = getContacts()
   var expected = [ '742 Evergreen Terrace', 'Bag End', 'Wayne Manor', 'Skull Island', 'Wayne Manor']
+
+  // act
   var actual = contacts.map(getAddress)
+
+  // assert
   t.deepEqual(actual, expected)
 })
 
-test('creating and updating matrices', function (t) {
+test('creating matrices of different sizes', function (t) {
   var expected = [
     [ 0, 0, 0 ],
     [ 0, 0, 0 ],
@@ -80,9 +87,19 @@ test('creating and updating matrices', function (t) {
   ]
   var actual = matrix.getMatrix(3)
   t.deepEqual(actual, expected)
+
+  var expectedBigger = [
+    [ 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0 ]
+  ]
+  var actualBigger = matrix.getMatrix(4)
+  t.deepEqual(actualBigger, expectedBigger)
 })
 
 test('updateMatrix can change the value at specified coordinates', function (t) {
+  // arrange
   var initialMatrix = [
     [ 0, 0, 0 ],
     [ 0, 0, 0 ],
@@ -93,41 +110,56 @@ test('updateMatrix can change the value at specified coordinates', function (t) 
     [ 0, 0, 1 ],
     [ 0, 0, 0 ]
   ]
+
+  // act
   var actual = matrix.updateMatrix(initialMatrix, [1, 2], 1)
+
+  // assert
   t.deepEqual(actual, expected)
 })
 
 test('find finds the first matching object in the array', function (t) {
   var contacts = getContacts()
   var expected = { id: '125', address: 'Wayne Manor', name: 'Bruce Wayne', age: 43 }
+
   var actual = find(contacts, { address: 'Wayne Manor' })
+
   t.deepEqual(actual, expected)
 })
 
 test('where finds an object by id in an array', function (t) {
   var contacts = getContacts()
   var expected = [{ id: '123', address: '742 Evergreen Terrace', name: 'Marge Simpson', age: 47 }]
+
   var actual = where(contacts, { id: '123' })
+
   t.deepEqual(actual, expected)
 })
 
 test('where finds an object by property', function (t) {
   var contacts = getContacts()
   var expected = [{ id: '126', address: 'Skull Island', name: 'Dr Evil', age: 51 }]
+
   var actual = where(contacts, { address: 'Skull Island' })
+
   t.deepEqual(actual, expected)
 })
 
 test('where returns multile correct results', function (t) {
   var contacts = getContacts()
   var expected = 2
+
   var actual = where(contacts, { age: 78 }).length
+
   t.is(actual, expected)
 })
 
 test('where finds objects with two search properties', function (t) {
   var contacts = getContacts()
   var expected = [{ id: '127', address: 'Wayne Manor', name: 'Alfred', age: 78 }]
+
   var actual = where(contacts, { age: 78, address: 'Wayne Manor' })
+
   t.deepEqual(actual, expected)
 })
+
