8fad74498ab4a0de6be9b57db227d06abc8f09e1
mix irving <whimful@gmail.com>
2017-02-23 15:46:15 +1300

    respond with a file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express')
+var path = require('path')
 
 var app = express()
 
 app.get('/compliment', function(req, res) {
   res.send("<h1>you are a beautiful human</h1>")
+})
+
+app.get('/profile', function(req, res) {
+  // http://expressjs.com/en/api.html#res.sendFile
+
+  var filePath = path.join(__dirname, 'public/silvia.html')
+  res.sendFile(filePath)
 })
 
 module.exports = app
