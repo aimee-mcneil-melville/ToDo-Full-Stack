2464cc0cf6e0acbe8c2d36e41b330d5fe19d38ae
Rich Churcher <rich.churcher@gmail.com>
2017-03-03 11:50:26 +1300

    Load and display files

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-function mainMenu () {
+var fs = require('fs')
+var prompt = require('prompt')
+var directory = require('./directory.json')
+
+prompt.message = ''
+prompt.delimiter = ' > '
+prompt.start()
+
+function mainMenu (cb) {
   console.log(
     ' Welcome!\n',
     '--------\n\n',
 
-    'Choose an artwork to display:\n',
-    '1. Kea\n',
-    '2. Kiwi\n',
-    '3. Kowhai\n\n'
+    'Choose an artwork to display:'
   )
+  var list = directory.artworks.reduce(function (list, artwork, i) {
+    return list + ' ' + i + ': ' + artwork + '\n'
+  }, '')
+
+  var choice = {
+    name: 'choice',
+    hidden: true,
+    message: 'Choice'
+  }
+  console.log(list)
+
+  prompt.get(choice, cb)
 }
 
-mainMenu()
+function loadArt(err, result) {
+  if (err) {
+    console.error("I don't understand that.")
+    return
+  }
+  fs.readFile(directory.artworks[result.choice], 'utf8', function (err, artwork) {
+    if (err) {
+      console.error("Can't load that file.")
+      return
+    }
+    console.log(artwork)
+  })
+}
+
+mainMenu(loadArt)
+
