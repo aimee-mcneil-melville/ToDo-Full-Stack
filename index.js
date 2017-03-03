e21f7db9461b835895b617e49e6252ab5a77c4b3
Rich Churcher <rich.churcher@gmail.com>
2017-03-03 14:09:05 +1300

    Clean up, allow exit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var fs = require('fs')
 var prompt = require('prompt')
-var readline = require('readline')
 
 var directory = require('./directory.json')
 
@@ -9,16 +8,10 @@ prompt.delimiter = ' > '
 prompt.start()
 
 function mainMenu (next) {
-  console.log(
-    ' Welcome!\n',
-    '--------\n\n',
-
-    'Choose an artwork to display:\n'
-  )
+  console.log(' Choose an artwork to display (or `q` to quit):\n')
   var list = directory.artworks.reduce(function (list, artwork, i) {
     return list + '  ' + i + ': ' + artwork + '\n'
   }, '')
-
   console.log(list)
 
   var choice = {
@@ -30,13 +23,7 @@ function mainMenu (next) {
 }
 
 function pressEnter () {
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  rl.question('Hit <enter> to continue...', function () {
-    rl.close()
+  prompt.get('Hit <enter> to continue...', function () {
     mainMenu(loadArt)
   })
 }
@@ -44,24 +31,31 @@ function pressEnter () {
 function loadArt(err, result) {
   if (err) {
     console.error("I don't understand that.")
-    return
+    return pressEnter()
   }
+  if (result.choice === 'q') {
+    process.exit()
+  }
+
   var file = directory.artworks[result.choice]
   if (!file) {
     console.error("That's not one of the artworks!")
-    pressEnter()
+    return pressEnter()
   } else {
     fs.readFile(file, 'utf8', function (err, artwork) {
       if (err) {
         console.error("Can't load that file.")
-        pressEnter()
-        return
+        return pressEnter()
       }
       console.log(artwork)
-      pressEnter()
+      return pressEnter()
     })
   }
 }
 
+console.log(
+  ' Welcome!\n',
+  '--------\n'
+)
 mainMenu(loadArt)
 
