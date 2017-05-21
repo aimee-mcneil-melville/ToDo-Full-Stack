8ca893b812372e2e6a2a6254e88e81ea535b4db8
Tony Luisi <t.luisi@gmail.com>
2017-05-21 17:49:18 +1200

    removed solution

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -6,18 +6,4 @@ const bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
 
-// router.put('/:id', (req, res) => {
-//   db.updateCommentById(req.params.id, { comment: req.body.comment })
-//     .then(updatedComment => {
-//       res.json(updatedComment)
-//     })
-// })
-// 
-// router.delete('/:id', (req, res) => {
-//   db.deleteCommentById(req.params.id)
-//     .then(() => {
-//       res.json({})
-//     })
-// })
-
 module.exports = router
