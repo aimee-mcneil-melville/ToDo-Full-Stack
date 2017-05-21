ce3e1bea82f392e69e9df6c9b16f4f788f600dea
Tony Luisi <t.luisi@gmail.com>
2017-05-22 08:19:58 +1200

    updated migration to remove comment count from posts

--- migrations/20170515163102_posts.js
+++ migrations/20170515163102_posts.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
     table.increments().primary()
     table.string('title')
     table.date('date_created')
-    table.integer('comment_count')
     table.string('paragraphs')
   })
 }
