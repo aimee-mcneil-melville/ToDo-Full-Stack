87e28382093839b11799c34ec94fa10add670555
Don Smith <don@antiantonym.com>
2017-11-28 01:06:37 +1300

    fix description and remove unneeded code

--- tests/functions/callsFunction.test.js
+++ tests/functions/callsFunction.test.js
@@ -2,13 +2,13 @@ var functions = require('../../functions')
 
 // If you find this test confusing, don't worry! You don't need to understand it right now.
 // If you're curious, after class read up on call, apply, bind, and the `this` keyword.
-test('functions.callsTheFunction takes a function as a parameter and calls it once', function () {
+test('functions.callsFunction takes a function as a parameter and calls it once', function () {
   var increment = function () {
     actual++
   }
   var expected = 1
   var actual = 0
-  functions.callsFunction(increment.bind(this))
+  functions.callsFunction(increment)
   expect(actual).toBe(expected)
 })
 
