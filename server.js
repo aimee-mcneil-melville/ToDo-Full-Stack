35d5df6ce3e44df4989aee8ac8f0884081e9a0ec
Don Smith <don@antiantonym.com>
2018-03-26 11:45:36 +1300

    remove body-parser

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const users = require('./routes/users')
 
 const server = express()
 
 // Middleware
-server.use(bodyParser.json())
+server.use(express.json())
 
 // Routes
 server.use('/users', users)
 
 module.exports = server
-
