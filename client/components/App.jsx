3de88c1ff6c16c97ce9f88a9fb72380c22f0a024
Emily Parkes <emilycoco@me.com>
2018-04-12 15:03:59 +1200

    solution with css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,34 +2,45 @@ import React from 'react'
 
 import Card from './Card'
 
-const cards = [
-  { id: 1, value: 1, isVisable: false },
-  { id: 2, value: 2, isVisable: false },
-  { id: 3, value: 2, isVisable: false },
-  { id: 4, value: 3, isVisable: false },
-  { id: 5, value: 1, isVisable: false },
-  { id: 6, value: 3, isVisable: false },
-]
+function startingCards() {
+  return [
+    { id: 1, value: 1, isVisable: false },
+    { id: 2, value: 2, isVisable: false },
+    { id: 3, value: 2, isVisable: false },
+    { id: 4, value: 3, isVisable: false },
+    { id: 5, value: 1, isVisable: false },
+    { id: 6, value: 3, isVisable: false },
+  ]
+}
+
+const win = 'Congratulations you matched all the cards!'
+const tryAgain = 'Try match again'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      cards: cards,
+      cards: startingCards(),
       card1: null,
-      card2: null
+      card2: null,
+      matches: 0,
+      allMatched: false,
+      mismatch: false,
+      replay: false
     }
     this.onClick = this.onClick.bind(this)
     this.clear = this.clear.bind(this)
     this.checkMatch = this.checkMatch.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
   onClick(id) {
+    const cards = this.state.cards
     const card1 = this.state.card1
     const card2 = this.state.card2
+    const newCards = [...cards]
+    const cardIndex = newCards.findIndex((card) => card.id === id)
     if (card1 === null) {
-      const newCards = [...this.state.cards]
-      const cardIndex = newCards.findIndex((card) => card.id === id)
       newCards[cardIndex].isVisable = true
       this.setState({
         card1: id,
@@ -37,8 +48,6 @@ class App extends React.Component {
       })
     }
     if ((card1 !== null) && (card2 === null)) {
-      const newCards = [...this.state.cards]
-      const cardIndex = newCards.findIndex((card) => card.id === id)
       newCards[cardIndex].isVisable = true
       this.setState({
         card2: id,
@@ -50,49 +59,86 @@ class App extends React.Component {
   }
 
   checkMatch() {
-    const card1 = this.state.cards.find(card => card.id === this.state.card1)
-    const card2 = this.state.cards.find(card => card.id === this.state.card2)
+    const cards = this.state.cards
+    const matches = this.state.matches
+    const card1 = cards.find(card => card.id === this.state.card1)
+    const card2 = cards.find(card => card.id === this.state.card2)
     if (card1.value !== card2.value) {
-
       setTimeout(() => {
-        console.log('reset')
-        const newCards = [...this.state.cards]
-        const cardIndex1 = newCards.findIndex((card) => card.id === card1.id)
-        const cardIndex2 = newCards.findIndex((card) => card.id === card2.id)
-        newCards[cardIndex1].isVisable = false
-        newCards[cardIndex2].isVisable = false
+        const newCards = [...cards]
+        const card1Index = newCards.findIndex((card) => card.id === card1.id)
+        const card2Index = newCards.findIndex((card) => card.id === card2.id)
+        newCards[card1Index].isVisable = false
+        newCards[card2Index].isVisable = false
         this.setState({
           cards: newCards,
           card1: null,
-          card2: null
+          card2: null,
+          mismatch: true
         })
-      }, 5000)
+      }, 1000)
     } else {
-      this.clear()
+      this.setState({
+        matches: matches + 1,
+        mismatch: false
+      }, () => {
+        this.clear()
+      })
     }
-
-
   }
 
   clear() {
+    const matches = this.state.matches
+    if (matches === 3) {
+      this.setState({
+        allMatched: true,
+        replay: true
+      })
+    } else {
+      this.setState({
+        card1: null,
+        card2: null
+      })
+    }
+  }
+
+  reset() {
     this.setState({
+      cards: startingCards(),
       card1: null,
-      card2: null
+      card2: null,
+      matches: 0,
+      allMatched: false,
+      mismatch: false,
+      replay: false
     })
   }
 
   render() {
     return (
-      <div className='memorygame'>
-        {this.state.cards.map((card) => {
-          return <Card
-            key={card.id}
-            id={card.id}
-            value={card.value}
-            isVisable={card.isVisable}
-            onClick={this.onClick}
-            checkMatch={this.checkMatch} />
-        })}
+      <div className='game'>
+      <h1>Welcome to the Memory Game!</h1>
+      <h2>Match all the cards to win.</h2>
+        
+        <div className='cards'>
+          {this.state.cards.map((card) => {
+            return <Card
+              key={card.id}
+              id={card.id}
+              value={card.value}
+              isVisable={card.isVisable}
+              onClick={this.onClick}
+              checkMatch={this.checkMatch} />
+          })}
+        </div>
+        <h5>{this.state.allMatched ? win : ' '}</h5>
+        <h5>{this.state.mismatch ? tryAgain : ' '}</h5>
+
+        <div className='replaybutton'>
+        {this.state.replay ?
+          <button onClick={this.reset}>Play Again</button>
+          : ' '}
+        </div>
       </div>
     )
   }
--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -4,28 +4,11 @@ class Card extends React.Component {
   constructor(props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
-  //   this.checkMatch = this.checkMatch.bind(this)
   }
 
   handleClick () {
     this.props.onClick(this.props.id)
   }
-
-  // checkMatch(match, notmatch) {
-  //   if (match)
-  //     this.setState({
-  //       isVisible: true
-  //     }), () => {
-  //       this.props.clear()
-  //     }
-  //   if (notmatch)
-  //     this.setState({
-  //       isVisible: false
-  //     }, () => {
-  //       this.props.clear()
-  //     })
-  // }
-
 
   render() {
     return (
