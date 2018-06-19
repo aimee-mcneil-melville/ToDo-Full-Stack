8d49be9aceffe6439aae35e33b7657c06af10bae
Emily Parkes <30305644+emilyparkes@users.noreply.github.com>
2018-06-19 17:35:13 +1200

    Delete how-to-play.js

--- how-to-play.js
+++
@@ -1,8 +0,0 @@
-// How to play:
-
-// 1. cards have a value.
-// 2. click to see value.
-// 3. click other card to see value.
-// 4. if values match cards stay visible.
-// 5. if cards dont match they become invisible again.
-// 6. match all cards to finish.
