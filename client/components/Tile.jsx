8df61a49c60fc13c30b5f81ba3c7dec5d0adcaff
Rich Churcher <rich.churcher@gmail.com>
2018-09-17 15:44:15 +1200

    Use web font to ensure glyphs are available

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Tile = props => {
-  // convert the unicode hexadecimal value of the emoji into a string
-  const tileValue = String.fromCodePoint(parseInt(props.value, 16))
   return (
     <div className='tile'>
-      {tileValue}
+      {props.value}
     </div>
   )
 }
