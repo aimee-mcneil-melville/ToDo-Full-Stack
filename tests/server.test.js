0593e4b81494d1c9fa346f5c0ca743a4c9e8fe44
Rich Churcher <rich.churcher@gmail.com>
2018-10-19 18:21:23 +1300

    Upgrade versions, use done in callbacks

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,18 +1,15 @@
-//needed to fix encoding problem with jest - https://stackoverflow.com/questions/49141927/express-body-parser-utf-8-error-in-test?rq=1
-require('../node_modules/iconv-lite/encodings')
-
 const supertest = require('supertest')
 const cheerio = require('cheerio')
 
 const app = require('../server')
 
-test('profile 1 should be Silvia', function () {
+test('profile 1 should be Silvia', function (done) {
   supertest(app)
     .get('/profiles/1')
     .end(checkProfileIsSilvia)
 
   function checkProfileIsSilvia (err, res) {
-    if (err) throw err
+    expect(err).toBe(null)
 
     const $ = cheerio.load(res.text)
     const actual = $('h1').text()
@@ -20,15 +17,15 @@ test('profile 1 should be Silvia', function () {
     const expected = 'Silvia'
 
     expect(actual).toBe(expected)
+    done()
   }
 })
 
-
-test('profile 2 should be Sampson', function () {
+test('profile 2 should be Sampson', function (done) {
   supertest(app)
     .get('/profiles/2')
     .end(function (err, res) {
-      if (err) throw err
+      expect(err).toBe(null)
 
       const $ = cheerio.load(res.text)
       const actual = $('h1').text()
@@ -36,18 +33,20 @@ test('profile 2 should be Sampson', function () {
       const expected = 'Sampson'
 
       expect(actual).toBe(expected)
+      done()
     })
 })
 
-test('post function works', function () {
+test('post function works', function (done) {
   supertest(app)
     .post('/named-compliment')
-    .send({"name": 'alice'})
+    .send({ name: 'alice' })
     .type('form')
     .end((err, res) => {
-      if (err) console.log(err)
+      expect(err).toBe(null)
       const actual = res.text
-      const expected = "You are wonderful alice"
+      const expected = 'You are wonderful alice'
       expect(actual).toBe(expected)
+      done()
     })
 })
