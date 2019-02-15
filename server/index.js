3407c12e9d580c4a455d394f3ee843cc4dcb93c5
Ross Jourdain <ross.jourdain@gmail.com>
2019-02-15 20:52:24 +1300

    Remove broken app.get and app.set strategy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
 require('dotenv').config()
 
-const Knex = require('knex')
-const config = require('../knexfile')[process.env.NODE_ENV || 'development']
-const knex = Knex(config)
-
 const server = require('./server')
-server.set('db', knex)
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
