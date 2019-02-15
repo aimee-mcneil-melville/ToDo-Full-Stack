9327ac05e47bb391b51adcc4befc0827077850ce
Ross Jourdain <ross.jourdain@gmail.com>
2019-02-15 21:10:37 +1300

    Clean up auth process

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,14 +10,13 @@ function register (req, res, next) {
   userExists(user_name)
     .then(exists => {
       if (exists) return res.status(400).send({message: "User Name Taken"})
+
       createUser(user_name, first_name, last_name, password)
         .then(() => next())
         .catch(err => res.status(500).send({message: "Server Error"}))
     })
     .catch(err => res.status(500).send({message: "Server Error"}))
 }
-
-
 
 router.post('/login', token.issue)
 
