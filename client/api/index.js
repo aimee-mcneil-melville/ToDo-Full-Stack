430507b65998e9f94e84e0e7f9fed0edeee05873
Ross Jourdain <unknown>
2020-02-03 12:16:56 +1300

    Surface error message being swallowed

--- client/api/index.js
+++ client/api/index.js
@@ -86,6 +86,8 @@ function errorHandler (method, route) {
   return (err) => {
     if (err.message === 'Not Found') {
       throw Error(`Error: You need to implement an API route for ${method} ${route}`)
+    } else {
+      throw Error(`${err.message} on ${method} ${route}`)
     }
   }
 }
