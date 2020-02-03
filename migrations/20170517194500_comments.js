b098016a128b95bdbfd4fe364220cd2ba6a33231
Bryce Kehoe <bryce@kehoe.co.nz>
2020-02-03 14:52:03 +1300

    Fix reference

--- migrations/20170517194500_comments.js
+++ migrations/20170517194500_comments.js
@@ -1,7 +1,7 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('Comments', function (table) {
     table.increments().primary()
-    table.integer('post_id').references('post.id')
+    table.integer('post_id').references('Posts.id')
     table.date('date_posted')
     table.string('comment')
   })
