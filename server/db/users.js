a40c3544eea181ea762fb07c0b5b059c05d9287c
Ross Jourdain <>
2020-04-22 10:37:43 +1200

    Refactor DB

--- server/db/users.js
+++ server/db/users.js
@@ -2,26 +2,20 @@ const {generatePasswordHash} = require('../auth/hash')
 
 const connection = require('./connection')
 
-function createUser (username, first_name, last_name, password, testDb) {
-  const db = testDb || connection
-
+function createUser (username, first_name, last_name, password, db = connection) {
   return generatePasswordHash(password)
     .then(hash => {
       return db('users').insert({username, first_name, last_name, hash})
     })
 }
 
-function userExists (username, testDb) {
-  const db = testDb || connection
-
+function userExists (username, db = connection) {
   return db('users')
     .where('username', username)
     .then(users => users.length > 0)
 }
 
-function getUserByUsername (username, testDb) {
-  const db = testDb || connection
-
+function getUserByUsername (username, db = connection) {
   return db('users')
     .where('username', username)
     .first()
