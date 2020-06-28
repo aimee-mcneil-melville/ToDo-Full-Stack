a9b07e884f81e9487fd3353b6a8ac9697862a783
Don Smith <hi@dons.dev>
2020-06-28 21:32:31 +1200

    move public folder into server folder

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const redditRoutes = require('./reddit')
 const server = express()
 
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/reddit', redditRoutes)
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './client/index.js',
   mode: 'development',
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, 'server/public'),
     filename: 'bundle.js'
   },
   module: {
