c2cc00cf06e38dcdb97f4e0450795d3f2399728a
Don Smith <hi@dons.dev>
2020-07-21 15:33:13 +1200

    fix failing test

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 
-import App from '../client/components/App'
+import App from './App'
 
 test('<App />', () => {
   const expected = 'React development has begun!'
