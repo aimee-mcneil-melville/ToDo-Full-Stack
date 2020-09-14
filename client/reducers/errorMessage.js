96ef35293a2ca9b92e24fa32a77fd6f0b58cf541
Lache Melvin <lache.melvin@gmail.com>
2020-09-14 22:58:54 +1200

    add error message reducer

---
+++ client/reducers/errorMessage.js
@@ -0,0 +1,13 @@
+import { SHOW_ERROR } from '../actions/error'
+
+const errorMessage = (state = '', action) => {
+  switch (action.type) {
+    case SHOW_ERROR:
+      return action.errorMessage
+
+    default:
+      return state
+  }
+}
+
+export default errorMessage
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import products from './products'
 import cart from './cart'
 import orders from './orders'
+import errorMessage from './errorMessage'
 import waiting from './waiting'
 
 export default combineReducers({
   products,
   cart,
   orders,
+  errorMessage,
   waiting
 })
