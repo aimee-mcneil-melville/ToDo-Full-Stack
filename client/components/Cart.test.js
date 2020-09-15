abb503eccae5d58b7628a6a0bb425f8b740973b3
Lache Melvin <lache.melvin@gmail.com>
2020-09-15 17:01:51 +1200

    fix cart component test

--- client/components/Cart.test.js
+++ client/components/Cart.test.js
@@ -1,55 +1,66 @@
 import React from 'react'
 import { fireEvent, screen } from '@testing-library/react'
-import { renderWithRouter, renderWithRedux } from '../testing/utils'
+import { renderWithRouter } from '../testing/utils'
 import '@testing-library/jest-dom'
 
 import { Cart } from './Cart'
-import mockCart from '../testing/mockCart'
 
-jest.mock('./ui-helpers', () => {
+import mockCart from '../testing/mockCart'
+import mockOrders from '../testing/mockOrders'
+
+import { createOrder } from '../coordinators/cart'
+import { placeOrder } from '../coordinators/orders'
+
+jest.mock('../coordinators/cart', () => {
   return {
-    placeOrder: (cart, history, dispatch) => {
-      expect(cart[2].quantity).toBe(7)
-      expect(history).toHaveLength(2)
-      expect(dispatch()).toMatch('i\'m a mock dispatch function')
-    }
+    createOrder: jest.fn()
+  }
+})
+
+jest.mock('../coordinators/orders', () => {
+  return {
+    placeOrder: jest.fn()
   }
 })
 
 test('renders cart when in props', async () => {
-  expect.assertions(3)
-  renderWithRedux(<Cart cart={mockCart} />)
+  const mockChild = <>mock child fragment</>
+  const { asFragment } = renderWithRouter(<Cart cart={mockCart}>
+    { mockChild }</Cart>)
 
-  const productCells = await screen.queryAllByRole('cell')
   const cartHeaders = await screen.getAllByRole('columnheader')
-  const quantityInputs = await screen.queryAllByRole('textbox')
+  const productCells = await screen.getAllByRole('cell')
+  const quantityInputs = await screen.getAllByRole('textbox')
 
   expect(cartHeaders).toHaveLength(3)
   expect(productCells[0]).toHaveTextContent('mocked cart item 1')
   expect(quantityInputs).toHaveLength(3)
+  expect(asFragment()).toMatchSnapshot()
 })
 
 test('calls placeOrder on button click', async () => {
-  expect.assertions(3)
+  const order = mockOrders[0]
+  const mockHistory = []
+  createOrder.mockImplementation(() => order)
 
-  const mockHistory = ['/', '/cart']
-  const mockDispatch = () => 'i\'m a mock dispatch function'
-  renderWithRedux(<Cart
+  renderWithRouter(<Cart
     cart={mockCart}
     history={mockHistory}
-    dispatch={mockDispatch}
   />)
-
-  const orderButton = await screen.getByText('Place Order')
+  const orderButton = await screen.getByRole('button', { name: 'Place Order' })
   fireEvent.click(orderButton)
+
+  expect(createOrder).toHaveBeenCalledWith(mockCart)
+  expect(placeOrder).toHaveBeenCalled()
+  expect(placeOrder.mock.calls[0][0]).toBe(order)
 })
 
 test('renders a start shopping message when cart is empty', async () => {
-  expect.assertions(3)
-  renderWithRouter(<Cart cart={[]}/>)
+  const { asFragment } = renderWithRouter(<Cart cart={[]}/>)
   const table = await screen.queryByRole('table')
   const emptyMessage = await screen.getByText('cart is empty', { exact: false })
+
   expect(table).not.toBeInTheDocument()
   expect(emptyMessage).toBeInTheDocument()
-  expect(screen).toMatchSnapshot()
+  expect(asFragment()).toMatchSnapshot()
 })
