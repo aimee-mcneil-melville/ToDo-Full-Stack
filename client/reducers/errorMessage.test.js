cb498d1db770e02eff9c425ec7c359215c32b826
Lache Melvin <lache.melvin@gmail.com>
2020-09-15 21:47:41 +1200

    hide error when another action is dispatched

--- client/reducers/errorMessage.test.js
+++ client/reducers/errorMessage.test.js
@@ -10,10 +10,10 @@ test('returns error message on SHOW_ERROR', () => {
   expect(newState).toBe('mock error')
 })
 
-test('returns old state if action does not match', () => {
+test('returns empty string if action does not match', () => {
   const action = {
     type: 'RANDOM_TEST_ACTION_TYPE'
   }
   const newState = errorMessage('mock old error state', action)
-  expect(newState).toBe('mock old error state')
+  expect(newState).toBe('')
 })
