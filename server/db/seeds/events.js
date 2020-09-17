ba323a750774e3ebc8280566d1ccbfb84f4a0ad9
rafaelnhster <rafaelnhster@gmail.com>
2020-09-18 10:39:29 +1200

    change strings to numbers

--- server/db/seeds/events.js
+++ server/db/seeds/events.js
@@ -3,19 +3,19 @@ exports.seed = (knex) => {
     .then(() => {
       return knex('events').insert([{
         id: 1,
-        garden_id: '1',
+        garden_id: 1,
         title: 'Weeding worker Bee',
         date: 'Wed, 27 Sep 2020 20:00:00 GMT',
         description: 'Its time to get these weeds under control.',
-        volunteers_needed: '8'
+        volunteers_needed: 8
       },
       {
         id: 2,
-        garden_id: '1',
+        garden_id: 1,
         title: 'Sowing Corn',
         date: 'Wed, 28 Sep 2020 20:00:00 GMT',
         description: 'Help get out the lovely corns in the ground!.',
-        volunteers_needed: '4'
+        volunteers_needed: 4
       }])
     })
 }
