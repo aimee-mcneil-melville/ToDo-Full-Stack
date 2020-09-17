31dbdcd9765ee2e676df70329f11bd7d819a1c3c
Andras Balint <anoctus@protonmail.com>
2020-09-17 15:27:14 +1200

    add userExist function

---
+++ server/db/users.js
@@ -0,0 +1,18 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  createUser,
+  userExists,
+  getUserByName
+}
+
+function userExists (username, db = connection) {
+  return db('users')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
