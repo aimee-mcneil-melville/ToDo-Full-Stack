3a86e074a909466055bac622c92890bcbf477ecb
Marika jane <66046690+marika-jane@users.noreply.github.com>
2020-09-17 16:31:01 +1200

    Move map fn into Events (from EventItem)

--- client/components/EventItem.jsx
+++ client/components/EventItem.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 
-const events = [{title: 'Weeds worker bee', day: '28/09/2020', time: '8.00 am', volunteers: '8', description: 'We need to weed the upper planting beds'}]
-
-function EventItem () {
+function EventItem ({ event }) {
   return (
     <div>
-      {events.map(event => 
-        <div>
-          <h4>{event.title}</h4>
-          <p>{event.day} at {event.time}</p>
-          <p>{event.volunteers} needed</p>
-          <p>{event.description}</p>          
-        </div>)}
+      <h4 >{event.title}</h4>
+      <p>{event.datetime}</p>
+      <p>{event.volunteersNeeded} volunteers needed</p>
+      <p>{event.description}</p>
     </div>
   )
 } 
