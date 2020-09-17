70fe968d7bfdb0a8e3a4bbfa7308edf0b699faa9
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2020-09-17 15:27:01 +1200

    Added Function

---
+++ server/db/users.js
@@ -0,0 +1,18 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+    createUser,
+    userExists,
+    getUserByName
+  }
+
+
+
+  function getUserByName (username, db = connection) {
+    return db('users')
+      .select()
+      .where('username', username)
+      .first()
+  }
