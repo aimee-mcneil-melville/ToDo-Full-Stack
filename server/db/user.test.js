b620e5732d33dae8f4fbb75c529154646c3b5c0d
Lewis <lewisnpakoti@gmail.com>
2020-09-18 11:56:44 +1200

    update users functions

--- server/db/user.test.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex')
-const { test } = require('./knexfile')
-const config = require('./knexfile').test
-const testDb = knex(config)
-
-const users = require('./users')
-
-test('createUser returns '
---
+++ server/users.js
@@ -0,0 +1,42 @@
+const { generateHash } = require('authenticare/server')
+const connection = require('./db/connection')
+
+// const environment = process.env.NODE_ENV || 'development'
+// const config = require('./knexfile')[environment]
+// const connection = require('knex')(config)
+// const { generateHash } = require('authenticare/server')
+
+module.exports = {
+  createUser,
+  userExists,
+  getUserByName
+}
+
+function createUser (user, db = connection) {
+  return userExists(user.username, db)
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('User exists'))
+      }
+    })
+    .then(() => generateHash(user.password))
+    .then(passwordHash => {
+      return db('users').insert({ username: user.username, hash: passwordHash })
+    })
+}
+
+function userExists (username, db = connection) {
+  return db('users')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
+
+function getUserByName (username, db = connection) {
+  return db('users')
+    .select()
+    .where('username', username)
+    .first()
+}
