b59308ca3e6699758164c59a312b47d01d3a18f0
Marika jane <66046690+marika-jane@users.noreply.github.com>
2020-09-17 15:39:27 +1200

    Hard-code events array

--- client/components/EventItem.jsx
+++ client/components/EventItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const events = [{title: 'Weeds worker bee', day: '28/09/2020', time: '8.00 am', volunteers: '8', description: 'We need to weed the upper planting beds'}]
+
 function EventItem () {
   return (
     <div>
-      <p>test</p>
+      {events.map(event => 
+        <div>
+          <h4>{event.title}</h4>
+          <p>{event.day} at {event.time}</p>
+          <p>{event.volunteers} needed</p>
+          <p>{event.description}</p>          
+        </div>)}
     </div>
   )
 } 
