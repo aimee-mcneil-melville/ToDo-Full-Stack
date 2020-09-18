1761e2250143c56288a81c4e5d4b47da9de502db
Evelyn Coulson <60915688+betty-bucket@users.noreply.github.com>
2020-09-18 16:28:38 +1200

    add styling

--- client/components/Events.jsx
+++ client/components/Events.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import EventItem from './EventItem'
 
-const events = [{title: 'Weeds worker bee', datetime: '28/09/2020 8.00 am', volunteersNeeded: '8', description: 'We need to weed the upper planting beds'}]
+const events = [{ title: 'Weeds worker bee', datetime: '28/09/2020 8.00 am', volunteersNeeded: '8', description: 'We need to weed the upper planting beds' }]
 
 function Events () {
   return (
-    <div>
-      <h2>Events</h2>
-      {events.map((event, index) => 
+    <>
+    <h2 className="my-4">Events</h2>
+    <div className="box">
+      {events.map((event, index) =>
         <EventItem key={index} event={event} />
       )}
     </div>
+    </>
   )
-} 
+}
 
 export default Events
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Map from './Map'
+
 function Home () {
   return (
-    <div className='app'>
-      <p>Home Page</p>
-      <Link to={'/garden'}>Garden</Link>
-      
+    <>
+    <div className='column'>
+      <p>Help your community get the most out of your garden with events and reporting and become elgible for government subsidies</p>
+      <Link className="button is-primary my-4" to={'/garden'}>Garden</Link>
     </div>
+    <Map/>
+    </>
   )
 }
 
