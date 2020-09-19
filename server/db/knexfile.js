21fed24278306c58e3925d77290bfa063ff7e5a3
Lache Melvin <lache.melvin@gmail.com>
2020-09-19 20:43:37 +1200

    create db testing setup

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -8,6 +8,19 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
+    }
+  },
   staging: {
     client: 'postgresql',
     connection: {
---
+++ server/db/products.test.js
@@ -0,0 +1,11 @@
+const env = require('./testEnv')
+const db = require('./products')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
+})
+
+afterEach(() => env.cleanup(testDb))
