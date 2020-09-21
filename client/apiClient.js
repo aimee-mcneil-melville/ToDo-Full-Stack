115b3a0313220c6c1b9ae40fe465d7d5841011df
Evelyn Coulson <60915688+betty-bucket@users.noreply.github.com>
2020-09-22 10:56:04 +1200

    install leaflet and add leaflet map

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,4 +1,5 @@
 import request from 'superagent'
+import L from 'leaflet'
 
 const rootUrl = '/api/v1'
 
@@ -7,4 +8,17 @@ export function getGardens () {
     .then(res => {
       return res.body.gardens
     })
+}
+
+export function getMap () {
+  const mymap = L.map('mapid').setView([-36.8666700, 174.7666700], 13)
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q'
+  }).addTo(mymap)
 }
--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getMap } from '../apiClient'
 
 function Map () {
+  useEffect(()=> {
+    getMap()
+  })
+  
+
 // fakeMap can be deleted once our API is in place and using our databse
-  const fakeMap = [
-    {
-      id: 1,
-      name: 'Kelmarna Gardens',
-      address: '12 Hukanui Crescent',
-      description: 'Kelmarna Gardens is a city farm and organic community garden, situated on 4.5 acres of council land in Ponsonby, close to the heart of Auckland City.',
-      lat: -36.86011508905973,
-      lon: 174.7330772002716,
-      url: 'http://www.kelmarnagardens.nz/'
-    }
-  ]
+  // const fakeMap = [
+  //   {
+  //     id: 1,
+  //     name: 'Kelmarna Gardens',
+  //     address: '12 Hukanui Crescent',
+  //     description: 'Kelmarna Gardens is a city farm and organic community garden, situated on 4.5 acres of council land in Ponsonby, close to the heart of Auckland City.',
+  //     lat: -36.86011508905973,
+  //     lon: 174.7330772002716,
+  //     url: 'http://www.kelmarnagardens.nz/'
+  //   }
+  // ]
   return (
     <div className='column'>
-      <img src="/placeholder_auckland.jpg"/>
+      <div id="mapid"/>
+      {/* <img src="/placeholder_auckland.jpg"/> */}
       <p>
-        {fakeMap.map(garden => (garden.address))}
+        {/* {fakeMap.map(garden => (garden.address))} */}
       </p>
     </div>
   )
