e43db3a85a25f91be1f2a02aae6560449fe4c0fc
Daryl Chen (Daz) <64760754+Daryl-creator@users.noreply.github.com>
2020-09-21 15:18:01 +1200

    attempt map component test

--- cypress/integration/gardenz_tests/homepage.spec.js
+++ cypress/integration/gardenz_tests/homepage.spec.js
@@ -5,11 +5,18 @@ describe('Home', () => {
     cy.visit('http://localhost:3000/#/')
     // evenutally this will be the deployed heroku app: 'https://pohutukawa-gardenz.herokuapp.com/'
   })
-})
-it('Click sign in', () => {
-  // get nav component and click on sign in
-  cy.getReact('Nav').contains('Sign in').click()
 
-  // assert
-  cy.location('pathname').should('include', 'signin')
+  it('renders map component', () => {
+    // get map component
+    cy.get('img')
+    .contains('/placeholder_auckland.jpg')
+  })
+
+  it('Click sign in', () => {
+    // get nav component and click on sign in
+    cy.getReact('Nav').contains('Sign in').click()
+
+    // assert
+    cy.location('pathname').should('include', 'signin')
+  })
 })
