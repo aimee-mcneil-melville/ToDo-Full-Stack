e07e425de2efa2a96c676b001d70980a4cdcad84
michael-rua <michaelrua1@gmail.com>
2020-09-21 17:45:40 +1200

    add changes to server file

---
+++ client/components/UserContext.jsx
@@ -0,0 +1,22 @@
+// import React, { useState, createContext } from 'react'
+
+// export const UserContext = createContext()
+
+// export const UserProvider = props => {
+//   const [user, setUser] = useState({
+//     username: '',
+//     isAdmin: '',
+//     gardenId: ''
+//   })
+
+//   const updateUser = (event) => {
+//     setUser(props.username)
+//   }
+
+//   console.log(props.username)
+//   return (
+//     <UserContext.Provider value= {[user, setUser]}>
+//       {props.children}
+//     </UserContext.Provider>
+//   )
+// }
