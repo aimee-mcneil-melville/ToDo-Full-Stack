18f460705638f7919cd0f7d8146bfbb2bc2a7a02
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-09-22 11:27:40 +1200

    global

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,14 +4,16 @@ import { Route } from 'react-router-dom'
 import Nav from './Nav'
 import Register from './Register'
 import SignIn from './SignIn'
+import Garden from './Garden'
 
-function App () {
+function App() {
   return (
     <div className="app">
       <h1>Gardenz</h1>
-      <Route path='/' component={Nav}/>
-      <Route path='/register' component={Register}/>
-      <Route path='/signin' component={SignIn}/>
+      <Route path="/" component={Nav} />
+      <Route path="/register" component={Register} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/garden" component={Garden} />
     </div>
   )
 }
