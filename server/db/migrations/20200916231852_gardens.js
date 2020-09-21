98855dd61c675eafca6a30f4c3207004b4a1da9d
Bhavisha Das <bhivxx@gmail.com>
2020-09-21 16:28:04 +1200

    update string num

--- server/db/migrations/20200916231852_gardens.js
+++ server/db/migrations/20200916231852_gardens.js
@@ -3,7 +3,7 @@ exports.up = (knex) => {
     table.increments('id')
     table.string('name')
     table.string('address')
-    table.string('description')
+    table.string('description', 1024)
     table.decimal('lat', 17, 14)
     table.decimal('lon', 17, 14)
     table.string('url')
