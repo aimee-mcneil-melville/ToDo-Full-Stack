780b6de4a694f4c5a94e9402c60baed824df759f
Kahu Leary <63678974+Kahu-Leary@users.noreply.github.com>
2020-09-21 16:27:21 +1200

    Remove mock function

--- server/routes/gardens.js
+++ server/routes/gardens.js
@@ -4,29 +4,9 @@ const db = require('../db')
 
 const router = express.Router()
 
-function getGardenById (id) {
-  const fakeGarden = {
-    'id': 2,
-    'name': 'Kahu Gardens',
-    'address': '12 Hukanui Crescent',
-    'description': 'Kelmarna Gardens is a city farm and ...',
-    'lat': -36.86011508905973,
-    'lon': 174.7330772002716,
-    'url': 'http://www.kelmarnagardens.nz',
-    'events': [{
-      'id': 1,
-      'volunteersNeeded': 8,
-      'title': 'Weeding Worker Bee',
-      'datetime': 'Wed, 27 Sep 2020 20:00:00 GMT',
-      'description': "It's time to get these weeds under control."
-    }]
-  }
-  return Promise.resolve(fakeGarden)
-}
-
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id)
-  getGardenById(id)
+  db.getGardenById(id)
     .then(singleGarden => {
       return res.json(singleGarden)
     })
