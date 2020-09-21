8d869d96dd3c6a4fafde091b8035403f6c43ccc5
Daryl Chen (Daz) <64760754+Daryl-creator@users.noreply.github.com>
2020-09-21 14:55:01 +1200

    create test for signin

--- cypress/integration/gardenz_tests/homepage.spec.js
+++ cypress/integration/gardenz_tests/homepage.spec.js
@@ -1,7 +1,15 @@
+/// <reference types="cypress" />
+
 describe('Home', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/#/')
     // evenutally this will be the deployed heroku app: 'https://pohutukawa-gardenz.herokuapp.com/'
-    cy.getReact('Nav').contains('Sign in').click()
   })
+})
+it('Click sign in', () => {
+  // get nav component and click on sign in
+  cy.getReact('Nav').contains('Sign in').click()
+
+  // assert
+  cy.location('pathname').should('include', 'signin')
 })
