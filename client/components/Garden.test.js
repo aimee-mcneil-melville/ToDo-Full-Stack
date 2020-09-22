93d74fbd268dd3b1d979bbfd739b4587bc2531b6
rafaelnhster <rafaelnhster@gmail.com>
2020-09-22 16:23:30 +1200

    lint and test passed

--- client/components/Garden.test.js
+++ client/components/Garden.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
 import renderer from 'react-test-renderer'
+import { UserProvider } from './UserContext'
 
 import Garden from './Garden'
 
@@ -12,7 +13,9 @@ it('renders correctly', () => {
   }
   const tree = renderer.create(
     <HashRouter>
-      <Garden garden={fakeGarden} />
+      <UserProvider>
+        <Garden garden={fakeGarden} />
+      </UserProvider>
     </HashRouter>
   ).toJSON()
   expect(tree).toMatchSnapshot()
