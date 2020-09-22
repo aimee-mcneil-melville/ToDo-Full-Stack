d332580b950dbbaac887d15d0c5ac28257f2add6
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2020-09-22 16:35:45 +1200

    resolved missing routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useContext } from 'react'
-import { Route } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import { isAuthenticated } from 'authenticare/client'
 
 import { UserContext, updateUserContext } from './UserContext'
 import Nav from './Nav'
 import Register from './Register'
 import SignIn from './SignIn'
+import Garden from './Garden'
+import Home from './Home'
+import AddEvent from './AddEvent'
+import EditEvent from './EditEvent'
 
 function App () {
   const [, setUser] = useContext(UserContext)
@@ -17,12 +21,21 @@ function App () {
   }, [])
 
   return (
-    <div className="app">
-      <h1>Gardenz</h1>
-      <Route path="/" component={Nav} />
-      <Route path="/register" component={Register} />
-      <Route path="/signin" component={SignIn} />
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">
+          <Link to="/">Garde<span>nz</span></Link></h1>
+        <div className='columns'>
+          <Route exact path='/' component={ Home } />
+          <Route path="/" component={Nav} />
+          <Route path="/register" component={Register} />
+          <Route path="/signin" component={SignIn} />
+          <Route path='/garden' component={ Garden } />
+          <Route path='/events/new' component={ AddEvent } />
+          <Route path='/events/:id/edit' component={ EditEvent } />
+        </div>
+      </div>
+    </>
   )
 }
 
