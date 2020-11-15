e558e2343eda0e548e79389204ced12c0664d8e8
Don Smith <hi@dons.dev>
2020-11-16 10:30:05 +1300

    add lint rules and fixes

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
 import wombatReducer from './reducer'
