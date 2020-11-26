fc646003ae14395f12c6140706ccfb9b0b5377fb
Anna-Green <67266645+Anna-Green@users.noreply.github.com>
2020-11-27 10:59:53 +1300

    api route done

--- client/api/events.js
+++ client/api/events.js
@@ -1,14 +1,18 @@
 export function getEvents (gardenId, consume = requestor) {
-    return consume(`/gardens/${gardenId}`)
-      .then(res => {
-        return res.body
-        console.log(hi)
-      })
-  }
+  return consume(`/gardens/${gardenId}`)
+    .then(res => {
+      return res.body
+    })
+}
 
-  export function addEvent (gardenId, consume = requestor) {
-    return consume(`/gardens/${gardenId}`)
-      .then(res => {
-        return res.body
-      })
-  }
+export function addEvent (gardenId, newEvent, consume = requestor) {
+  return consume
+    .post('/api/v1/events/new')
+    .send({
+      gardenId: gardenId,
+      title: newEvent.state.title,
+      dateTime: newEvent.state.dateTime,
+      volunteersNeeded: newEvent.state.volunteersNeeded,
+      description: newEvent.state.description
+    })
+}
