42446507f8bd3866bbceb3a5fbe17f689b05886a
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2020-11-26 16:13:08 +1300

    added get events

--- server/db/event.js
+++ server/db/event.js
@@ -1,10 +1,14 @@
 const connection = require('./connection')
 
 module.exports = {
-  getEvents
+  getEvents,
+  addEvent
 }
 
-function getEvents (newEvent, db = connection) {
+function getEvents(){
+    return db('events').select('id','garden_id','title','date','description','volunteers_needed')
+}
+function addEvent (newEvent, db = connection) {
     return db('events')
         .insert(newEvent)
 }
