63c49f4433cc87c665bec80ade30f223675c6ee7
Lache Melvin <lache.melvin@gmail.com>
2020-11-27 16:34:18 +1300

    add clear waiting action

--- client/actions/waiting.js
+++ client/actions/waiting.js
@@ -1,7 +1,14 @@
 export const SET_WAITING = 'SET_WAITING'
+export const CLEAR_WAITING = 'CLEAR_WAITING'
 
 export const setWaiting = () => {
   return {
     type: SET_WAITING
+  }
+}
+
+export const clearWaiting = () => {
+  return {
+    type: CLEAR_WAITING
   }
 }
