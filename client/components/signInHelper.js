0adf47b4c1da0f7f676869c4070aa23f8af78a41
Lache Melvin <lache.melvin@gmail.com>
2020-11-27 13:58:05 +1300

    refactor sign in event handling to signInHelper

---
+++ client/components/signInHelper.js
@@ -0,0 +1,17 @@
+import { isAuthenticated, signIn, getDecodedToken } from 'authenticare/client'
+
+import { dispatch } from '../store'
+import { setUser } from '../actions/user'
+
+export function signInUser (user, navigateTo) {
+  const { username, password } = user
+  return signIn({ username, password }, { baseUrl: '/api/v1' })
+    .then(() => {
+      if (isAuthenticated()) {
+        const { username, isAdmin, gardenId } = getDecodedToken()
+        dispatch(setUser({ username, isAdmin, gardenId }))
+        navigateTo('/garden')
+      }
+      return null
+    })
+}
