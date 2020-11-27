9ff4668c27417edb9b6c1a63870eba394b5643e7
Lache Melvin <lache.melvin@gmail.com>
2020-11-27 16:30:23 +1300

    create error reducer;

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import user from './user'
 import waiting from './waiting'
+import error from './error'
 
 export default combineReducers({
   user,
-  waiting
+  waiting,
+  error
 })
