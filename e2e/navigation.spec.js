2dcee71b8e70c09e0de8aa2016d2d1d4b91f27fc
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-11-27 15:03:17 +1300

    Created tests for navigation from the home link

--- e2e/navigation.spec.js
+++ e2e/navigation.spec.js
@@ -4,9 +4,12 @@ let browser
 let page
 // Declare app variable
 const app = 'http://localhost:3000/#/'
+const signIn = 'http://localhost:3000/#/signin'
+
 beforeAll(async () => {
   browser = await playwright.chromium.launch({
     headless: true
+    // slowMo: 2000
   })
 })
 beforeEach(async () => {
@@ -19,15 +22,62 @@ afterEach(async () => {
   await page.close()
 })
 
+// THE FOLLOWING TESTS ARE FROM THE HOME PAGE!!!!!!!!!!!!!!
+
 test('clicking on sign-in takes you to sign in page', async () => {
   // Go to http://localhost:3000/#/
   await page.goto(app)
 
   await page.click('text="Sign in"')
-  await page.goto('http://localhost:3000/#/signin')
 
-  const signIn = page.url('http://localhost:3000/#/signin')
+  const url = page.url()
 
-  // expect(page).toBe(signIn)
-  expect(page).toBe('http://localhost:3000/#/signin')
+  expect(url).toBe('http://localhost:3000/#/signin')
+})
+
+test('clicking on register takes you to register page', async () => {
+  // Go to http://localhost:3000/#/
+  await page.goto(app)
+
+  await page.click('text="Register"')
+
+  const url = page.url()
+
+  expect(url).toBe('http://localhost:3000/#/register')
+})
+
+test('clicking on garden button takes you to garden page', async () => {
+  // Go to http://localhost:3000/#/
+
+  await page.goto(app)
+
+  await page.click('text="Garden"')
+
+  const url = page.url()
+
+  expect(url).toBe('http://localhost:3000/#/garden')
+}, 20000)
+
+// THE FOLLOWING TESTS ARE FROM THE SIGN IN PAGE!!!!!!!!!!!!!!
+
+test('clicking on register (from sign in page) takes you to register page', async () => {
+  // Go to http://localhost:3000/#/
+  await page.goto(signIn)
+
+  await page.click('text="Register"')
+
+  const url = page.url()
+
+  expect(url).toBe('http://localhost:3000/#/register')
+})
+
+test('clicking on Home(from sign in page) takes you to Home page', async () => {
+  // Go to http://localhost:3000/#/
+  await page.goto(signIn)
+
+  await page.click('text="Home"')
+
+  const url = page.url()
+
+  expect(url).toBe('http://localhost:3000/#/')
 })
