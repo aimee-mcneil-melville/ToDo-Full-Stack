db2d26a3dde1325e44a54f09f72484e62ac8cd6f
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-11-30 10:38:29 +1300

    Updated test for signin to work with mock database

--- e2e/signIn.spec.js
+++ e2e/signIn.spec.js
@@ -1,5 +1,8 @@
 import 'regenerator-runtime/runtime'
 const playwright = require('playwright')
+
+const connection = require('../server/db/connection')
+
 let browser
 let page
 // Declare app variable
@@ -7,6 +10,7 @@ const app = 'http://localhost:3000/#/signin'
 
 // Run this before all tests
 beforeAll(async () => {
+  connection.migrate.latest()
   browser = await playwright.chromium.launch({
     headless: true
   })
@@ -14,11 +18,13 @@ beforeAll(async () => {
 
 // Run this before each test
 beforeEach(async () => {
+  connection.seed.run()
   page = await browser.newPage()
 })
 
 // Run this after all tests
 afterAll(async () => {
+  connection.destroy()
   await browser.close()
 })
 
@@ -27,26 +33,26 @@ afterEach(async () => {
   await page.close()
 })
 
-test('username and password is entered correctly', async () => {
+test('username and password are each a string', async () => {
   await page.goto(app)
   // Click input[name="username"]
   await page.click('input[name="username"]')
 
   // Fill input[name="username"]
-  await page.fill('input[name="username"]', 'admin')
+  await page.fill('input[name="username"]', 'member')
 
   // Click input[name="password"]
   await page.click('input[name="password"]')
 
   // Fill input[name="password"]
-  await page.fill('input[name="password"]', 'admin')
+  await page.fill('input[name="password"]', 'member')
 
   const username = await page.$eval('[name="username"]', userName => userName.value)
 
   const password = await page.$eval('[name="password"]', password => password.value)
 
-  expect(username).toMatch('admin')
-  expect(password).toMatch('admin')
+  expect(typeof username).toBe('string')
+  expect(typeof password).toBe('string')
 })
 
 test('clicking the signin button takes you to the garden page', async () => {
