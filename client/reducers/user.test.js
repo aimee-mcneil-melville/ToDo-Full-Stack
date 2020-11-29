6a76b46b8d4764f87ed1c99227af15f9fc0f84a5
Michelle (Shelly) Mutu-Grigg <shellymutugrigg@gmail.com>
2020-11-30 12:42:00 +1300

    add in testing for USER_LOCATION to reducers/user.test.js

--- client/reducers/user.test.js
+++ client/reducers/user.test.js
@@ -4,7 +4,9 @@ test('returns new user object on "SET_USER"', () => {
   const oldState = {
     username: '',
     isAdmin: false,
-    gardenId: null
+    gardenId: null,
+    latitude: null,
+    longitude: null
   }
 
   const action = {
@@ -12,7 +14,9 @@ test('returns new user object on "SET_USER"', () => {
     user: {
       username: 'test user',
       isAdmin: false,
-      gardenId: 2
+      gardenId: 2,
+      latitude: null,
+      longitude: null
     }
   }
   const newState = userReducer(oldState, action)
@@ -24,7 +28,9 @@ test('returns default empty user object on "CLEAR_USER"', () => {
   const oldState = {
     username: 'test user',
     isAdmin: false,
-    gardenId: 2
+    gardenId: 2,
+    latitude: null,
+    longitude: null
   }
   const action = {
     type: 'CLEAR_USER'
@@ -38,11 +44,32 @@ test('returns old state on unknown action type', () => {
   const oldState = {
     username: 'test user',
     isAdmin: false,
-    gardenId: 2
+    gardenId: 2,
+    latitude: null,
+    longitude: null
   }
   const action = {
     type: 'RANDOM_OTHER_ACTION'
   }
   const newState = userReducer(oldState, action)
   expect(newState).toBe(oldState)
+})
+
+test('returns user location data attached to object on "USER_LOCATION"', () => {
+  const oldState = {
+    username: '',
+    isAdmin: false,
+    gardenId: null,
+    latitude: null,
+    longitude: null
+  }
+  const latitude = -36.8721312
+  const longitude = 174.7837615
+  const location = { latitude, longitude }
+  const action = {
+    type: 'USER_LOCATION',
+    location
+  }
+  const newState = userReducer(oldState, action)
+  expect(newState).not.toBe(oldState)
 })
