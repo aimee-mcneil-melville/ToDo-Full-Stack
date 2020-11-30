0f7304c3252c963ae93905f4f0c87d2bcc0bb331
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2020-11-30 15:31:40 +1300

    added client api getEventById

--- client/api/events.test.js
+++ client/api/events.test.js
@@ -1,4 +1,5 @@
-import { addEvent } from './events'
+import { getEvents } from '../../server/db/event'
+import { addEvent, getEventsById } from './events'
 
 test('addEvent', () => {
   const consume = () => Promise.resolve({
@@ -8,6 +9,21 @@ test('addEvent', () => {
     }
   })
   return addEvent({}, consume)
+    .then((event) => {
+      expect(event.id).toBe(1)
+      return null
+    })
+})
+
+test('getEvents return a id', () => {
+  const consume = () => Promise.resolve({
+    body: {
+      id: 1,
+      title : 'Patrick'
+    }
+  })
+  const id = consume.id
+  return getEventsById(id, consume)
     .then((event) => {
       expect(event.id).toBe(1)
       return null
