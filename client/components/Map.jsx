c6805fc74483a7142a6167adf49e36eb0ed2027f
Michelle (Shelly) Mutu-Grigg <shellymutugrigg@gmail.com>
2020-12-01 14:28:45 +1300

    update to correct lat long coords of gardens

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -10,7 +10,7 @@ function Map (props) {
     <div className='column'>
       <MapContainer
         center={[-36.8666700, 174.7666700]}
-        zoom={12}
+        zoom={11}
         scrollWheelZoom={true}>
         <TileLayer
           url='https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q'/>
