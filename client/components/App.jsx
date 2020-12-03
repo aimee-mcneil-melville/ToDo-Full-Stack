5afc3f01fb5611c4218183e9428365f9f4861ff0
Kelly Keating <keating.kel@gmail.com>
2020-12-03 15:32:12 +1300

    hooks solution

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>React development has begun!</div>
-  )
+import Pixel from './Pixel'
+
+function App () {
+  return Array.from({ length: 1000 }, (item, i) => <Pixel key={i} />)
 }
 
 export default App
