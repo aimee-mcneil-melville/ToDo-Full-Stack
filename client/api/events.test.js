6248a6790b4fc12aceb710dc892bd258e38c35ef
Lache Melvin <lache.melvin@gmail.com>
2020-12-06 16:41:54 +1300

    fix up events api function tests

--- client/api/events.test.js
+++ client/api/events.test.js
@@ -1,51 +1,57 @@
 import { postEvent, patchEvent, getEventById } from './events'
 
-jest.mock('../store')
-
-test('postEvent', () => {
-  const consume = () => Promise.resolve({
-    body: {
-      id: 1,
-      title: 'testevent'
-    }
-  })
-  return postEvent({}, consume)
+test('getEventById builds url correctly and returns res.body', () => {
+  expect.assertions(2)
+  const consume = (url) => {
+    expect(url).toBe('/events/1')
+    return Promise.resolve({
+      body: {
+        title: 'Patricks event'
+      }
+    })
+  }
+  return getEventById(1, consume)
     .then((event) => {
-      expect(event.id).toBe(1)
+      expect(event.title).toBe('Patricks event')
       return null
     })
 })
 
-test('patchEvent', () => {
-  const consume = () => Promise.resolve({
-    body: {
-      id: 1,
-      title: 'title',
-      date: '01/12/2020',
-      description: 'this is the description',
-      volunteers_needed: 9
-
-    }
-  })
-  return patchEvent({}, consume)
-    .then((event) => {
-      expect(event.id).toBe(1)
+test('postEvent calls consume correctly and returns res.body', () => {
+  expect.assertions(4)
+  const consume = (url, method, event) => {
+    expect(url).toBe('/events')
+    expect(method).toBe('post')
+    expect(event.title).toBe('test event')
+    return Promise.resolve({
+      body: {
+        id: 2,
+        title: 'test event'
+      }
+    })
+  }
+  return postEvent({ title: 'test event' }, consume)
+    .then((newEvent) => {
+      expect(newEvent.id).toBe(2)
       return null
     })
 })
 
-test('getEventById return a id', () => {
-  const consume = () => Promise.resolve({
-    body: {
-      id: 1,
-      title: 'Patrick'
-    }
-  })
-  const id = consume.id
-  return getEventById(id, consume)
+test('patchEvent builds url correctly and returns res.body', () => {
+  const consume = (url, method, event) => {
+    expect(url).toBe('/events/1')
+    expect(method).toBe('patch')
+    expect(event.title).toBe('epic event')
+    return Promise.resolve({
+      body: {
+        id: 1,
+        title: 'epic event'
+      }
+    })
+  }
+  return patchEvent({ id: 1, title: 'epic event' }, consume)
     .then((event) => {
       expect(event.id).toBe(1)
-      expect(event.title).toBe('Patrick')
       return null
     })
 })
