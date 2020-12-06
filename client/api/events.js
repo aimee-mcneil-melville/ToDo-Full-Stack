808d77582bb442a75e0b8d102660ea1d4eb5d2ee
Lache Melvin <lache.melvin@gmail.com>
2020-12-06 16:35:46 +1300

    reorder api calls to crud order for my sanity;

--- client/api/events.js
+++ client/api/events.js
@@ -7,15 +7,15 @@ export function getEventById (id, consume = requestor) {
     })
 }
 
-export function patchEvent (event, consume = requestor) {
-  return consume(`/events/${event.id}`, 'patch', event)
+export function postEvent (newEvent, consume = requestor) {
+  return consume('/events', 'post', newEvent)
     .then(res => {
       return res.body
     })
 }
 
-export function postEvent (newEvent, consume = requestor) {
-  return consume('/events', 'post', newEvent)
+export function patchEvent (event, consume = requestor) {
+  return consume(`/events/${event.id}`, 'patch', event)
     .then(res => {
       return res.body
     })
