ce8ef56171524ebf79711619cfac83eaaf9213d2
jgray1 <john.edwin.gray@gmail.com>
2021-01-05 17:17:12 +1300

    convert Comments.jsx to use hooks

--- client/components/Comments.jsx
+++ client/components/Comments.jsx
@@ -1,43 +1,40 @@
 import React from 'react'
-import {Link, Route} from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 
 import Comment from './Comment'
 import CommentForm from './CommentForm'
-
-class Comments extends React.Component {
-  render () {
-    const {postId, comments, fetchComments} = this.props
-    return (
-      <div>
-        <p>
-          <Link
-            className='pure-button'
-            to={`/posts/${postId}/comments/new`}>
+function Comments (props) {
+  const { postId, comments, fetchComments } = props
+  return (
+    <div>
+      <p>
+        <Link
+          className='pure-button'
+          to={`/posts/${postId}/comments/new`}>
             Add A New Comment
-          </Link>
-        </p>
-        <Route
-          path='/posts/:postId/comments/new'
-          render={props => (
-            <CommentForm
-              fetchComments={fetchComments}
-              {...props}
-            />
-          )}
-        />
-        <div>
-          {comments.map(comment => (
-            <Comment
-              key={comment.id}
-              postId={postId}
-              comment={comment}
-              fetchComments={fetchComments}
-            />
-          ))}
-        </div>
+        </Link>
+      </p>
+      <Route
+        path='/posts/:postId/comments/new'
+        render={props => (
+          <CommentForm
+            fetchComments={fetchComments}
+            {...props}
+          />
+        )}
+      />
+      <div>
+        {comments.map(comment => (
+          <Comment
+            key={comment.id}
+            postId={postId}
+            comment={comment}
+            fetchComments={fetchComments}
+          />
+        ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Comments.defaultProps = {
