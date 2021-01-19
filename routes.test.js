74f4fa793d26e0c39f12ed9a3c7401b4399ca41c
Joshua Vial <joshua@enspiral.com>
2021-01-19 15:04:10 +1300

    removing test setup file from routes since we're using mocks instead

--- routes.test.js
+++ routes.test.js
@@ -1,18 +1,5 @@
 const request = require('supertest')
-
 const server = require('./server')
-const testEnv = require('./test-environment')
-
-let testDb = testEnv.getTestDb()
-
-// Create a separate in-memory database before each test.
-beforeEach(() => {
-  testDb = testEnv.getTestDb()
-  return testEnv.initialise(testDb)
-})
-
-// Destroy the database connection after each test.
-afterEach(() => testEnv.cleanup(testDb))
 
 test('list wombles', () => {
   const expected = 'WOMBLES!'
