c9388dca46b7166078d04ace42b58ccc98af844e
Don Smith <hi@dons.dev>
2021-02-01 16:50:44 +1300

    move webpack.config into client and public into server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const posts = require('./routes/posts')
 const comments = require('./routes/comments')
 
 const server = express()
-server.use(express.static('public'))
+server.use(express.static(path.join(__dirname, 'public')))
 server.use(express.json())
 
 server.use('/v1/posts', posts)
@@ -13,7 +13,7 @@ server.use('/v1/comments', comments)
 server.use('/v1/*', (req, res) => res.sendStatus(404))
 
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 module.exports = server
