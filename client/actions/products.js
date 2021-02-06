3bcb1c8edd8780ab95736bfd38807d2277dc410c
Lache Melvin <lache.melvin@gmail.com>
2021-02-07 10:49:38 +1300

    dispatch getProducts error message sent from route

--- client/actions/products.js
+++ client/actions/products.js
@@ -26,7 +26,13 @@ export function fetchProducts () {
         return null
       })
       .catch((err) => {
-        dispatch(showError(err.message))
+        // if the error is from our routes, this will use the message our route
+        // sends back, rather than the generic 'Internal Server Error' from a
+        // status 500
+        // if the error is from elsewhere in the Promise chain, there won't be
+        // an err.response object, so we use err.message
+        const errMessage = err.response?.text || err.message
+        dispatch(showError(errMessage))
       })
   }
 }
