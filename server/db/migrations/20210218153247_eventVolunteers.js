8a179671a18951a16bcbedfa20df9dd09c8e1b7c
Christo-lk <73858996+Christo-lk@users.noreply.github.com>
2021-02-18 15:39:42 +1300

    created eventVolunteers table

---
+++ server/db/migrations/20210218153247_eventVolunteers.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('eventVolunteers', (table) => {
+    table.increments('id')
+    table.integer('user_id').references('users.id')
+    table.integer('event_id').references('events.id')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('eventVolunteers')
+}
