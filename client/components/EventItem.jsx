c45997d69522fdb14fa4093b95c022e77b63365d
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-19 11:58:27 +1300

    check if already a volunteer

--- client/components/EventItem.jsx
+++ client/components/EventItem.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { getIfVolunteer } from './EventItemHelper'
 
 export default function EventItem ({ event, isAdmin }) {
   const { id, title, date, volunteersNeeded, description } = event
+  // volunteers to be recieved from event object
+  const volunteers = ['member', 'anna', 'tausani']
+
+  useEffect(() => {
+    const ifVolunteer = getIfVolunteer(volunteers)
+    setIsVolunteer(ifVolunteer)
+  }, [])
 
   const [isVolunteer, setIsVolunteer] = useState(false)
 
   function clickHandler () {
     setIsVolunteer(!isVolunteer)
-    console.log(isVolunteer)
   }
 
   return (
--- client/components/EventItemHelper.js
+++ client/components/EventItemHelper.js
@@ -1,1 +1,19 @@
-import { dispatch } from '../store'
+import { dispatch, getState } from '../store'
+
+export function getIfVolunteer (volunteers) {
+  const storeState = getState()
+  const { username } = storeState.user
+  // const username = 'clare'
+
+  const index = volunteers.findIndex((volunteer) => {
+    return volunteer === username
+  })
+
+  const ifVolunteer = index !== -1
+
+  return ifVolunteer
+}
+
+// export function () {
+
+// }
