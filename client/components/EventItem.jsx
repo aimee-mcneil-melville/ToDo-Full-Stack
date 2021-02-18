e1161a4a7e87e6140e90a78ecc4bc1c878740876
tausani-ah-chong <tausani93@gmail.com>
2021-02-18 17:27:41 +1300

    add func to toggle button

--- client/components/EventItem.jsx
+++ client/components/EventItem.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function EventItem ({ event, isAdmin }) {
   const { id, title, date, volunteersNeeded, description } = event
+
+  const [isVolunteer, setIsVolunteer] = useState(false)
+
+  function clickHandler () {
+    setIsVolunteer(!isVolunteer)
+    console.log(isVolunteer)
+  }
+
   return (
     <div>
       <h4 className="is-pulled-left">{title}</h4>
       {
         isAdmin
           ? <Link to={`/events/${id}/edit`} className="button is-pulled-right">Edit Event</Link>
-          : null
+          : !isVolunteer
+            ? <button onClick={clickHandler} className="button is-pulled-right">Volunteer</button>
+            : <button onClick={clickHandler} className="button is-pulled-right">Un-Volunteer</button>
+
       }
       <p className="clearfix">{date}</p>
       <p>{volunteersNeeded} volunteers needed</p>
