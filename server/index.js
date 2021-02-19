29a3ba13b5e50b25bf674497921bc3762c21595b
sirchras <cjlynch@hotmail.co.nz>
2021-02-19 15:52:14 +1300

    remove breaking change

--- server/index.js
+++ server/index.js
@@ -3,11 +3,6 @@ const server = require('./server')
 // https://stackoverflow.com/a/18024792
 const port = process.env.PORT || 3000
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  const envConfig = require('dotenv').config()
-  if (envConfig.error) throw envConfig.error
-}
-
 server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', port)
