08aa7482bd6fc9e66af73a57f714ab757d863b8b
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-22 10:56:03 +1300

    tests for volunteer button

--- client/components/EventItem.test.js
+++ client/components/EventItem.test.js
@@ -16,3 +16,20 @@ describe('Edit Event button', () => {
     expect(screen.queryByRole('link')).toBeNull()
   })
 })
+
+describe('Volunteer/ Un-Volunteer button', () => {
+  it('displays Volunteer for member if not volunteered for event', () => {
+    renderWithRouter(<EventItem isAdmin={false} isVolunteer={false} event={{}} />)
+    const button = screen.queryByRole('button')
+    expect(button.textContent).toBe('Volunteer')
+  })
+  // it('displays Un-Volunteer for member if already volunteered for event', () => {
+  //   renderWithRouter(<EventItem isAdmin={false} isVolunteer={true} event={{}} />)
+  //   const button = screen.queryByRole('button')
+  //   expect(button.textContent).toBe('Un-Volunteer')
+  // })
+  it('does not display if not a member', () => {
+    renderWithRouter(<EventItem isAdmin={true} event={{}}/>)
+    expect(screen.queryByRole('button')).toBeNull()
+  })
+})
