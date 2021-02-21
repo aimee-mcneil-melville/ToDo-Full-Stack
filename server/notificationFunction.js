f3b13a04e56390e0ef9bbc5f6f0099fd4b36efc4
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-22 10:17:29 +1300

    notificationFunction created

---
+++ server/notificationFunction.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+
+export function sendNotification (userdata, eventdata) {
+  const http = require('https')
+
+  const options = {
+    method: 'POST',
+    hostname: 'api.sendgrid.com',
+    port: null,
+    path: '/v3/mail/send',
+    headers: {
+      authorization: `Bearer ${process.env.JWT_SECRET}`,
+      'content-type': 'application/json'
+    }
+  }
+
+  // const data = {
+
+  // }
+
+  const req = http.request(options, function (res) {
+    const chunks = []
+
+    res.on('data', function (chunk) {
+      chunks.push(chunk)
+    })
+
+    res.on('end', function () {
+      const body = Buffer.concat(chunks)
+      console.log(body.toString())
+    })
+  })
+
+  req.write(JSON.stringify({
+    personalizations: [
+      {
+        to: [
+          {
+            email: 'zeppamariano@gmail.com',
+            name: 'John Doe'
+          }
+        ],
+        dynamic_template_data: {
+          name: 'Mariano',
+          id: '1',
+          gardenID: 'Grow this',
+          eventID: '5',
+          title: 'This is a title',
+          date: 'date',
+          description: 'This is a description',
+          volunteersneeded: '5',
+          verb: '',
+          adjective: '',
+          noun: '',
+          currentDayofWeek: ''
+        },
+        subject: 'Hello, World!'
+      }
+    ],
+    from: {
+      email: 'zeppamariano@gmail.com',
+      name: 'John Doe'
+    },
+    reply_to: {
+      email: 'noreply@johndoe.com',
+      name: 'John Doe'
+    },
+    template_id: 'd-78cba85ace40431b84f04f35d9f51f8d'
+  }))
+  req.end()
+}
