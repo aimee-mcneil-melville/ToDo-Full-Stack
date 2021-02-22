cc45a4b92eefd1ffcbc249c5b7806a278deb45f7
Don Smith <don@devacademy.co.nz>
2021-02-22 16:09:19 +1300

    fix linting errors

--- resources/z_resetSequences.js
+++ resources/z_resetSequences.js
@@ -1,9 +1,11 @@
 exports.seed = (knex) => {
   // Reset the sequences in postgres
-  if (knex.client.config.client == 'postgresql') {
-    return knex('pg_class').where('relkind', 'S').not.where('relname', 'LIKE', '%migration%').select('relname').debug()
-    .then(sequences => {
-      return Promise.all(sequences.map(sequenceObj => {
+  if (knex.client.config.client === 'postgresql') {
+    return knex('pg_class')
+      .where('relkind', 'S')
+      .not.where('relname', 'LIKE', '%migration%')
+      .select('relname').debug()
+      .then(sequences => sequences.map(sequenceObj => {
         console.log(sequenceObj)
         const sequence = sequenceObj.relname
         const table = sequence.replace('_id_seq', '')
@@ -11,10 +13,11 @@ exports.seed = (knex) => {
         console.log(`select setval('${sequence}', (select max(id) from ${table}) + 1);`)
         return knex.raw(`select setval('${sequence}', (select max(id) from ${table}) + 1)`)
       }))
+      .then(Promise.all)
       .then(results => {
         console.log(results)
+        return null
       })
-    })
   }
 }
 
