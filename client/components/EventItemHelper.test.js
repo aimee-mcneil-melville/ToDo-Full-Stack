6cfa4e5bd92daf6d66840ffe1d92ac92607fd439
steve-kim-96 <soungku@hotmail.com>
2021-02-23 13:43:43 +1300

    Update first test for eventItemHelper

--- client/components/EventItemHelper.test.js
+++ client/components/EventItemHelper.test.js
@@ -4,16 +4,20 @@ import { getIfVolunteer, toggleVolunteerButton } from './EventItemHelper'
 
 jest.mock('../store')
 
+afterEach(() => {
+  return dispatch.mockClear()
+})
+
 describe('getIfVolunteer', () => {
   it('return true if userId is in the volunteers array', () => {
-    getState.mockImplementation((storeState) => storeState.user.id: 2)
+    getState.mockImplementation(() => ({ user: { id: 2 } }))
     const volunteers = [
       { userId: 2, eventId: 2 },
       { userId: 3, eventId: 1 }
     ]
-    getIfVolunteer(volunteers)
-    expect(getState).toHaveBeenCalled
-
+    const test = getIfVolunteer(volunteers)
+    expect(getState).toHaveBeenCalled()
+    expect(test).toBe(true)
   })
 })
 
