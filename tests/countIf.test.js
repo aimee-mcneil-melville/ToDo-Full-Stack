01079c330ad455e6454f0c5d648d1242c05f35af
Kelly Keating <keating.kel@gmail.com>
2021-03-16 16:09:18 +1300

    simplify test

--- tests/countIf.test.js
+++ tests/countIf.test.js
@@ -2,25 +2,23 @@ const countIf = require('../utilities').countIf
 
 const mixedArray = [1, '21', null, Date.now(), 5, {}, 42]
 
-function isType (type) {
-  // eslint-disable-next-line valid-typeof
-  return (thing) => typeof thing === type
-}
-
 test('countIf can count the numbers in an array', () => {
+  const checkerFunc = (thing) => typeof thing === 'number'
   const expected = 4
-  const actual = countIf(mixedArray, isType('number'))
+  const actual = countIf(mixedArray, checkerFunc)
   expect(actual).toBe(expected)
 })
 
 test('countIf can count the strings in an array', () => {
+  const checkerFunc = (thing) => typeof thing === 'string'
   const expected = 1
-  const actual = countIf(mixedArray, isType('string'))
+  const actual = countIf(mixedArray, checkerFunc)
   expect(actual).toBe(expected)
 })
 
 test('countIf can count the objects in an array', () => {
+  const checkerFunc = (thing) => typeof thing === 'object'
   const expected = 2
-  const actual = countIf(mixedArray, isType('object'))
+  const actual = countIf(mixedArray, checkerFunc)
   expect(actual).toBe(expected)
 })
