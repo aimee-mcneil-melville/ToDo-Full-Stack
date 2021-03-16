99813d1a6b6328e84238ff4acabe4000bb47a715
emily coco parkes <emilycoco@me.com>
2021-03-16 17:11:11 +1300

    simplify splitStringByCommas

--- tests/splitStringByCommas.test.js
+++ tests/splitStringByCommas.test.js
@@ -6,9 +6,16 @@ const filterStringsWithCommas = require('../utilities').filterStringsWithCommas
 const randomStrings = require('../data/random-strings')
 const arrayOfArrays = require('../data/array-of-arrays')
 
-test('the generated array of array of strings matches the expected array', () => {
+test('splitStringByCommas will split a string at each comma', () => {
+  const expected = ['hello', 'world']
+  const actual = splitStringByCommas('hello, world')
+  expect(actual).toEqual(expected)
+})
+
+test('splitStringByCommas will split all strings in the arrayOfArrays, at each comma', () => {
   const stringsWithCommas = filter(randomStrings, filterStringsWithCommas) || []
   const mappedArray = map(stringsWithCommas, splitStringByCommas) || []
+  
   const arraysMatch = mappedArray.length && mappedArray.every((arr, i) => {
     return arr.every((str, j) => {
       return str === arrayOfArrays[i][j]
