a06571db0cca62ab709ca5b26f3620d8cf8cb653
Karel Fourie <karelmakazi@gmail.com>
2021-03-22 14:32:50 +1300

    Add design details and files

---
+++ public/resources/fonts.js
@@ -0,0 +1,6 @@
+export default {
+  mainHeadingFont: "RubberNippleFactory",
+  itemHeadingFont: "@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap');",
+  itemHeadingWeight: 700,
+  itemBodyWeight: 400
+}
---
+++ public/resources/pallette.js
@@ -0,0 +1,7 @@
+export default {
+  deepRed: '#BA3F37',
+  midRed: '#E54B4A',
+  borderPeach: '#F3B997',
+  h1Teal: '#465768',
+  h2Teal: '#4D7883'
+}
