9cb9a36a2aea7f1c504b972c594001e54a86a7cc
Don Smith <hi@dons.dev>
2021-03-24 17:19:28 +1300

    wip: add styling

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia
+    },
+    fontFamily: {
+      sans: ['"Roboto Slab"', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    }
+  }
+}
+// deepRed: '#BA3F37',
+// midRed: '#E54B4A',
+// borderPeach: '#F3B997',
+// h1Teal: '#465768',
+// h2Teal: '#4D7883'
