1f8b7d80d0f2a95a3f4204c5118d1ab3ff780fcc
Don Smith <hi@dons.dev>
2021-03-27 12:29:30 +1300

    remove unneeded resources

--- public/resources/fonts.js
+++
@@ -1,6 +0,0 @@
-export default {
-  mainHeadingFont: "RubberNippleFactory",
-  itemHeadingFont: "@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap');",
-  itemHeadingWeight: 700,
-  itemBodyWeight: 400
-}
