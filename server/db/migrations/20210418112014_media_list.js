ddb73be1f0378c327567a88b09cd8e76961371da
Jared Pinfold <77950295+jared-pinfold@users.noreply.github.com>
2021-04-19 17:58:49 +1200

    update db structure to remove media type

--- server/db/migrations/20210418112014_media_list.js
+++ server/db/migrations/20210418112014_media_list.js
@@ -2,7 +2,6 @@ exports.up = (knex) => {
   return knex.schema.createTable('media_list', (table) => {
     table.increments('id').primary()
     table.integer('user_id').references('users.id')
-    table.string('type')
     table.string('genre')
     table.string('media_name')
     table.string('artist')
