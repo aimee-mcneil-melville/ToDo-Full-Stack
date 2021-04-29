fb247064169da44c52e4cd7a5ec0f9cad8ee304b
Jared Pinfold <77950295+jared-pinfold@users.noreply.github.com>
2021-04-29 16:30:26 +1200

    re-write router.get garden by id inc middleware function etc

--- server/routes/gardens.js
+++ server/routes/gardens.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const { getTokenDecoder } = require('authenticare/server')
 
 const log = require('../logger')
 const db = require('../db/gardens')
+const dbUsers = require('../db/users')
 
 const router = express.Router()
 
@@ -22,18 +24,28 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', getTokenDecoder(), async (req, res) => {
   const id = Number(req.params.id)
-  db.getGardenById(id)
-    .then((singleGarden) => {
-      return res.json(singleGarden)
-    })
-    .catch((err) => {
-      log(err.message)
-      res.status(500).json({
-        error: {
-          title: 'Unable to retrieve garden'
-        }
-      })
+  const userName = req.user.username
+  dbUsers.getUserByName(userName)
+    .then((user) => {
+      db.getGardenById(id)
+        .then((singleGarden) => {
+          singleGarden.events.map(event => {
+            if (user.is_admin === false) {
+              event.isVolunteer = event.volunteers.some((v) => v.username === userName)
+              delete event.volunteers
+            }
+          })
+          return res.json(singleGarden)
+        })
+        .catch((err) => {
+          log(err.message)
+          res.status(500).json({
+            error: {
+              title: 'Unable to retrieve garden'
+            }
+          })
+        })
     })
 })
