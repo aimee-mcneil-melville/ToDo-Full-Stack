9f54d43adece3268a81587b6a1a5808f3d5814c1
grace-malae <grace.t.malae@gmail.com>
2021-04-29 16:07:40 +1200

    accessing dummy data from obj

--- client/App.jsx
+++ client/App.jsx
@@ -14,57 +14,64 @@ import EditEvent from './pages/admin/EditEvent'
 import Error from './components/Error'
 import WaitIndicator from './components/WaitIndicator'
 
+import Event from './pages/Event'
+
 export default function App () {
   const isAdmin = useSelector(globalState => globalState.user.isAdmin)
 
   return (
-    <main className='container p-3'>
-      <Error />
-      <Header />
-      <WaitIndicator />
-      <section className='columns'>
-        <Route exact path='/' component={Home} />
-        <Route
-          path='/register'
-          render={() => {
-            return isAuthenticated()
-              ? <Redirect to='/garden' />
-              : <Register />
-          }}
-        />
-        <Route
-          path='/signin'
-          render={() => {
-            return isAuthenticated()
-              ? <Redirect to='/garden' />
-              : <SignIn />
-          }}
-        />
-        <Route
-          path='/garden'
-          render={() => {
-            return isAuthenticated()
-              ? <Garden />
-              : <Redirect to='/signin' />
-          }}
-        />
-        <Route
-          path='/events/new'
-          render={() => {
-            return isAdmin
-              ? <AddEvent />
-              : <Redirect to='/garden' />
-          }}
-        />
-        <Route
-          path='/events/:id/edit'
-          render={() => {
-            return isAdmin
-              ? <EditEvent />
-              : <Redirect to='/garden' />
-          }}
-        />
-      </section>
-    </main>
+    <>
+      <main className='container p-3'>
+        <Error />
+        <Header />
+        <WaitIndicator />
+        <section className='columns'>
+          <Route exact path='/' component={Home} />
+          <Route
+            path='/register'
+            render={() => {
+              return isAuthenticated()
+                ? <Redirect to='/garden' />
+                : <Register />
+            }}
+          />
+          <Route
+            path='/signin'
+            render={() => {
+              return isAuthenticated()
+                ? <Redirect to='/garden' />
+                : <SignIn />
+            }}
+          />
+          <Route
+            path='/garden'
+            render={() => {
+              return isAuthenticated()
+                ? <Garden />
+                : <Redirect to='/signin' />
+            }}
+          />
+          <Route
+            path='/events/new'
+            render={() => {
+              return isAdmin
+                ? <AddEvent />
+                : <Redirect to='/garden' />
+            }}
+          />
+          <Route
+            path='/events/:id/edit'
+            render={() => {
+              return isAdmin
+                ? <EditEvent />
+                : <Redirect to='/garden' />
+            }}
+          />
+        </section>
+      </main>
+      <br/>
+      <br/>
+      <Event />
+    </>
   )
 }
--- client/pages/Event.jsx
+++ client/pages/Event.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 function Event () {
+  const details = {
+    title: 'Wednesday Weeding',
+    gardenName: 'Kelmarna Gardens',
+    gardenAddress: '12 Hukanui Crescent, Ponsonby, Auckland 1021',
+    date: '29th April 2021',
+    volunteers: 'Volunteers Needed: 8',
+    description: 'This is a really cool description of this really cool event coming up.'
+  }
+
   return (
     <>
-      <h1>EVENT TITLE</h1>
-      <h2>Garden Name</h2>
-      <h3>date + time</h3>
-      <h3>volunteers needed: 8</h3>
-      <p>This is a really cool description of this really cool event coming up.</p>
+      <h1>{details.title}</h1>
+      <h2>{details.gardenName}</h2>
+      <h2>{details.gardenAddress}</h2>
+      <h3>{details.date}</h3>
+      <h3>{details.volunteers}</h3>
+      <p>{details.description}</p>
     </>
   )
 }
