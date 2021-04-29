da99908ef42e082b26a98dfe4af747ce440818ef
Lorena Costa <loraphaela@gmail.com>
2021-04-30 11:44:03 +1200

    add checkVolunteerStatus function to eventHelper

--- client/pages/eventHelper.js
+++ client/pages/eventHelper.js
@@ -17,3 +17,27 @@ export function getEvent (consume = requestor) {
       dispatch(showError(error.message))
     })
 }
+
+export function checkVolunteerStatus (eventId, isVolunteer, consume = requestor) {
+  const storeState = getState()
+  const { id } = storeState.user
+  if (!id) {
+    dispatch(showError('Please register or sign in to volunteer.'))
+    return Promise.resolve(false)
+  }
+  else {
+    dispatch(setWaiting())
+    const routeMethod = isVolunteer ? 'delete' : 'post'
+    const userData = { userId: id, eventId }
+  
+    return consume('/volunteer', routeMethod, userData)
+      .then(() => {
+        dispatch(clearWaiting())
+        return true
+      })
+      .catch((error) => {
+        dispatch(showError(error.message))
+        return false
+      })
+  }
+}
