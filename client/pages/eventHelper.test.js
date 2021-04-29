adda463a48ead4f10de6e4371fdc5f5894fad053
Sarah Smith <77254910+sarah-smith-nz@users.noreply.github.com>
2021-04-30 11:21:10 +1200

    Creates tests for eventHelper

--- client/pages/eventHelper.test.js
+++ client/pages/eventHelper.test.js
@@ -9,57 +9,48 @@ afterEach(() => {
   return jest.resetAllMocks()
 })
 
-// describe('getGarden', () => {
-//   describe('-> GET /gardens/:id api call success', () => {
-//     it('dispatches with the correct garden action', () => {
-//       getState.mockImplementation(() => ({ user: { gardenId: 2 } }))
-//       function consume (path) {
-//         expect(path).toMatch('2')
-//         return Promise.resolve({
-//           body: {
-//             name: 'test garden',
-//             description: 'a rad test garden',
-//             url: 'cooltestgarden.com',
-//             events: [],
-//             address: 'cool place, nz',
-//             lat: 123,
-//             lon: -123,
-//             fake: 'asdf'
-//           }
-//         })
-//       }
+describe('getEvent', () => {
+  describe('-> GET /events/:id api call success', () => {
+    it('dispatches with the correct event action', () => {
+      getState.mockImplementation(() => ({ event: { id: 2 } }))
+      function consume (path) {
+        expect(path).toMatch('2')
+        return Promise.resolve({
+          body: {
+            gardenId: 1,
+            title: 'test event',
+            date: '2021-04-30',
+            volunteersNeeded: 3,
+            description: 'wow great description',
+            fake: 'asdf'
+          }
+        })
+      }
 
-//       return getGarden(consume)
-//         .then((garden) => {
-//           expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
-//           expect(dispatch).toHaveBeenCalledWith({
-//             type: SET_GARDEN,
-//             garden: {
-//               name: 'test garden',
-//               description: 'a rad test garden',
-//               url: 'cooltestgarden.com',
-//               events: [],
-//               address: 'cool place, nz',
-//               lat: 123,
-//               lon: -123
-//             }
-//           })
-//           return null
-//         })
-//     })
-//   })
+      return getEvent(consume)
+        .then((event) => {
+          expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+          expect(dispatch).toHaveBeenCalledWith({
+            type: CLEAR_WAITING
+          })
+          expect(event.title).toBe('test event')
+          expect(event).not.toHaveProperty('fake')
+          return null
+        })
+    })
+  })
 
-//   describe('-> GET /gardens/:id api call rejection', () => {
-//     it('dispatches error correctly', () => {
-//       getState.mockImplementation(() => ({ user: { gardenId: null } }))
-//       function consume () {
-//         return Promise.reject(new Error('mock error'))
-//       }
-//       return getGarden(consume)
-//         .then(() => {
-//           expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
-//           return null
-//         })
-//     })
-//   })
-// })
+  describe('-> GET /event/:id api call rejection', () => {
+    it('dispatches error correctly', () => {
+      getState.mockImplementation(() => ({ event: { id: null } }))
+      function consume () {
+        return Promise.reject(new Error('mock error'))
+      }
+      return getEvent(consume)
+        .then(() => {
+          expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
+          return null
+        })
+    })
+  })
+})
