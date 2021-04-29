abb2b38d2e0fe6b86623bdcfdce656de83624a4b
grace-malae <grace.t.malae@gmail.com>
2021-04-29 15:46:10 +1200

    created component for specific event

---
+++ client/pages/Event.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Event () {
+  return (
+    <>
+      <h1>EVENT TITLE</h1>
+      <h2>Garden Name</h2>
+      <h3>date + time</h3>
+      <h3>volunteers needed: 8</h3>
+      <p>This is a really cool description of this really cool event coming up.</p>
+    </>
+  )
+}
+
+export default Event
