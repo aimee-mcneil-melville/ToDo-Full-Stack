117075253fd6d30754595ae594cfaaf66cb551c5
Rebecca Hallas <77651862+rebecca-hallas@users.noreply.github.com>
2021-04-30 15:50:45 +1200

    complete checkVolunteerStatus tests

--- client/pages/eventHelper.test.js
+++ client/pages/eventHelper.test.js
@@ -1,4 +1,5 @@
-import { getEvent } from './eventHelper'
+import requestor from '../consume'
+import { checkVolunteerStatus } from './eventHelper'
 import { SET_WAITING, CLEAR_WAITING } from '../actions/waiting'
 import { dispatch, getState } from '../store'
 // NOTE! Not written for eventHelper - must edit
@@ -60,6 +61,95 @@ afterEach(() => {
 //           expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
 //           return null
 //         })
-//     })
-//   })
-// })
+//     })eerStatus', (consume = requestor) => {
+  
+  it('dispatches post', () => {
+    getState.mockImplementation(() => ({ user: { id: 2 } }))
+    const eventId = 1
+    const isVolunteer = false
+
+    function consume (url, method, userData) {
+      expect(method).toBe('post')
+      expect(userData.userId).toBe(2)
+      return Promise.resolve()
+    }
+    return checkVolunteerStatus(eventId, isVolunteer, consume)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+        return null
+      })
+  })
+
+describe('checkVolunteerStatus', (consume = requestor) => {
+  it('dispatches post', () => {
+    getState.mockImplementation(() => ({ user: { id: 2 } }))
+    const eventId = 1
+    const isVolunteer = false
+
+    function consume (url, method, userData) {
+      expect(method).toBe('post')
+      expect(userData.userId).toBe(2)
+      return Promise.resolve()
+    }
+    return checkVolunteerStatus(eventId, isVolunteer, consume)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+        return null
+      })
+  })
+
+  it('dispatches delete', () => {
+    getState.mockImplementation(() => ({ user: { id: 4 } }))
+    const eventId = 3
+    const isVolunteer = true
+
+    function consume (url, method, userData) {
+      expect(method).toBe('delete')
+      expect(userData.userId).toBe(4)
+      return Promise.resolve()
+    }
+    return checkVolunteerStatus(eventId, isVolunteer, consume)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({ type: CLEAR_WAITING })
+        return null
+      })
+  })
+
+  it('dispatches waiting actions correctly', () => {
+    getState.mockImplementation(() => ({ user: { id: 4 } }))
+
+    function consume () {
+      return Promise.resolve()
+    }
+    return checkVolunteerStatus(null, null, consume)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+        expect(dispatch).toHaveBeenCalledWith({ type: CLEAR_WAITING })
+        return null
+      })
+  })
+
+  it('dispatches error correctly', () => {
+    getState.mockImplementation(() => ({ user: { id: 1 } }))
+    function consume () {
+      return Promise.reject(new Error('mock error'))
+    }
+    return checkVolunteerStatus(null, null, consume)
+      .then(() => {
+        console.log(dispatch)
+        expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
+        return null
+      })
+  })
+
+  it('shows error if no user id', () => {
+    getState.mockImplementation(() => ({ user: {id: null}}))
+
+    return checkVolunteerStatus(consume)
+    .then(() => {
+      expect(dispatch.mock.calls[0][0].errorMessage).toMatch('Please register or sign in to volunteer.')
+      return null
+    })
+  })
+})
+
