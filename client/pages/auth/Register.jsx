e4b82a28eff6b1994d8da26f43953e129a017119
Sina Kakhki <78020532+sina-kakhki@users.noreply.github.com>
2021-04-30 15:21:15 +1200

    pushing branch

--- client/pages/auth/Register.jsx
+++ client/pages/auth/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-
+.
 import { registerUser } from './registerHelper'
 
 export default function Register (props) {
