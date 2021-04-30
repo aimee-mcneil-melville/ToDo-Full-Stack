c7a2019a58403012c5a2bc0f99ed1d9fbf30cfd2
Jonathan-Zhou <jonathan.zhou1688@gmail.com>
2021-04-30 14:14:31 +1200

    event helper updated for sarah and sam

--- client/pages/eventHelper.js
+++ client/pages/eventHelper.js
@@ -3,15 +3,15 @@ import { dispatch, getState } from '../store'
 import { setWaiting, clearWaiting } from '../actions/waiting'
 import { showError } from '../actions/error'
 
-export function getEvent (consume = requestor) {
-  const storeState = getState()
-  const { id } = storeState.event
+export function getEvent (id, consume = requestor) {
+  // const storeState = getState()
+  // const { id } = storeState.event
   dispatch(setWaiting())
   return consume(`/events/${id}`)
     .then((res) => {
-      const { gardenId, title, date, volunteersNeeded, description } = res.body
       dispatch(clearWaiting())
-      return { gardenId, title, date, volunteersNeeded, description }
+      const { title, gardenName, date, volunteersNeeded, description } = res.body
+      return { title, gardenName, date, volunteersNeeded, description }
     })
     .catch((error) => {
       dispatch(showError(error.message))
