87635e0581a2fbd3473a33ebc14503bccf585cda
yuan tan <dyxtan@gmail.com>
2021-05-03 11:22:07 +1200

    add e2e test for clicking on register link

--- tests/e2e/home.test.js
+++ tests/e2e/home.test.js
@@ -11,5 +11,12 @@ afterAll(async () => {
 test('Home page should display the text Gardenz', async () => {
   const page = await browser.newPage()
   await page.goto('localhost:3000')
-  expect(await page.title()).toMatch('knob')
+  expect(await page.title()).toMatch('Gardenz')
+})
+
+test('Clicking register on home goes to /register', async () => {
+  const page = await browser.newPage()
+  await page.goto('localhost:3000')
+  await page.click('text=register')
+  expect(await page.url()).toBe('http://localhost:3000/register')
 })
