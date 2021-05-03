1c01cc381b69f721681fd0e3c5aa81e5f7fbc4f9
Kateti Mareko <katetimareko@codenertapps.com>
2021-05-03 15:24:40 +1200

    Add backend functionality to support setting the value for if the event is added or not

--- server/routes/volunteer.js
+++ server/routes/volunteer.js
@@ -3,6 +3,7 @@ const express = require('express')
 const log = require('../logger')
 const db = require('../db/volunteers')
 const { decode } = require('../notifications/emailTokens')
+/* const { getTokenDecoder } = require('../auth') */
 
 const router = express.Router()
 
@@ -54,4 +55,32 @@ router.delete('/', (req, res) => {
         }
       })
     })
+})
+
+router.put('/', /* getTokenDecoder(), */ (req, res) => {
+  if (!req.user.isAdmin) {
+    res.status(401).json({
+      error: {
+        title: 'Unauthorized'
+      }
+    })
+    return
+  }
+
+  const { isAttended, userId, eventId } = req.body
+  db.attend({
+    isAttended,
+    userId,
+    eventId
+  }).then(() => {
+    res.sendStatus(200)
+    return null
+  }).catch(err => {
+    log(err.message)
+    res.status(500).json({
+      error: {
+        title: 'Unable to set is attend to this event'
+      }
+    })
+  })
 })
