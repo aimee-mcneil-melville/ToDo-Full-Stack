19d78c936ec5368e3b827f456ac2d8d6ce0a30fd
Alexander McKerrow <mckerrow4@gmail.com>
2021-05-03 12:02:41 +1200

    can redirect getstarted and register user

--- tests/e2e/home.test.js
+++ tests/e2e/home.test.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright')
 
 let browser
 beforeAll(async () => {
-  browser = await chromium.launch()
+  browser = await chromium.launch({headless:false, slowMo:500})
 })
 afterAll(async () => {
   await browser.close()
@@ -19,4 +19,25 @@ test('Clicking register on home goes to /register', async () => {
   await page.goto('localhost:3000')
   await page.click('text=register')
   expect(await page.url()).toBe('http://localhost:3000/register')
+})
+
+test('Clicking get started when not logged in redirects to login', async () => {
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto('localhost:3000')
+  await page.click('text=Get Started')
+  expect(await page.url()).toBe('http://localhost:3000/signin')
+})
+
+test('Register fill in redirects to garden after signing up', async () => {
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto('localhost:3000/register')
+  await page.fill('#username', 'Peteraaaaaaaa');
+  await page.fill('#password', 'Peter');
+  await page.fill('#email', 'peteraaaaaaaaa@peteremail.com');
+  await page.selectOption('select#garden', '1');
+  await page.click('button', { force: true });
+  expect(await page.url()).toBe('http://localhost:3000/garden')
+
 })
