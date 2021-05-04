4a23bf7fab0def3552bdb5def1dd25787bb582af
yuan tan <dyxtan@gmail.com>
2021-05-04 15:19:20 +1200

    refactor tests to remove repeating code

--- tests/e2e/end2end.test.js
+++ tests/e2e/end2end.test.js
@@ -5,64 +5,65 @@ const db = require('knex')(config)
 jest.setTimeout(10000)
 
 const imgpath = 'tests/e2e/screenshots/'
+const timeOut = 2000
 
 let browser
+let page
 beforeAll(async () => {
   browser = await chromium.launch({ headless: false, slowMo: 500 })
   await db.migrate.latest({ directory: './server/db/migrations' })
 })
 
 beforeEach(async () => {
+  const context = await browser.newContext()
+  page = await context.newPage()
   await db.seed.run({ directory: './server/db/seeds' })
+})
+
+afterEach(async () => {
+  await page.close()
 })
 
 afterAll(async () => {
   await browser.close()
 })
 
+function waitForAmount (time) {
+  return setTimeout(() => {
+    // Do nothing
+  }, time)
+}
+
 // USER TESTS
 
 test('Home page should display the text Gardenz', async () => {
-  const page = await browser.newPage()
   await page.goto('localhost:3000')
   expect(await page.title()).toMatch('Gardenz')
   await page.screenshot({ path: imgpath + 'home.png' })
-  await page.close()
 })
 
 test('Clicking register on home goes to /register', async () => {
-  const page = await browser.newPage()
   await page.goto('localhost:3000')
   await page.click('text=register')
   expect(await page.url()).toBe('http://localhost:3000/register')
   await page.screenshot({ path: imgpath + 'registerpage.png' })
-  await page.close()
 })
 
 test('Clicking get started when not logged in redirects to login', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Get Started')
   expect(await page.url()).toBe('http://localhost:3000/signin')
   await page.screenshot({ path: imgpath + 'redirect.png' })
-
-  await page.close()
 })
 
 test('Clicking get sign in goes to /signin', async () => {
-  const page = await browser.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
   await page.screenshot({ path: imgpath + 'signinpage.png' })
-
-  await page.close()
 })
 
 test('Register fill in redirects to garden after signing up', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000/register')
   await page.fill('#firstName', 'bob')
   await page.fill('#lastName', 'dylan')
@@ -76,16 +77,11 @@ test('Register fill in redirects to garden after signing up', async () => {
   ])
   expect(await page.url()).toBe('http://localhost:3000/garden')
   await page.screenshot({ path: imgpath + 'registersuccess.png' })
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(page.close())
-    }, 2000)
-  })
+
+  await waitForAmount(timeOut)
 })
 
 test('Login', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -94,12 +90,9 @@ test('Login', async () => {
   await page.click('button', { force: true })
   await page.screenshot({ path: imgpath + 'signinsuccess.png' })
   expect(await page.url()).toBe('http://localhost:3000/garden')
-  await page.close()
 })
 
 test('Login & Logout', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -110,12 +103,9 @@ test('Login & Logout', async () => {
   await page.click('text=Log out')
   await page.screenshot({ path: imgpath + 'logoutsuccess.png' })
   expect(await page.url()).toBe('http://localhost:3000/')
-  await page.close()
 })
 
 test('Login & volunteer', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -126,15 +116,10 @@ test('Login & volunteer', async () => {
   await page.click('text=Volunteer')
   expect(await page.content()).toMatch('Un-Volunteer')
   await page.screenshot({ path: imgpath + 'volunteersuccess.png' })
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(page.close())
-    }, 2000)
-  })
+  await waitForAmount(timeOut)
 })
+
 test('Login & volunteer then logout, relogin and un-volunteer', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -154,18 +139,12 @@ test('Login & volunteer then logout, relogin and un-volunteer', async () => {
   await page.click('text=Un-Volunteer')
   expect(await page.content()).toMatch('Volunteer')
   await page.screenshot({ path: imgpath + 'un-volunteersuccess.png' })
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(page.close())
-    }, 2000)
-  })
+  await waitForAmount(timeOut)
 })
 
 // ADMIN TESTS
 
 test('Login as admin', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -174,39 +153,30 @@ test('Login as admin', async () => {
   await page.click('button', { force: true })
   expect(await page.url()).toBe('http://localhost:3000/garden')
   await page.screenshot({ path: imgpath + 'adminsigninsuccess.png' })
-  page.close()
 })
 
-test('Admin can login and add event', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
-  await page.goto('localhost:3000')
-  await page.click('text=Sign in')
-  expect(await page.url()).toBe('http://localhost:3000/signin')
-  await page.fill('#username', 'admin')
-  await page.fill('#password', 'admin')
-  await page.click('button', { force: true })
-  expect(await page.url()).toBe('http://localhost:3000/garden')
-  await page.click('text=Add New Event')
-  expect(await page.url()).toBe('http://localhost:3000/events/new')
-  await page.fill('#title', 'Christmas Gardening!')
-  await page.fill('[type=date]', '2021-12-25')
-  await page.fill('[type=number]', '100')
-  await page.fill('#description', "I don't want a lot for Christmas, there is just one thing I need, I don't care about the presents, underneath the Christmas tree, I just want you for my own, more than you could ever know, make my wish come true, all I want for Christmas is you")
-  await page.click('button', { force: true })
-  expect(await page.url()).toBe('http://localhost:3000/garden')
-  await page.screenshot({ path: imgpath + 'eventadded.png', fullPage: true })
-  expect(await page.content()).toMatch('Christmas Gardening!')
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(page.close())
-    }, 2000)
-  })
-})
+// test('Admin can login and add event', async () => {
+//   await page.goto('localhost:3000')
+//   await page.click('text=Sign in')
+//   expect(await page.url()).toBe('http://localhost:3000/signin')
+//   await page.fill('#username', 'admin')
+//   await page.fill('#password', 'admin')
+//   await page.click('button', { force: true })
+//   expect(await page.url()).toBe('http://localhost:3000/garden')
+//   await page.click('text=Add New Event')
+//   expect(await page.url()).toBe('http://localhost:3000/events/new')
+//   await page.fill('#title', 'Christmas Gardening!')
+//   await page.fill('[type=date]', '2021-12-25')
+//   await page.fill('[type=number]', '100')
+//   await page.fill('#description', "I don't want a lot for Christmas, there is just one thing I need, I don't care about the presents, underneath the Christmas tree, I just want you for my own, more than you could ever know, make my wish come true, all I want for Christmas is you")
+//   await page.click('button', { force: true })
+//   expect(await page.url()).toBe('http://localhost:3000/garden')
+//   await page.screenshot({ path: imgpath + 'eventadded.png', fullPage: true })
+//   expect(await page.content()).toMatch('Christmas Gardening!')
+//   await waitForAmount(timeOut)
+// })
 
-test('Admin can login and add event, then edit', async () => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
+test('Admin can login edit event', async () => {
   await page.goto('localhost:3000')
   await page.click('text=Sign in')
   expect(await page.url()).toBe('http://localhost:3000/signin')
@@ -224,9 +194,5 @@ test('Admin can login and add event, then edit', async () => {
   expect(await page.content()).toMatch('Come for a fun day out weeding!')
   await page.screenshot({ path: imgpath + 'eventedited.png', fullPage: true })
 
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(page.close())
-    }, 2000)
-  })
+  await waitForAmount(timeOut)
 })
