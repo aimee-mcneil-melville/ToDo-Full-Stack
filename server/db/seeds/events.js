b8f3f9633612755cf895dc973eca4b5fff1dbc33
1Karol1 <kbajczyk@gmail.com>
2021-05-04 15:55:42 +1200

    seeds updated

--- server/db/seeds/events.js
+++ server/db/seeds/events.js
@@ -56,7 +56,39 @@ exports.seed = (knex) => {
         date: '2020-08-28',
         description: 'Help get out the lovely corns in the ground!.',
         volunteers_needed: 40
+      },
+      {
+        id: 8,
+        garden_id: 2,
+        title: 'Zen Gardens',
+        date: '2021-04-29',
+        description: 'Come and hear a talk on how to set up your own zen garden.',
+        volunteers_needed: 6
+      },
+      {
+        id: 9,
+        garden_id: 2,
+        title: 'Collecting Cucumbers',
+        date: '2020-02-14',
+        description: 'The time is right to pick some cucumbers.',
+        volunteers_needed: 8
+      },
+      {
+        id: 10,
+        garden_id: 3,
+        title: 'Wicked Wicked Weeds',
+        date: '2021-05-07',
+        description: 'Those pesky weeds are back. Come and help us keep our garden beautiful',
+        volunteers_needed: 8
+      },
+      {
+        id: 11,
+        garden_id: 3,
+        title: 'Giant Pumpkins',
+        date: '2021-02-27',
+        description: 'There is something in the soil here, these pumpkins are massive.',
+        volunteers_needed: 8
       }
-    ])
+      ])
     })
 }
