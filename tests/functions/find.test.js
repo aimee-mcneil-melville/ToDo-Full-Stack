07f53fa26e2f11e92353fd339c6707de0cb87985
Don Smith <hi@dons.dev>
2021-05-26 12:07:11 +1200

    fix lint errors

--- tests/functions/find.test.js
+++ tests/functions/find.test.js
@@ -4,7 +4,7 @@ const functions = require('../../functions')
 test('functions.find takes an array as the first parameter, a function as the second parameter, and returns the first single element for which the function returns true', function () {
   const expected = 1
 
-  function isOdd(n) {
+  function isOdd (n) {
     return n % 2 !== 0
   }
   const actual = functions.find([2, 1, 2, 2, 5], isOdd)
--- tests/functions/map.test.js
+++ tests/functions/map.test.js
@@ -3,7 +3,7 @@ const functions = require('../../functions')
 // Write a simple map implementation
 test('functions.map takes an array as the first parameter, a function as the second parameter, and returns the result of calling the function on each array element', function () {
   const expected = [1, 2, 3]
-  function addOne(n) {
+  function addOne (n) {
     return n + 1
   }
   const actual = functions.map([0, 1, 2], addOne)
