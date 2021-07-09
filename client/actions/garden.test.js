91fcb9861b8aabe3c4684a2052fd9d0a600108a9
krjw-00 <kriswd0@gmail.com>
2021-07-09 17:26:25 +1200

    add test for updateEventVols action dispatch

--- client/actions/garden.test.js
+++ client/actions/garden.test.js
@@ -1,9 +1,17 @@
-import { setGarden, SET_GARDEN } from './garden'
+import { setGarden, updateEventVols, SET_GARDEN, UPDATE_EVENT_VOLS } from './garden'
 
 describe('setGarden', () => {
   it('returns the correct action', () => {
     const action = setGarden({ name: 'garden' })
     expect(action.type).toBe(SET_GARDEN)
     expect(action.garden).toEqual({ name: 'garden' })
+  })
+})
+
+describe('updateEventVols', () => {
+  it('returns the correct action', () => {
+    const action = updateEventVols({ eventId: 2 })
+    expect(action.type).toBe(UPDATE_EVENT_VOLS)
+    expect(action.garden).toEqual({ eventId: 2 })
   })
 })
