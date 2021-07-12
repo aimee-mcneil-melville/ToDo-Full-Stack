6f8c873af6749d9ab162a27c606076fff82b8a8f
dylan-toomey <dtoomey322@gmail.com>
2021-07-12 16:33:17 +1200

    Back end Role requests routing users

--- server/auth.js
+++ server/auth.js
@@ -7,3 +7,21 @@
 //   generateHash,
 //   getTokenDecoder
 // }
+
+const request = require('request')
+
+function getUserRoles (uid, token) {
+  var options = {
+    method: 'GET',
+    url: `https://gardenz.au.auth0.com/api/v2/users/${uid}/roles`,
+    headers: { authorization: `bearer ${token}` }
+  }
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error)
+
+    console.log(body)
+  })
+}
+
+module.exports = getUserRoles
