c00c22ddf018e084e904f7be7e27ebc3a7dabfbe
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-07-12 13:59:27 +1200

    fixed logo position and added hover effect on the links

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -7,11 +7,9 @@ import WaitIndicator from '../components/WaitIndicator/WaitIndicator'
 export default function Header () {
   return (
     <header className="header-container">
-      <div className="header-wrapper">
-        <Link className="logo-wrapper" to="/">
-          <img src='/images/gardenzLogo.svg' alt="gardenzlogo" className='logo-image' />
-        </Link>
-      </div>
+      <Link className="logo-wrapper" to="/">
+        <img src='/images/gardenzLogo.svg' alt="gardenzlogo" className='logo-image' />
+      </Link>
       <WaitIndicator />
       <Nav />
     </header>
