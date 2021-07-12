fbb7758a5deea9513e8a1f07b7edca6aa97385ef
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-07-12 17:47:53 +1200

    fix nav menu dropdown  toglling on large screens

--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -18,7 +18,25 @@ export default function Nav () {
 
   return (
     <nav className="navi" onClick={toggleMenu}>
-      <div className={open ? 'navi-toggle' : 'navi-item'}>
+      {open && <div className='navi-toggle'>
+        <IfAuthenticated>
+          <Link to={`/gardens/${user.gardenId}`} className='navi-link'>My Garden</Link>
+          <Link to="/" onClick={logOut} className='navi-link'>
+              Log out
+          </Link>
+          <Link to="/" className='navi-link'>Home</Link>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          {navLinks.map(({ to, name }) => (
+            <Link key={to} to={to} className='navi-link'>
+              {name}
+            </Link>
+          ))}
+        </IfNotAuthenticated>
+        <div className='hamburger' onClick={toggleMenu} ><GiHamburgerMenu/></div>
+      </div>
+      }
+      <div className='navi-item'>
         <IfAuthenticated>
           <Link to={`/gardens/${user.gardenId}`} className='navi-link'>My Garden</Link>
           <Link to="/" onClick={logOut} className='navi-link'>
