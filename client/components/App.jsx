f765b6621e55203b2b3a5f8a30f4074d10d2ab38
kieran-tahir <kierantahir@gmail.com>
2021-09-16 18:10:32 +1200

    add register.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchFriends } from './appHelper'
+import Register from './Register'
+import Login from './Login'
+
+import { Route } from 'react-router-dom'
 
 // temporary until auth is working
 const id = 10001
 
 function App (props) {
+  // function handleRegister ()
+
   useEffect(() => {
     fetchFriends(props.dispatch, id)
   }, [])
@@ -17,6 +23,8 @@ function App (props) {
         {/* <ErrorMessage /> */}
         <h1>rcmndr</h1>
         {/* <WaitIndicator /> */}
+        <Register />
+        {/* <Route path='/Login' component={Login} /> */}
         <p>Friends List:</p>
         <ul>
           {props.friends.map(friend => (
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,1 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { register } from 'authenticare/client'
+
+// import { baseApiUrl as baseUrl } from '../config'
+
+function Register (props) {
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  function handleClick () {
+    console.log('clickhandled')
+    return (
+      <>
+        <h2> Register </h2>
+        <form>
+          <label htmlFor='username'> Username: </label>
+          <input type="text" name="username">
+
+          </input>
+        </form>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <button type='button' onClick={handleClick}>Register</button>
+    </>
+  )
+}
+
+export default Register
