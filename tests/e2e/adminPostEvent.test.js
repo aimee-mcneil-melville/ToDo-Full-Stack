4b42377a90f3ee7bf313ede517a34ae65494c927
AliakbarSu <aliakbar.su@hotmail.com>
2021-09-17 10:46:14 +1200

    Able to signin

--- tests/e2e/adminPostEvent.test.js
+++ tests/e2e/adminPostEvent.test.js
@@ -8,7 +8,7 @@ jest.setTimeout(20000)
 let browser
 let page
 beforeAll(async () => {
-  browser = await chromium.launch({ headless: true, slowMo: 500 })
+  browser = await chromium.launch({ headless: false, slowMo: 500 })
   await db.migrate.latest({ directory: './server/db/migrations' })
 })
 
@@ -29,21 +29,23 @@ afterAll(async () => {
 
 // Test goes here
 test('Admin can login & add event', async () => {
+  // going to localhost:3000
   await page.goto(serverUrl)
 
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('text=Sign in')
-  ])
+  // clicking on sign In
+  await Promise.all([page.waitForNavigation(), page.click('text=Sign in')])
 
-  expect(await page.url()).toBe(`${serverUrl}/signin`)
+  // checking if the url changes to /signin
+  expect(await page.url()).toContain(
+    'https://gardenz.au.auth0.com/u/login?state='
+  )
 
   await page.fill('#username', 'admin')
   await page.fill('#password', 'admin')
 
   await Promise.all([
     page.waitForNavigation(),
-    page.click('button', { force: true })
+    page.click('.c132a5a03', { force: true })
   ])
 
   expect(await page.content()).toMatch('Log out')
@@ -58,7 +60,10 @@ test('Admin can login & add event', async () => {
   await page.fill('#title', 'Christmas Gardening!')
   await page.fill('[type=date]', '2021-12-25')
   await page.fill('[type=number]', '100')
-  await page.fill('#description', "I don't want a lot for Christmas, there is just one thing I need, I don't care about the presents, underneath the Christmas tree, I just want you for my own, more than you could ever know, make my wish come true, all I want for Christmas is you")
+  await page.fill(
+    '#description',
+    "I don't want a lot for Christmas, there is just one thing I need, I don't care about the presents, underneath the Christmas tree, I just want you for my own, more than you could ever know, make my wish come true, all I want for Christmas is you"
+  )
 
   expect(await page.innerText('.box .title')).toBe('Christmas Gardening!')
 
