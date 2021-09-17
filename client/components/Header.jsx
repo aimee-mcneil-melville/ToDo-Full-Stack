9eaa6dc5621da4a25679804933343a426b0f8fbb
caleb-perrett <caleb@iamdigital.co.nz>
2021-09-17 14:34:44 +1200

    Refactor nav and home

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,8 +6,8 @@ import WaitIndicator from '../components/WaitIndicator/WaitIndicator'
 
 export default function Header () {
   return (
-    <header className="container">
-      <div className="flex-container">
+    <header className="header">
+      <div className="flex-container container">
         <Link to="/">
           <div className="logo-container">
             <img src='/images/gardenzLogo.svg' alt="gardenzlogo" className='logo-image' />
