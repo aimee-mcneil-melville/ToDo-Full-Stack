d70240737fa89d57a754119dfb310da39e71b246
csiyang <csiyang@yahoo.co.nz>
2021-09-17 15:46:25 +1200

    validation inital complete- date formated

--- client/components/events/EventForm/EventForm.jsx
+++ client/components/events/EventForm/EventForm.jsx
@@ -22,10 +22,16 @@ export default function EventForm (props) {
       description: ''
     },
     onSubmit: values => {
-      console.log(values)
+      props.submitEvent({ ...values, date: dateFormater(values.date) })
     },
     validationSchema: eventSchema
   })
+
+  function dateFormater (date) {
+    // input: yyyy-MM-dd
+    // output: dd-MM-yyyy
+    return `${date[8]}${date[9]}/${date[5]}${date[6]}/${date[0]}${date[1]}${date[2]}${date[3]}`
+  }
 
   return (
     <>
