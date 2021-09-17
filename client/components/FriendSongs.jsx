a19027b4e457acddb64b107734da4353f1270c8b
austin <austin.weng@yahoo.co.nz>
2021-09-17 15:42:16 +1200

    add friendSongs

--- client/components/FriendSongs.jsx
+++ client/components/FriendSongs.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+// import getUserMedia from '../actions'
 
-function FriendSongs () {
+function FriendSongs (props) {
+  // const {nickname}=props.friend
+  const nickname = 'Goku'
+
+  useEffect(() => {
+    // props.dispatch(getUserMedia())
+  }, [])
+
   return (
-    <p></p>
+    <>
+      <h1>{nickname}</h1>
+      <p>click the heart to follow/unfollow</p>
+      {!props.friendSongs.length &&
+      (<p>friend hasn’t added any songs yet</p>)}
+      {props.friendSongs.map(media => {
+        const { id, mediaName, artist, link } = media
+        return (
+          <li key={id}>
+            {link}
+            {mediaName}
+            {artist}
+          </li>
+        )
+      })}
+    </>
   )
 }
 
-export default FriendSongs
+function mapStateToProps (globalState) {
+  return {
+    friendSongs: globalState.friendSongs
+  }
+}
+
+export default connect(mapStateToProps)(FriendSongs)
