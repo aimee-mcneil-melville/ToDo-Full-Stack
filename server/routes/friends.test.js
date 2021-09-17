9ea75b6d34baf95354a994ec8fe511029f9e1526
alexarlington <alexsarlington@gmail.com>
2021-09-17 15:36:30 +1200

    friends GET all friends route test completed and passing

--- server/routes/friends.test.js
+++ server/routes/friends.test.js
@@ -1,25 +1,21 @@
 const request = require('supertest')
-
 const server = require('../server')
 const db = require('../db/friends')
-const { test } = require('../db/knexfile')
-
 jest.mock('../db/friends')
 
 test('GET /user_id return all friends', () => {
   db.getFriends.mockImplementation(() => {
     return Promise.resolve([
-      { id: 1, name: 'test name 1', lastName: 'test lastName 1', nickname: 'test nickname 1' },
-      { id: 2, name: 'test name 2', lastName: 'test lastName 2', nickname: 'test nickname 2' },
-      { id: 3, name: 'test name 3', lastName: 'test lastName 3', nickname: 'test nickname 3' }
+      { id: 10001, name: 'Jared', lastName: 'Pinfold', nickname: 'Daoloth69' },
+      { id: 10002, name: 'Remington', lastName: 'Smythe', nickname: 'Remmy' },
+      { id: 10003, name: 'Gertrude', lastName: 'Diamond', nickname: 'D1am0nd' }
     ])
   })
   return request(server)
-    .get('/10001')
+    .get('/api/v1/friends/10001')
     .expect(200)
-    // eslint-disable-next-line promise/always-return
-    .then(res => {
-      console.log('res.body: ', res.body)
-      expect(res.body.user_id).toHaveLength(3)
+    .then((response) => {
+      expect(response.body.friends).toHaveLength(3)
+      return null
     })
 })
