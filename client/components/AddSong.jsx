a28e6064a92ce0badfae5296c666e578be5a3f8d
austin <austin.weng@yahoo.co.nz>
2021-09-17 16:45:18 +1200

    add addSong component

--- client/components/AddSong.jsx
+++ client/components/AddSong.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+// import { addSong } from '../actions'
 
-function AddSong () {
+function AddSong (props) {
+  const [songForm, setSongForm] = useState({
+    songTitle: '',
+    artist: '',
+    genre: '',
+    comment: '',
+    link: ''
+  })
+  const { songTitle, artist, genre, comment, link } = songForm
+
+  function handleChangeAddSong (e) {
+    const { name, value } = e.target
+    const newSongForm = {
+      ...songForm,
+      [name]: value
+    }
+    setSongForm(newSongForm)
+  }
+
+  function handleSubmitAddSong () {
+    // props.dispatch(addSong(songForm))
+  }
+
   return (
-    <p></p>
+    <>
+      <h1>Add new song</h1>
+      <p>fill in the details below to add a new song to your list</p>
+      <form onSubmit={handleSubmitAddSong}>
+        <label htmlFor="songTitle">
+          <input type="text"
+            id="songTitle"
+            name="songTitle"
+            value={songTitle}
+            placeholder="The full title of the song"
+            onChange={handleChangeAddSong} />
+        </label>
+        <label htmlFor="artist">
+          <input type="text"
+            id="artist"
+            name="artist"
+            value={artist}
+            placeholder="Name of the artist / singer/ group"
+            onChange={handleChangeAddSong} />
+        </label>
+        <label htmlFor="genre">
+          <input type="text"
+            id="genre"
+            name="genre"
+            value={genre}
+            placeholder="Genre of music (optional)"
+            onChange={handleChangeAddSong} />
+        </label>
+        <label htmlFor="comment">
+          <input type="text"
+            id="comment"
+            name="comment"
+            value={comment}
+            placeholder="Your thoughts on this track (optional)"
+            onChange={handleChangeAddSong} />
+        </label>
+        <label htmlFor="link">
+          <input type="text"
+            id="link"
+            name="link"
+            value={link}
+            placeholder="A link so others can listen (optional)"
+            onChange={handleChangeAddSong} />
+        </label>
+        <button>Save</button>
+      </form>
+    </>
   )
 }
 
-export default AddSong
+export default connect()(AddSong)
