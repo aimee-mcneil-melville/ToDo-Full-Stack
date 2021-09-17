25eb2e4e0192c2a0aa21f9656a1aae81290deb46
Sophia-Lawler-Dormer <sophialawler@gmail.com>
2021-09-17 14:22:11 +1200

    import yup

--- client/components/events/EventForm/EventForm.jsx
+++ client/components/events/EventForm/EventForm.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+// const SignupSchema = Yup.object().shape({
+//   firstName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   lastName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   email: Yup.string().email('Invalid email').required('Required')
+// })
 
 export default function EventForm (props) {
   // function myHandleSubmit (data) {
