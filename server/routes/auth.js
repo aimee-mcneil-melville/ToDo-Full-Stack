acbdd9af1df7c36f1974ed78b8e2796d216b4c2b
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-20 13:48:52 +1200

    move values to env file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,21 +2,13 @@ const jwt = require('express-jwt')
 const jwksRsa = require('jwks-rsa')
 const request = require('superagent')
 
-/*
-* this object holds the required information to acquire a new access token for the purpose of retrieving
-* the user's role using machine-to-machine.
-* The clientid and secret are for the API Explorer Application
-* NOTE: the access token passed to routes from the client would not work, because it has a different audience.
-*/
-const apiExplorer = {
-  domain: 'https://gardenz.au.auth0.com',
-  clientId: '2cxnaycUS9BKgNlesVGlfIGIQZjVxPTu',
-  secret: 'WSWxcWFQNZWgUFn_ojRwYuraFkynxs6yFwu-SHMnl1AvzXt4JE-cfN2y-B5Aw_Pa'
-}
+const domain = process.env.AUTH0_DOMAIN
+const clientId = process.env.AUTH0_API_EXPLORER_CLIENTID
+const secret = process.env.AUTH0_API_EXPLORER_SECRET
 
 const getUserRoles = async (uid) => {
   const accessToken = await getAccessToken()
-  const { body } = await request(`${apiExplorer.domain}/api/v2/users/${uid}/roles`)
+  const { body } = await request(`${domain}/api/v2/users/${uid}/roles`)
     .set({ authorization: `Bearer ${accessToken}` })
 
   return body[0].name
@@ -25,12 +17,12 @@ const getUserRoles = async (uid) => {
 const getAccessToken = async () => {
   const data = {
     grant_type: 'client_credentials',
-    client_id: apiExplorer.clientId,
-    client_secret: apiExplorer.secret,
-    audience: `${apiExplorer.domain}/api/v2/`
+    client_id: clientId,
+    client_secret: secret,
+    audience: `${domain}/api/v2/`
   }
 
-  const { body } = await request.post(`${apiExplorer.domain}/oauth/token`)
+  const { body } = await request.post(`${domain}/oauth/token`)
     .send(data)
     .type('form')
   return body.access_token
@@ -47,12 +39,12 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `${apiExplorer.domain}/.well-known/jwks.json`
+    jwksUri: `${domain}/.well-known/jwks.json`
   }),
 
   // Validate the audience and the issuer.
   audience: 'https://garden/nz/api',
-  issuer: `${apiExplorer.domain}/`,
+  issuer: `${domain}/`,
   algorithms: ['RS256']
 })
 
