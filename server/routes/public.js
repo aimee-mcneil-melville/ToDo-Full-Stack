ff65fd594954e3089b47542f67cd9e61146ec2f1
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-21 07:50:42 +1200

    add a public route

---
+++ server/routes/public.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json({ message: 'I\'m a public endpoint, any one can access it.' })
+})
+
+module.exports = router
