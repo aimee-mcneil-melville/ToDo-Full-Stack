b2c43dd3ca50ef4948cc06c6356570c04e440fef
Zahira Champion <zahira.ali.champion@gmail.com>
2021-09-21 10:44:02 +1200

    create addFriend & deleteFriend client api's

--- client/apis/friends.js
+++ client/apis/friends.js
@@ -8,3 +8,20 @@ export function getFriends (id) {
       return res.body.friends
     })
 }
+
+export function addFriend (userId, followingId) {
+  return request.post(rootUrl + '/friends/' + userId)
+    .send({ userId, followingId })
+    .then(res => {
+      return res.body
+    })
+}
+
+export function deleteFriend (userId, followingId) {
+  return request.delete(rootUrl + '/friend/' + userId)
+    .send({ userId, followingId })
+    .then(() => {
+      console.log('User unfollowed')
+      return null
+    })
+}
