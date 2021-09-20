ca941643618dfd7987cb53b504d466586642597f
james-wilson-21 <treppiejames@hotmail.com>
2021-09-21 11:04:59 +1200

    Add media api functions

--- client/apis/friends.js
+++ client/apis/friends.js
@@ -3,6 +3,7 @@ import request from 'superagent'
 const rootUrl = '/api/v1'
 
 export function getFriends (id) {
+  console.log(id)
   return request.get(rootUrl + '/friends/' + id)
     .then(res => {
       return res.body.friends
