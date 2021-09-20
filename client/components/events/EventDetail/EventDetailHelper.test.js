e8a6de60f7c158b15713a1053606695abdb08617
LouisDVL <louis.lising@outlook.com>
2021-09-20 17:37:29 +1200

    added tests and helper function

--- client/components/events/EventDetail/EventDetailHelper.test.js
+++ client/components/events/EventDetail/EventDetailHelper.test.js
@@ -1,17 +1,42 @@
 import { getEvent } from './EventDetailHelper'
-import { SET_WAITING, CLEAR_WAITING } from '../../../actions/waiting'
-import { dispatch } from '../../../store'
+import { showError } from '../../../actions/error'
 
 jest.mock('../../../store')
+jest.mock('../../../actions/error')
 
 describe('testing for getEvent', () => {
   it('expect to return the data in the correct shape', () => {
-    const requestor = jest.mockImplementation((path) => {
+    const requestor = jest.fn().mockImplementation((path) => {
       return Promise.resolve({
         body: {
-          title
+          title: 'test data',
+          gardenName: 'test garden',
+          gardenAddress: 'test address',
+          date: 'test date',
+          volunteersNeeded: 'test volunteers needed',
+          description: 'test description'
         }
       })
     })
+    return getEvent(1, requestor).then(data => {
+      expect(data).toEqual({
+        title: 'test data',
+        gardenName: 'test garden',
+        gardenAddress: 'test address',
+        date: 'test date',
+        volunteersNeeded: 'test volunteers needed',
+        description: 'test description'
+      })
+      return null
+    })
+  })
+
+  it('should call the function dispatch show error when the promise fails', async () => {
+    expect.assertions(1)
+    const requestor = jest.fn().mockImplementation((path) => {
+      return Promise.reject(new Error('This is a test error'))
+    })
+    await getEvent(1, requestor)
+    expect(showError).toHaveBeenCalled()
   })
 })
