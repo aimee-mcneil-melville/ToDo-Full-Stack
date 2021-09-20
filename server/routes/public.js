a69b08fcf64b2fce938fa57bc75297542a9a09d7
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-21 08:12:05 +1200

    rename

--- server/routes/public.js
+++ server/routes/public.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ message: 'I\'m a public endpoint, any one can access it.' })
+  res.json({ message: 'I\'m a public endpoint, any one can access me.' })
 })
 
 module.exports = router
