22457b30f81b38548af85fc552464edf44e32a0a
Jared Pinfold <j.pinfold@gmail.com>
2021-09-21 12:57:56 +1200

    ready to merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import Auth from './Auth'
-import Nav from './Nav'
+// import Auth from './Auth'
+// import Nav from './Nav'
 import Register from './Register'
 import SignIn from './SignIn'
 import { Route } from 'react-router-dom'
 import AddSong from './AddSong'
 import Friends from './Friends'
-import Media from './Media'
+import Songs from './Songs'
 import UpdateSong from './UpdateSong'
 import FriendSongs from './FriendSongs'
 import Header from './Header'
@@ -19,17 +19,14 @@ function App () {
     <>
       <div className='app'>
         <Header />
-        <Route path='/' component={Nav} />
-        <Route exact path='/' component={Auth}/>
-        <Route path='/friends' component={Friends} />
+        <Route exact path='/friends' component={Friends} />
         <Route path='/friends/:id' component={FriendSongs} />
-        <Route path='/media/:id' component={Media} />
-        <Route path='/media/update/:songId' component={UpdateSong} />
-        <Route path='/media/add' component={AddSong} />
+        <Route path='/songs/add' component={AddSong} />
+        <Route path='/songs/update/:id' component={UpdateSong} />
+        <Route exact path='/songs' component={Songs} />
+        <Route exact path='/' component={Tagline} />
         <Route path='/register' component={Register}/>
         <Route path='/signIn' component={SignIn}/>
-        <Route exact path='/' component={Tagline} />
-
       </div>
     </>
   )
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { logOff } from '../auth-utils'
 import { IfAuthenticated } from './Authenticated'
-const id = 10001
+// const id = 10001
 const inviteCode = '111A11'
 
 function Nav () {
   return (
     <ul className='main-nav'>
-      <li className='main-nav__item'><Link to={`/songs/${id}`}>My tracks</Link></li>
-      <li className='main-nav__item'><Link to='/friends'>My friends</Link></li>
-      <li className='main-nav__item'><Link to='/profile'>Edit my profile</Link></li>
+      <li className='main-nav__item'><Link to='/songs'>My tracks</Link></li>
+      <li className='main-nav__item'><Link to={'/friends'}>My friends</Link></li>
+      <li className='main-nav__item'><Link to={'/profile'}>Edit my profile</Link></li>
       <li className='main-nav__item'>My code: {inviteCode}</li>
       <IfAuthenticated>
         <li className='main-nav__item'><Link to='/' onClick={logOff}>Log Out</Link></li>
