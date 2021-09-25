6a1858da4c48cc8e8841b426b10c22704bd727a0
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-26 12:10:37 +1300

    make events collapsible

--- client/components/events/EventItem/EventItem.jsx
+++ client/components/events/EventItem/EventItem.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import Collapsible from 'react-collapsible'
 import VolunteerButton from '../../volunteers/VolunteerButton/VolunteerButton'
 
 export default function EventItem ({ gardenid, event, isAdmin }) {
+  const [open, setOpen] = useState(false)
   const { id, title, date, volunteersNeeded, totalVolunteers, isVolunteer, status } = event
   const [isVolunteering, setIsVolunteering] = useState(isVolunteer)
   const remainingVolunteers = volunteersNeeded - totalVolunteers
@@ -13,27 +14,34 @@ export default function EventItem ({ gardenid, event, isAdmin }) {
     setIsVolunteering(isVolunteer)
   }, [isVolunteer])
 
+  function handleCollapse () {
+    setOpen(!open)
+  }
+
   return (
-    <article className='card-primary'>
-      <h2 className='card-title'><Link to={`/gardens/${gardenid}/events/${id}`}>{title}</Link></h2>
-      <ul className='list-primary'>
-        <li>{date}</li>
-        {remainingVolunteers > 0
-          ? <li>{remainingVolunteers} of {volunteersNeeded} volunteers still needed</li>
-          : <li>No more volunteers needed, but we can always use more hands! (Currently {additionalVolunteers} extra volunteer{additionalVolunteers !== 1 ? 's' : ''})</li>
-        }
-        <li>Event is {status}</li>
-      </ul>
-      {isAdmin
-        ? <Link to={`/gardens/${gardenid}/events/${id}/edit`}>Edit Event</Link>
-        : <VolunteerButton
-          eventId={id}
-          volunteering={isVolunteering}
-          setVolunteering={setIsVolunteering}
-        />
-      }
-      {/* Does below need to go here */}
-      {/* <Link to={`/events/${id}`}>View Event</Link> */}
-    </article>
+    <div className='Collapsible__contentInner'>
+      <Collapsible trigger={<h2 className='card-title'><a href='#' to={`/gardens/${gardenid}/events/${id}`}>{title}</a></h2>} {...{ open, handleCollapse }}>
+        <article className='card-primary'>
+          <ul className='list-primary'>
+            <li>{date}</li>
+            {remainingVolunteers > 0
+              ? <li>{remainingVolunteers} of {volunteersNeeded} volunteers still needed</li>
+              : <li>No more volunteers needed, but we can always use more hands! (Currently {additionalVolunteers} extra volunteer{additionalVolunteers !== 1 ? 's' : ''})</li>
+            }
+            <li>Event is {status}</li>
+          </ul>
+          {isAdmin
+            ? <Link to={`/gardens/${gardenid}/events/${id}/edit`}>Edit Event</Link>
+            : <VolunteerButton
+              eventId={id}
+              volunteering={isVolunteering}
+              setVolunteering={setIsVolunteering}
+            />
+          }
+          {/* Does below need to go here */}
+          {/* <Link to={`/events/${id}`}>View Event</Link> */}
+        </article>
+      </Collapsible>
+    </div>
   )
 }
