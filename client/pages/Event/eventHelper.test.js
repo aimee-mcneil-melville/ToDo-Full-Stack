7265d86d6fca7946173c1158f87603d74bf7314b
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-27 07:10:19 +1300

    fix test

--- client/pages/Event/eventHelper.test.js
+++ client/pages/Event/eventHelper.test.js
@@ -8,9 +8,18 @@ afterEach(() => {
   return jest.resetAllMocks()
 })
 
+const mockUserAdmin = {
+  isAdmin: true
+}
+
+const mockUserNonAdmin = {
+  isAdmin: false,
+  id: 1
+}
+
 describe('getEvent', () => {
   describe('-> GET /events/:id api call success', () => {
-    it('dispatches with the correct event action', () => {
+    it('dispatches with the correct event action for admin', () => {
       function consume (path) {
         expect(path).toMatch('2')
         return Promise.resolve({
@@ -21,18 +30,53 @@ describe('getEvent', () => {
             date: '2021-04-30',
             volunteersNeeded: 3,
             description: 'wow great description',
+            volunteers: [{}],
+            extraVolunteers: [{}],
             fake: 'asdf'
           }
         })
       }
 
-      return getEvent(2, consume)
+      return getEvent(2, mockUserAdmin, consume)
         .then((event) => {
           expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
           expect(dispatch).toHaveBeenCalledWith({
             type: CLEAR_WAITING
           })
           expect(event.title).toBe('test event')
+          expect(event.volunteers).toHaveLength(1)
+          expect(event.extraVolunteers).toHaveLength(1)
+          expect(event).not.toHaveProperty('fake')
+          return null
+        })
+    })
+    it('dispatches with the correct event action for non admin', () => {
+      function consume (path) {
+        expect(path).toMatch('2')
+        return Promise.resolve({
+          body: {
+            gardenName: 'test name',
+            gardenAddress: 'test address',
+            title: 'test event',
+            date: '2021-04-30',
+            volunteersNeeded: 3,
+            description: 'wow great description',
+            volunteers: [{ userId: mockUserNonAdmin.id }],
+            isVolunteer: true,
+            extraVolunteers: [],
+            fake: 'asdf'
+          }
+        })
+      }
+
+      return getEvent(2, mockUserNonAdmin, consume)
+        .then((event) => {
+          expect(dispatch).toHaveBeenCalledWith({ type: SET_WAITING })
+          expect(dispatch).toHaveBeenCalledWith({
+            type: CLEAR_WAITING
+          })
+          expect(event.title).toBe('test event')
+          expect(event.isVolunteer).toBe(true)
           expect(event).not.toHaveProperty('fake')
           return null
         })
@@ -44,7 +88,7 @@ describe('getEvent', () => {
       function consume () {
         return Promise.reject(new Error('mock error'))
       }
-      return getEvent(null, consume)
+      return getEvent(null, mockUserAdmin, consume)
         .then(() => {
           expect(dispatch.mock.calls[1][0].errorMessage).toBe('mock error')
           return null
