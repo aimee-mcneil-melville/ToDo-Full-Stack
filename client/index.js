fd8726bba16ea1e245c33e03b9524fb20963a10a
ahmad <eng.ahmed.anwar@gmail.com>
2021-09-30 09:40:30 +1300

    add auth0Provider

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Auth0Provider
+      domain={'gardenz.au.auth0.com'}
+      clientId={'UPX21QATLKQlu9iMNfWmZd24Y3Xk2ezF'}
+      redirectUri={window.location.origin}
+      audience={'https://fullstackboilerplate/api'}
+    >
+      <App />
+    </Auth0Provider>,
     document.getElementById('app')
   )
 })
