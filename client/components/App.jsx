d14c864da6d2b114873144bc24dbe7b2f7e908d4
ahmad <eng.ahmed.anwar@gmail.com>
2021-10-02 18:14:47 +1300

    cache user when App component re-renders

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Fruits from './Fruits'
+import { cacheUser } from '../auth0-utils'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App () {
+  cacheUser(useAuth0)
   return (
     <Router>
       <Route path='/' component={Nav} />
---
+++ client/components/Register.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
+import { addUser } from '../api'
+
+import { GridForm, ColOne, ColTwo, Button } from './Styled'
+
+function Register () {
+  const user = useSelector(state => state)
+  const history = useHistory()
+  const [form, setForm] = useState({
+    auth0Id: '',
+    email: ''
+  })
+
+  useEffect(() => {
+    setForm({
+      auth0Id: user?.auth0Id,
+      email: user?.email
+    })
+  }, [user])
+
+  function handleChange (e) {
+    const { name, value } = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  async function handleClick () {
+    await addUser(form, user.token)
+    history.push('/')
+  }
+
+  return (
+    <>
+      <h2>Register</h2>
+      <GridForm>
+        <ColOne htmlFor='username'>Auth0 Id:</ColOne>
+        <ColTwo type='text'
+          id='auth0Id'
+          name='auth0Id'
+          value={form.auth0Id}
+          onChange={handleChange}
+          disabled={true}
+        />
+
+        <ColOne htmlFor='email'>Email:</ColOne>
+        <ColTwo type='text'
+          id='email'
+          name='email'
+          onChange={handleChange}
+          value={form.email}
+          disabled={true}
+        />
+
+        <Button type='button' onClick={handleClick}>Register</Button>
+      </GridForm>
+    </>
+  )
+}
+
+export default Register
