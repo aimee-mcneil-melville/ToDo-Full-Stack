f94fb79cca74a024df59a287534c5aa2564640d5
ahmad <eng.ahmed.anwar@gmail.com>
2021-10-14 10:34:51 +1300

    switch redirect form

--- client/auth-utils.js
+++ client/auth-utils.js
@@ -40,7 +40,7 @@ export function getRegisterFn (useAuth0) {
   const redirectUri = `${window.location.origin}/profile`
   return () => loginWithRedirect({
     redirectUri,
-    screen_hint: 'signup',
+    screen_hint: 'signin',
     scope: 'role:member'
   })
 }
