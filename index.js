5a7540b464245431380da8631768e4c470a0843b
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2021-10-14 18:18:33 +1300

    Correct spelling of refreshInterval

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const nextBoard = require('./nextBoard')
 const displayBoard = require('./displayBoard')
 
 const size = 10
-const refreshInteral = 100
+const refreshInterval = 100
 
 let board = createBoard(size)
 
 setTimeout(() => {
   displayBoard(board)
   board = nextBoard(board)
-}, refreshInteral)
+}, refreshInterval)
