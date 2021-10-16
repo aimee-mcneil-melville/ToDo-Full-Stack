b15e1f6913e09649557940796f7339e9bc02a7ca
ahmad <eng.ahmed.anwar@gmail.com>
2021-10-17 10:35:37 +1300

    animate the home page

--- client/pages/Home/Home.jsx
+++ client/pages/Home/Home.jsx
@@ -1,6 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { getUserLocation, getGardenLocations } from './homeHelper'
+import { motion } from 'framer-motion'
+
+const leftVariant = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0,
+    transition: {
+      ease: 'easeInOut',
+      duration: 1
+    }
+  }
+}
+
+const rightVariant = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      ease: 'easeInOut',
+      duration: 1
+    }
+  }
+}
 
 export default function Home () {
   const [, setUserCoordinates] = useState(null)
@@ -39,15 +66,26 @@ export default function Home () {
 
   return (
     <section className='flex-container'>
-      <article className='column-6 home-title-container'>
+      <motion.article className='column-6 home-title-container'
+        variants={leftVariant}
+        initial='hidden'
+        animate='visible'
+      >
         <h1 className="home-title-1">Empowering</h1>
         <h1 className="home-title-2">Community Gardens</h1>
         <h1 className="home-title-3">in New Zealand</h1>
         <p className='hero-title'>Help your community get the most out of your garden with events and reporting and become eligible for government subsidies</p>
         <Link className='button-primary button-width' to='/signin'>Get Started</Link>
-      </article>
+      </motion.article>
 
-      <img className='home-image' src='/images/comGardenPlant.png' alt="" />
+      <motion.img
+        className='home-image'
+        src='/images/comGardenPlant.png'
+        alt=""
+        variants={rightVariant}
+        initial='hidden'
+        animate='visible'
+      />
 
     </section>
   )
