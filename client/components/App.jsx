9b5bf22124a6731f95576b6344f8d3e75234b0c6
Kelly Keating <keating.kel@gmail.com>
2021-11-11 18:39:00 +1300

    move import to reduce unneeded props passing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 import Header from './Header'
 import BeerList from './BeerList'
 
-import beerData from '../../data/beers'
-
 function App () {
   return (
     <div className='app'>
       <Header />
-      <BeerList beers={beerData.beers} />
+      <BeerList />
     </div>
   )
 }
--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import BeerListItem from './BeerListItem'
 
+import beerData from '../../data/beers'
+
 function BeerList (props) {
   return (
     <div className='beerlist'>
       <p className='welcome'>Welcome! Please select from our delicious selection and don&apos;t hesitate to let us know if we can answer any of your questions.</p>
-      {props.beers.map(beer => {
+      {beerData.beers.map(beer => {
         return (
           <BeerListItem key={beer.id} beer={beer} />
         )
