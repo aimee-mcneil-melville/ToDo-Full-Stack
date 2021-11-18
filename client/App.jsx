972ad11d39163e566bd66de602f03efe11c9bdb8
Prue Singh <pruesinghnagra@gmail.com>
2021-11-18 22:36:39 +1300

    updated imports

--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import Register from './components/Register'
-import SignIn from './components/SignIn'
-import { Route } from 'react-router-dom'
-import AddSong from './components/AddSong'
-import Friends from './components/Friends'
-import Songs from './components/Songs'
-import UpdateSong from './components/UpdateSong'
-import FriendSongs from './components/FriendSongs'
-import Header from './components/Header'
-import Tagline from './components/Tagline'
+import Register from './components/Register/Register'
+import SignIn from './components/SignIn/SignIn'
+import AddSong from './components/AddSong/AddSong'
+import Friends from './components/Friends/Friends'
+import Songs from './components/Songs/Songs'
+import UpdateSong from './components/UpdateSong/UpdateSong'
+import FriendSongs from './components/Friends/FriendSongs'
+import Header from './components/Header/Header'
+import Tagline from './components/Tagline/Tagline'
 
 function App () {
   return (
     <>
       <div className='app'>
         <Header />
-        {/* <Route exact path='/' component={Auth} /> //waiting Auth team creating this component */}
         <Route exact path='/friends' component={Friends} />
         <Route path='/friends/:id' component={FriendSongs} />
         <Route path='/songs/add' component={AddSong} />
