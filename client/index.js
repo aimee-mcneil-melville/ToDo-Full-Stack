188feb46befca80a6aa07ff082e73bf7acbe438d
Prue Singh <pruesinghnagra@gmail.com>
2021-11-18 22:36:39 +1300

    updated imports

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { HashRouter as Router } from 'react-router-dom'
 
-import App from './components/App'
+import App from './App'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
