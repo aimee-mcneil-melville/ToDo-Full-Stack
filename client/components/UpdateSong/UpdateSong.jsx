1ea4a8196fed7362f1c2f6d82a29e40709748815
Jared Pinfold <j.pinfold@gmail.com>
2021-11-24 11:31:44 +1300

    link song list to redux store

--- client/components/UpdateSong/UpdateSong.jsx
+++ client/components/UpdateSong/UpdateSong.jsx
@@ -3,16 +3,9 @@ import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 function UpdateSong (props) {
-  const userSongs = [
-    { id: 1, user_id: 10001, genre: 'Indie Rock', mediaName: 'Hengelo', artist: 'Spring Offensive', link: 'https://open.spotify.com/track/4rqpg85XNApASjAvqjHlb1?si=2bdc00343f3e47f2' },
-    { id: 2, user_id: 10001, genre: 'Indie Rock', mediaName: 'By Design', artist: 'Tigercub', link: 'https://open.spotify.com/track/6ICdz2wvVMDC4u801OwHA2?si=8e214d39012c4685' },
-    { id: 3, user_id: 10001, genre: 'Indie Rock', mediaName: 'High Strings', artist: 'Noise Punk', link: 'https://open.spotify.com/track/38HaKBYwrqnhrjf9sqJbfc?si=e9bc96d2247c4347' },
-    { id: 4, user_id: 10001, genre: 'Funky House', mediaName: 'I look to you (feat. Kimbra)', artist: 'Miami Horror', link: 'https://open.spotify.com/track/0L0GeZL4lyx34nYDzsNuG4?si=a89fdafe24d74899' }
-  ]
-
   const { id } = useParams()
 
-  const song = userSongs.find(song => song.id === Number(id))
+  const song = props.userSongs.find(song => song.id === Number(id))
 
   const { mediaName, artist, genre, comment, link } = song || {}
 
