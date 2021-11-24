a6b16d9ae60247c42b2638b3adfdb90249fadd26
John Gray <john.edwin.gray@gmail.com>
2021-11-24 13:08:14 +1300

    App.jsx code clean-up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -36,9 +36,7 @@ function App () {
 
           <div className=''>
             <Routes>
-             
-                <Route path="/" element={auth.isAuthenticated ? <></> : <Login />} />
-              {/* <Route path="/login" element={<Login /} /> */}
+              <Route path="/" element={auth.isAuthenticated ? <></> : <Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/meeting" element={<Meeting />} />
               <Route path="/history" element={<History />} />
