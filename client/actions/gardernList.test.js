8d7965745b2f9020783803b3a2c45e60ef59c333
ppc99999 <ppc99999@gmail.com>
2021-11-26 13:53:20 +1300

    fix reducer

--- client/actions/gardernList.test.js
+++
@@ -1,8 +0,0 @@
-import { GET_GARDEN_LIST } from '../actions/gardernList'
-
-export default function allGardenList (state = [], action) {
-  switch (action.type) {
-    case GET_GARDEN_LIST:
-      return action.gardernList
-  }
-}
