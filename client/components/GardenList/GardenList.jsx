dea4216b1cab492d71dc08242486bebcdd9472a5
ppc99999 <ppc99999@gmail.com>
2021-11-26 15:42:41 +1300

    update render data

--- client/components/GardenList/GardenList.jsx
+++ client/components/GardenList/GardenList.jsx
@@ -4,14 +4,6 @@ import { Link } from 'react-router-dom'
 import { getAllGardens } from './gardernListHelper'
 
 export default function GardenList () {
-  // const testGardenList = [{
-  //   id: 0,
-  //   name: 'Test Garden',
-  //   address: '222 Test St, Testington, Testburg',
-  //   description: 'The testiest of all test gardens. The best garden for all your testing needs.',
-  //   events: ['event1', 'event2', 'event3']
-  // }]
-
   const [gardenList, setGardenList] = useState([])
 
   useEffect(() => {
@@ -33,7 +25,7 @@ export default function GardenList () {
           </Link>
           <h4>Address: {garden.address}</h4>
           <p>{garden.description}</p>
-          <a href={garden.url} > <span className='fas fa-home'/>website </a>
+
         </div>
       })}
     </>
