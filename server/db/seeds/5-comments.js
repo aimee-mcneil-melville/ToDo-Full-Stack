6d20e88008deea8c8f358a1cd9aa20b88d974d50
Julianne McCoun <jmccoun7@gmail.com>
2021-11-30 11:51:21 +1300

    Fix seed integer issue

--- server/db/seeds/5-comments.js
+++ server/db/seeds/5-comments.js
@@ -13,15 +13,15 @@ exports.seed = function (knex) {
       return knex('comments').insert([
         {
           id: 1,
-          post_id: '1',
-          author: '2',
+          post_id: 1,
+          author: 2,
           created_on: diffDate(1),
           content: 'Twas a wonderful day for lettuce picking!'
         },
         {
           id: 2,
-          post_id: '2',
-          author: '1',
+          post_id: 2,
+          author: 1,
           created_on: todayDate,
           content: 'Would have been a wonderful day for picking cabbages if any of the volunteers had shown up >:('
         }
