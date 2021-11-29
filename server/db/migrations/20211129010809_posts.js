9cbe2fdf9c9317b0a55dfbf7b4f20d435a541470
Julianne McCoun <jmccoun7@gmail.com>
2021-11-29 14:56:14 +1300

    Create migrations and seeds for the new blog implementation, adding three new db tables: blogs, posts and comments

---
+++ server/db/migrations/20211129010809_posts.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('posts', table => {
+    table.increments('id').primary()
+    table.integer('blog_id').references('blogs.id')
+    table.integer('author').references('users.id')
+    table.string('title')
+    table.datetime('created_on')
+    table.string('content')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('posts')
+}
