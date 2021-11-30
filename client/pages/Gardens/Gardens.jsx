f74d2927ee345f559c3f78b0b1127d976d90bced
GorettiA <gorettialani@gmail.com>
2021-11-30 13:35:04 +1300

    garden list rendering

--- client/pages/Gardens/Gardens.jsx
+++ client/pages/Gardens/Gardens.jsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 import { getAllGardens } from './gardensHelper'
 
-export default function Gardens () {
+export default function Gardens() {
   const [gardenList, setGardenList] = useState([])
+
+  const cardVariant1 = {
+    hidden: {
+      y: '-100vw'
+    },
+    visible: {
+      y: 0,
+      transition: {
+        ease: 'easeInOut',
+        duration: 2
+      }
+    },
+    transition: {
+      duration: 2
+    }
+  }
 
   useEffect(() => {
     getAllGardens()
@@ -16,15 +33,23 @@ export default function Gardens () {
   }, [])
 
   return (
-    <ul>
+    <ul className='garden-list-container'>
       {gardenList.map((garden) => {
-        return <li key={garden.id}>
-          <Link to={`/gardens/${garden.id}`}>
-            <h2>{garden.name}</h2>
-          </Link>
-          <h4>Address: {garden.address}</h4>
-          <p>{garden.description}</p>
-        </li>
+        return (
+          <motion.li
+            className='home-title-container garden-list-info'
+            key={garden.id}
+            variants={cardVariant1}
+            initial='hidden'
+            animate='visible'
+            whileHover={{ scale: 1.2, transition: { duration: 0.5 } }}>
+            <Link to={`/gardens/${garden.id}`}>
+              <h2 className='garden-list-header'>{garden.name}</h2>
+            </Link>
+            <h4>Address: {garden.address}</h4>
+            <p className='garden-description'>{garden.description}</p>
+          </motion.li>
+        )
       })}
     </ul>
   )
