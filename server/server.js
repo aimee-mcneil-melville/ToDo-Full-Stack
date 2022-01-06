ef67274e7c0ec6b53ab98d064123463cc3179e0c
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-01-06 23:17:05 +1300

    Change from HashRouter to BrowserRouter

--- server/server.js
+++ server/server.js
@@ -5,4 +5,9 @@ const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
+// This is the BrowserRouter config - recommended approach is to only use HashRouter if you don't have a server
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve('server/public/index.html'))
+})
+
 module.exports = server
