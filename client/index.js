38c67ffb819a4218ccc0320b7640f6abd57eab65
gus.hawke <gus.hawke@gmail.com>
2022-02-18 11:06:25 +1300

    FINALLY linting resolved

--- client/index.js
+++ client/index.js
@@ -7,7 +7,6 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 import store from './store'
 
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Auth0Provider
