b4cfc086f5228c6c84ebce2d657dbba1b40bea3f
jinny <vedaforest@gmail.com>
2022-02-18 18:53:04 +1300

    put new router in App.jsx

--- client/pages/Posts/Posts.jsx
+++ client/pages/Posts/Posts.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
-export default function Posts () {
+export default function Posts() {
+  const { id } = useParams()
+  // const globalState = useSelector(globalState => globalState)
+
+  // console.log(globalState)
   return (
     <p>show posts here</p>
   )
--- client/pages/Posts/postsHelper.js
+++ client/pages/Posts/postsHelper.js
@@ -1,4 +1,25 @@
-export function fetchPostsByGardenId (gardenId) {
+// import requestor from '../../consume'
+// import { dispatch } from '../../store'
+
+// export function getPosts(id, user, consume = requestor) {
+//   dispatch(setWaiting())
+//   const headers = {
+//     Accept: 'application/json',
+//     userid: user.id
+//   }
+
+//   return consume(`/gardens/${id}/posts`, token, 'get', {}, headers)
+//     .then((res) => {
+//       const garden = res.body
+//       console.log(garden)
+//       return null
+//     })
+//     .catch((error) => {
+//       dispatch(showError(error.message))
+//     })
+// }
+
+export function fetchPostsByGardenId(gardenId) {
   return Promise.resolve([{
     id: 1,
     name: 'replace this and call consume'
