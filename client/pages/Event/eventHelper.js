7c56a5982102782885d87cc0c6f618f20c5d086f
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-02-21 15:17:41 +1300

    Clean up

--- client/pages/Event/eventHelper.js
+++ client/pages/Event/eventHelper.js
@@ -3,7 +3,7 @@ import { dispatch } from '../../store'
 import { setWaiting, clearWaiting } from '../../actions/waiting'
 import { showError } from '../../actions/error'
 
-export function getEvent(id, user, consume = requestor) {
+export function getEvent (id, user, consume = requestor) {
   dispatch(setWaiting())
   return consume(`/events/${id}`)
     .then((res) => {
