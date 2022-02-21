fc72162421e4233ef39264c4bcd111edee24a849
Chihiro0420 <chihiroe2510@gmail.com>
2022-02-22 10:55:30 +1300

    finish 2 test

--- client/auth-utils.test.js
+++ client/auth-utils.test.js
@@ -38,3 +38,40 @@ describe('verified email', () => {
       })
   })
 })
+
+describe('user registration', () => {
+  test('if user is not registered navigate to profile', () => {
+    const useAuth0 = () => ({
+      isAuthenticated: true,
+      getAccessTokenSilently: () => Promise.resolve('token'),
+      user: { id: undefined }
+    })
+
+    const navigate = jest.fn()
+    consume.mockImplementation(() => Promise.resolve({ body: { id: undefined } }))
+
+    return cacheUser(useAuth0, navigate)
+      .then(() => {
+        expect(navigate).toHaveBeenCalledWith('/profile')
+        return null
+      })
+  })
+
+  test('if user is registered,navigate is not called', () => {
+    const useAuth0 = () => ({
+      isAuthenticated: true,
+      getAccessTokenSilently: () => Promise.resolve('token'),
+      user: { id: 1 }
+    })
+
+    const navigate = jest.fn()
+    console.log(useAuth0)
+    consume.mockImplementation(() => Promise.resolve({ body: { id: 1 } }))
+
+    return cacheUser(useAuth0, navigate)
+      .then(() => {
+        expect(navigate).toHaveBeenCalledTimes(0)
+        return null
+      })
+  })
+})
