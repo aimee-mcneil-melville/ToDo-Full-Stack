afa482e73b4c127cafd7d5cb2f2b2ed40f4426e5
ahmad <eng.ahmed.anwar@gmail.com>
2022-02-25 08:28:18 +1300

    use inline expression

--- client/components/Posts/PostList.jsx
+++ client/components/Posts/PostList.jsx
@@ -4,11 +4,12 @@ import Post from './Post'
 export default function PostList ({ posts }) {
   return (
     <ul className='list-primary'>
-      {posts.map(post => {
-        return (
-          <Post post={post} key={post.id} />
-        )
-      })}
+      {posts.map(post =>
+        <Post
+          post={post}
+          key={post.id}
+        />
+      )}
     </ul>
   )
 }
