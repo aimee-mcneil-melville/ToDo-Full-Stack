70c5334537403e482664009293a67bd0f166073d
ahmad <eng.ahmed.anwar@gmail.com>
2022-02-24 16:33:17 +1300

    fix test
    this test always returned true

--- client/components/events/EventItem/EventItem.test.js
+++ client/components/events/EventItem/EventItem.test.js
@@ -27,8 +27,9 @@ describe('Volunteer button', () => {
   })
 
   it('does not display for an admin', () => {
-    renderWithRouter(<EventItem isAdmin={true} event={{}} />)
-    expect(screen.queryByRole('view-event-link')).toBeNull()
+    renderWithRouter(<EventItem isAdmin={false} event={{ isVolunteer: true }} />)
+    const volunteerButton = screen.queryByRole('link', { name: 'Volunteer' })
+    expect(volunteerButton).toBeNull()
   })
 })
 
