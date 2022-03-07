cb1159822edba89d6964f9f8e2748f7e4f606026
Gerard Paapu <gerard@gerardpaapu.com>
2022-03-07 17:56:15 +1300

    Replaced enzyme with @testing-library

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,22 +6,18 @@ import startingTiles from '../startingTiles'
 const tryAgain = 'No match, try again'
 const winMessage = 'Congratulations, you matched all the tiles!'
 
-function App (props) {
+function App(props) {
   const isMatch = false
   const matchCount = 0
 
-  const hasWon = matchCount === (startingTiles.length / 2)
+  const hasWon = matchCount === startingTiles.length / 2
 
-  const reset = () => {
+  const reset = () => {}
 
-  }
-
-  const evalMatch = () => {
-
-  }
+  const evalMatch = () => {}
 
   return (
-    <div className='game'>
+    <div className="game">
       <h1>Welcome to the Memory Game</h1>
       <h2>Match all the tiles to win</h2>
 
@@ -30,7 +26,7 @@ function App (props) {
       <h5>{hasWon && winMessage}</h5>
       <h5>{!isMatch && tryAgain}</h5>
 
-      <div className='replaybutton'>
+      <div className="replaybutton">
         {hasWon && <button onClick={reset}>Play Again</button>}
       </div>
     </div>
