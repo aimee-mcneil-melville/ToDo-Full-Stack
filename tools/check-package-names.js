174d81c9c6e324753d97f725588cf61d8a29bf51
Gerard Paapu <gerard@gerardpaapu.com>
2022-03-10 11:30:39 +1300

    Fixed the path back to package.json

--- tools/check-package-names.js
+++ tools/check-package-names.js
@@ -11,14 +11,14 @@ const main = async () => {
         '..',
         'packages',
         challenge,
-        'packageObj.json'
+        'package.json'
       )
       const packageJson = await fs.readFile(packagePath, 'utf8')
       const packageObj = JSON.parse(packageJson)
 
       if (packageObj.name !== challenge) {
         console.log(
-          `correcting ${challenge}'s packageObj.name from ${packageObj.name}`
+          `correcting ${challenge}'s package.name from ${packageObj.name}`
         )
         const newJson = JSON.stringify(
           { ...packageObj, name: challenge },
