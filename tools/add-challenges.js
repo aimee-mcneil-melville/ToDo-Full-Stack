bb6c90c95e105f2664e325d08bbf17e49bf6cdc5
Gerard Paapu <gerard@gerardpaapu.com>
2022-03-10 17:08:45 +1300

    Add safeguards to "add-challenges"

--- tools/add-challenges.js
+++ tools/add-challenges.js
@@ -1,11 +1,20 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
+const { existsSync } = require('fs')
+const path = require('path')
 
 const challenges = require('./challenges-list')
 
 const main = async () => {
+  process.chdir(path.join(__dirname, '..'))
+
   for (const challenge of challenges) {
+    const packagePath = path.join(__dirname, '..', 'packages', challenge)
     try {
+      if (existsSync(packagePath)) {
+        console.log(`challenge ${challenge} exists at ${packagePath}`)
+      }
+
       await exec(
         `git subtree add --prefix="packages/${challenge}" git@github.com:dev-academy-challenges/${challenge} main`
       )
