4406ca1b8950bb4fe92a15f33a83dc18df73c314
Gerard Paapu <gerard@gerardpaapu.com>
2022-03-31 15:06:40 +1300

    Add the rest of the challenges back

--- tools/add-challenges.js
+++ tools/add-challenges.js
@@ -1,7 +1,7 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const { existsSync } = require('fs')
-const path = require('path')
+const path = require('path/posix')
 
 const challenges = require('./challenges-list')
 
@@ -13,8 +13,15 @@ const main = async () => {
     try {
       if (existsSync(packagePath)) {
         console.log(`challenge ${challenge} exists at ${packagePath}`)
+        await exec(
+          `git subtree pull --prefix="packages/${challenge}" git@github.com:dev-academy-challenges/${challenge} main`
+        )
+        continue
       }
 
+      console.log(
+        `challenge ${challenge} doesn't exist at ${packagePath}, adding`
+      )
       await exec(
         `git subtree add --prefix="packages/${challenge}" git@github.com:dev-academy-challenges/${challenge} main`
       )
@@ -25,6 +32,7 @@ const main = async () => {
 }
 
 main().catch((e) => {
-  console.error(`pull-challenges: ${e}`)
+  const scriptName = path.basename(process.argv[1], '.js')
+  console.error(`${scriptName}: ${e}`)
   process.exitCode = 1
 })
