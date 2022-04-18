016fb26cb9912c6b9df15e5ddf75423969a5bb30
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-04-19 01:12:05 +1200

    Use combineReducers

--- client/store.js
+++ client/store.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux'
 
-import wombatReducer from './reducer'
+import reducer from './reducers'
 
 const store = createStore(
-  wombatReducer,
+  reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
