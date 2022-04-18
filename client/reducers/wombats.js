47c130c72753c93fcad8147461f985300c623aea
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-04-19 01:24:17 +1200

    Update reducer to use payload

--- client/reducers/wombats.js
+++ client/reducers/wombats.js
@@ -3,9 +3,9 @@ const initialWombatState = ['Gertrude', 'Bartholemew']
 const wombatReducer = (state = initialWombatState, action) => {
   switch (action.type) {
     case 'ADD_WOMBAT':
-      return [...state, action.wombat]
+      return [...state, action.payload]
     case 'DEL_WOMBAT':
-      return state.filter((wombat) => wombat !== action.wombat)
+      return state.filter((wombat) => wombat !== action.payload)
     default:
       return state
   }
