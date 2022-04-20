b8af545192648801aadd6afb98ea0e5360b892db
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-04-20 21:33:08 +1200

    Destructure the action

--- client/reducers/wombats.js
+++ client/reducers/wombats.js
@@ -1,11 +1,12 @@
 const initialWombatState = ['Gertrude', 'Bartholemew']
 
 const wombatReducer = (state = initialWombatState, action) => {
-  switch (action.type) {
+  const { type, payload } = action
+  switch (type) {
     case 'ADD_WOMBAT':
-      return [...state, action.payload]
+      return [...state, payload]
     case 'DEL_WOMBAT':
-      return state.filter((wombat) => wombat !== action.payload)
+      return state.filter((wombat) => wombat !== payload)
     default:
       return state
   }
