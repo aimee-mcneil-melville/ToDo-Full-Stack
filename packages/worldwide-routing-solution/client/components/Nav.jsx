2cb9157055b6969044c805c58f0e0513ebca708f
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2022-05-08 22:12:17 +1200

    Fixups per Gerard's suggestions

--- packages/worldwide-routing-solution/client/components/Nav.jsx
+++ packages/worldwide-routing-solution/client/components/Nav.jsx
@@ -3,10 +3,9 @@ import { Link, useLocation } from 'react-router-dom'
 
 import continentData from '../../data/continents'
 
-function Nav(props) {
+function Nav() {
   const continents = Object.keys(continentData)
-  const url = useLocation().pathname
-
+  const url = decodeURI(useLocation().pathname)
   return (
     <div>
       <h2>Nav</h2>
@@ -18,7 +17,6 @@ function Nav(props) {
       <ul>
         {continents.map((name, i) => {
           // This className stuff is to make the selected continent bold in the Nav
-          // But there is a bug - it doesn't go bold for continents whose name is two words, e.g. "North America"
           return (
             <li key={i} className={url.includes(name) ? 'selected' : ''}>
               <Link to={`/continent/${name}`}>{name}</Link>
--- packages/worldwide-routing-solution/server/server.js
+++ packages/worldwide-routing-solution/server/server.js
@@ -5,7 +5,6 @@ const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
-// BrowserRouter is recommended (over HashRouter) unless you don't have a server
 // This is the BrowserRouter config
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
