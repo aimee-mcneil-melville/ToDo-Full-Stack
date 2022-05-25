db7ce74354330014ce9dc65de0b67e33437586cc
Kelly <keating.kel@gmail.com>
2022-05-25 13:17:11 +1200

    add extra item to filter test

--- packages/kata-types-modules/tests/functions/filter.test.js
+++ packages/kata-types-modules/tests/functions/filter.test.js
@@ -2,10 +2,10 @@ const functions = require('../../functions')
 
 // Write a simple filter implementation
 test('functions.filter takes an array as the first parameter, a function as the second parameter, and only returns elements for which the function returns true', function () {
-  const expected = [2]
+  const expected = [2, 4]
   function isEven(n) {
     return n % 2 === 0
   }
-  const actual = functions.filter([1, 2, 3], isEven)
+  const actual = functions.filter([1, 2, 3, 4], isEven)
   expect(actual).toEqual(expected)
 })
