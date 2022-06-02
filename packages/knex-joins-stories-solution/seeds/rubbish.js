937bc5f28fbeccc1f1fbc09ce8a4073840b9f876
sarah-emerson <70471216+sarah-emerson@users.noreply.github.com>
2022-06-03 00:25:45 +1200

    Fix hbs middleware, fix seeds

--- packages/knex-joins-stories-solution/seeds/rubbish.js
+++ packages/knex-joins-stories-solution/seeds/rubbish.js
@@ -1,13 +1,13 @@
-exports.seed = function (knex, Promise) {
-  return knex('rubbish').del()
-    .then(function () {
-      return Promise.all([
-        knex('rubbish').insert({id: 1, name: 'polystyrene'}),
-        knex('rubbish').insert({id: 2, name: 'tin can'}),
-        knex('rubbish').insert({id: 3, name: 'nappy'}),
-        knex('rubbish').insert({id: 4, name: 'coffee cup'}),
-        knex('rubbish').insert({id: 5, name: 'plastic'}),
-        knex('rubbish').insert({id: 6, name: 'dust'})
+exports.seed = (knex) =>
+  knex('rubbish')
+    .del()
+    .then(() =>
+      knex('rubbish').insert([
+        { id: 77701, name: 'polystyrene' },
+        { id: 77702, name: 'tin can' },
+        { id: 77703, name: 'nappy' },
+        { id: 77704, name: 'coffee cup' },
+        { id: 77705, name: 'plastic' },
+        { id: 77706, name: 'dust' },
       ])
-    })
-}
+    )
--- packages/knex-joins-stories-solution/seeds/wombles.js
+++ packages/knex-joins-stories-solution/seeds/wombles.js
@@ -1,14 +1,49 @@
-exports.seed = function (knex, Promise) {
-  return knex('wombles').del()
-    .then(function () {
-      return Promise.all([
-        knex('wombles').insert({id: 88801, name: 'Great Uncle Bulgaria', characteristic_id: 99901, rubbish_id: 1}),
-        knex('wombles').insert({id: 88802, name: 'Tobermory', characteristic_id: 99902, rubbish_id: 2}),
-        knex('wombles').insert({id: 88803, name: 'Madame Cholet', characteristic_id: 99903, rubbish_id: 3}),
-        knex('wombles').insert({id: 88804, name: 'Orinoco', characteristic_id: 99904, rubbish_id: 4}),
-        knex('wombles').insert({id: 88805, name: 'Wellington', characteristic_id: 99905, rubbish_id: 5}),
-        knex('wombles').insert({id: 88806, name: 'Tomsk', characteristic_id: 99906, rubbish_id: 6}),
-        knex('wombles').insert({id: 88807, name: 'Bungo', characteristic_id: 99907, rubbish_id: 1})
+exports.seed = (knex) =>
+  knex('wombles')
+    .del()
+    .then(() =>
+      knex('wombles').insert([
+        {
+          id: 88801,
+          name: 'Great Uncle Bulgaria',
+          characteristic_id: 99901,
+          rubbish_id: 1,
+        },
+        {
+          id: 88802,
+          name: 'Tobermory',
+          characteristic_id: 99902,
+          rubbish_id: 2,
+        },
+        {
+          id: 88803,
+          name: 'Madame Cholet',
+          characteristic_id: 99903,
+          rubbish_id: 3,
+        },
+        {
+          id: 88804,
+          name: 'Orinoco',
+          characteristic_id: 99904,
+          rubbish_id: 4,
+        },
+        {
+          id: 88805,
+          name: 'Wellington',
+          characteristic_id: 99905,
+          rubbish_id: 5,
+        },
+        {
+          id: 88806,
+          name: 'Tomsk',
+          characteristic_id: 99906,
+          rubbish_id: 6,
+        },
+        {
+          id: 88807,
+          name: 'Bungo',
+          characteristic_id: 99907,
+          rubbish_id: 1,
+        },
       ])
-    })
-}
+    )
