3709faca7109b7538948d78fa65db71090555eb1
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-09-08 10:24:21 +1200

    add JSDoc comments to helper functions

--- packages/dreamfest/helpers.js
+++ packages/dreamfest/helpers.js
@@ -6,17 +6,23 @@ module.exports = {
   validateDay,
 }
 
-/*
- * We work with the days in all lowercase (above).
- * This function helps make them look nice for the user.
+/**
+ * Takes a string and capitalises the first letter.
+ *
+ * e.g. capitalise('tangle stage') => returns 'Tangle stage'
+ * @param {string} name
+ * @returns string
  */
 function capitalise(name) {
   return name[0].toUpperCase() + name.substring(1)
 }
 
-/*
- * This helps us always use a valid day, even
- * if the user puts an invalid one in the URL.
+/**
+ * Ensures that @param day is a string and is a valid Event day
+ * The default valid event days are: friday, saturday, and sunday
+ * @param {string} day
+ * @param {string[]} days
+ * @returns string
  */
 function validateDay(day, days = eventDays) {
   // Use the first day as the default value if the day argument isn't valid
