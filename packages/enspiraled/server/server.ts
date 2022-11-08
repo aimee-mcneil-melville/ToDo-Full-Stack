e25b14317a277a154dd1a8b46fcb5b154517692d
Gerard Paapu <gerard@gerardpaapu.com>
2022-11-08 14:11:15 +1300

    feat: ported enspiraled to typescript

--- packages/enspiraled/server/server.ts
+++ packages/enspiraled/server/server.ts
@@ -1,8 +1,8 @@
-var path = require('path')
-var express = require('express')
+import path from 'node:path'
+import express from 'express'
 
 var server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
-module.exports = server
+export default server
