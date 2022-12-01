2dd9da5f05bb76c9d2e93f5c804eb5a4368885a1
sophia-lawler <sophialawler@gmail.com>
2022-12-02 09:53:01 +1300

    update store with composeWithDevTools

--- packages/my-fullstack-collection-scss/client/index.js
+++ packages/my-fullstack-collection-scss/client/index.js
@@ -1,13 +1,13 @@
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
