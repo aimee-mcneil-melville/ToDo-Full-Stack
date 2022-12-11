04751b8a7be1595b8f2f9c1f014ae2b84cf37620
Benjamin <benjamin.trainor@devacademy.co.nz>
2022-12-12 11:51:08 +1300

    knex-join-stories and solution updated with server folder in root

--- packages/knex-joins-stories-solution/server/index.js
+++ packages/knex-joins-stories-solution/server/index.js
@@ -1,4 +1,4 @@
-const server = require('./server')
+const server = require('../server')
 
 const PORT = 3000
 
--- packages/knex-joins-stories/server/test/routes.test.js
+++ packages/knex-joins-stories/server/test/routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const server = require('./server')
+const server = require('..')
 
 test('list wombles', () => {
   const expected = 'WOMBLES!'
