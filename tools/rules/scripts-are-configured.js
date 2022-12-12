47891d9ab673374e8d12c360642510c466ca40b2
Benjamin <benjamin.trainor@devacademy.co.nz>
2022-12-12 18:16:07 +1300

    jest and lint scripts

--- tools/rules/scripts-are-configured.js
+++ tools/rules/scripts-are-configured.js
@@ -1,24 +1,39 @@
-// const EXPECTED_SCRIPTS = {
-//   start: 'ts-node server/index.ts',
-//   'build:client': 'NODE_ENV=production webpack',
-//   'build:server': 'knex --knexfile ./server/db/knexfile.js migrate:latest',
-//   dev: 'run-p dev:client dev:server',
-//   'dev:client': 'webpack --watch',
-//   'dev:server': 'nodemon server/index.ts',
-//   lint: 'eslint --ext .js,.jsx,.ts,.tsx',
-//   test: 'jest',
-//   knex: 'knex --knexfile ./server/db/knexfile.js',
-// }
+const EXPECTED_SCRIPTS = {
+  start: 'ts-node server/index.ts',
+  'build:client': 'NODE_ENV=production webpack',
+  'build:server': 'knex --knexfile ./server/db/knexfile.js migrate:latest',
+  dev: 'run-p dev:client dev:server',
+  'dev:client': 'webpack --watch',
+  'dev:server': 'nodemon server/index.ts',
+  lint: 'eslint --ext .js,.jsx,.ts,.tsx',
+  test: 'jest',
+  knex: 'knex --knexfile ./server/db/knexfile.js',
+}
 
 module.exports = async ({ package: packageFile, versions, fix }) => {
   const { scripts, dependencies, devDependencies } = packageFile
   const deps = { ...dependencies, ...devDependencies }
-  if (!deps['react'] || !deps['express']) {
-    return
+
+  if ('express' in deps && !('react' in deps)) {
+    scripts.start = 'node index'
+    scripts.dev = 'nodemon index'
   }
+
+  // Every repo should have these scripts
+  if ('jest' in deps) {
+    scripts.test = 'jest --watchAll'
+  }
+
+  scripts.lint = 'eslint --ext .js,.jsx,.ts,.tsx .'
 }
 
-// Express
+// I am going to attempt to have a script working for both Express and Knex projects
+
+// Every Repo
+// "test": "jest --watchAll",
+// "lint": "eslint --ext .js,.jsx,.ts,.tsx .",
+
+// EXPRESS
 // express-server
 // "start": "node index",
 
@@ -26,11 +41,11 @@ module.exports = async ({ package: packageFile, versions, fix }) => {
 // "start": "node index",
 // "dev": "nodemon index",
 
-// Knex
-// knex-todo-cli & boilerplate-express-api
+// KNEX
+// knex-todo-cli
 // "knex": "knex",
 
-// todo-full-stack, knex-join-stories, dreamfest & boilerplate-fullstack
+// todo-full-stack, knex-join-stories, dreamfest, boilerplate-express-api & boilerplate-fullstack
 // "knex": "knex --knexfile ./server/db/knexfile.js",
 
 // React
@@ -48,7 +63,3 @@ module.exports = async ({ package: packageFile, versions, fix }) => {
 // "build": "ts-node server/index.ts",
 
 // same as lines 48-51
-
-// Every Repo
-// "test": "jest --watchAll",
-// "lint": "eslint --ext .js,.jsx,.ts,.tsx .",
