1337d5f15ade9c142b1334cdde77b0310b7dca84
Benjamin <benjamin.trainor@devacademy.co.nz>
2022-12-13 19:35:53 +1300

    almost completed the TypeScript logic

--- tools/rules/scripts-are-configured.js
+++ tools/rules/scripts-are-configured.js
@@ -1,13 +1,10 @@
-// const EXPECTED_SCRIPTS = {
-//   start: 'ts-node server/index.ts',
+// const UNIMPLEMENTATED_SCRIPTS = {
 //   'build:client': 'NODE_ENV=production webpack',
 //   'build:server': 'knex --knexfile ./server/db/knexfile.js migrate:latest',
-//   dev: 'run-p dev:client dev:server',
-//   'dev:client': 'webpack --watch',
-//   'dev:server': 'nodemon server/index.ts',
-//   lint: 'eslint --ext .js,.jsx,.ts,.tsx',
-//   test: 'jest',
-//   knex: 'knex --knexfile ./server/db/knexfile.js',
+//   'db-reset': 'run-s db:*',
+//   'db:delete': 'rm server/db/dev.sqlite3',
+//   'db:migrate': 'npm run knex migrate:latest',
+//   'db:seed': 'npm run knex seed:run',
 // }
 
 const COMMON_SCRIPTS = {
@@ -22,13 +19,22 @@ const JS_SCRIPTS = {
   knex: 'knex --knexfile ./server/db/knexfile.js',
 }
 
+const TS_SCRIPTS = {
+  start: 'ts-node server/index.ts',
+  // build: 'npm run webpack --mode production',
+  dev: 'run-p dev:server dev:client',
+  'dev:client': 'npm run webpack -- --watch',
+  'dev:server': 'nodemon server/index.ts',
+  webpack: 'webpack --config ./client/webpack.config.js',
+}
+
 module.exports = async ({ package: packageFile, versions, fix }) => {
   const { scripts, dependencies, devDependencies } = packageFile
   const deps = { ...dependencies, ...devDependencies }
 
   let modified = false
   let errors = ''
-  const errorMessage = `Scripts are misconfigured: ${errors}`
+  const errorMessage = `Scripts are misconfigured: \n ${errors}`
 
   const message = (scriptName) =>
     (errors += `${scriptName} script in: ${packageFile.name} is incorrect, \n`)
@@ -63,7 +69,7 @@ module.exports = async ({ package: packageFile, versions, fix }) => {
       modified = true
     }
 
-    devDependencies.nodemon = versions.nodemon
+    devDependencies.nodemon = versions.nodemon // maybe we don't need this?
 
     if ('knex' in deps) {
       if (scripts.knex != JS_SCRIPTS.knex) {
@@ -89,11 +95,52 @@ module.exports = async ({ package: packageFile, versions, fix }) => {
     }
   }
 
-  // Express, and Express and Knex with TS
+  // TS scripts
   if ('express' in deps && 'typescript' in deps) {
-    scripts.start = 'ts-node server/index.ts'
+    if (scripts.start != TS_SCRIPTS.start) {
+      scripts.start = TS_SCRIPTS.start
+      message('start')
+      modified = true
+    }
+    if ('webpack' in deps) {
+      if (scripts.webpack != TS_SCRIPTS.webpack) {
+        scripts.webpack = TS_SCRIPTS.webpack
+        message('webpack')
+        modified = true
+      }
+      if (scripts.dev != TS_SCRIPTS.dev) {
+        scripts.dev = TS_SCRIPTS.dev
+        message('dev')
+        modified = true
+      }
+      // Does this incorrect naming exist
+      if (scripts['watch:client']) {
+        delete scripts['watch:client']
+        scripts['dev:client'] = TS_SCRIPTS['dev:client']
+        message('dev:client')
+        modified = true
+      }
+      if (scripts['dev:client'] != TS_SCRIPTS['dev:client']) {
+        scripts['dev:client'] = TS_SCRIPTS['dev:client']
+        message('dev:client')
+        modified = true
+      }
+      // Does this incorrect naming exist
+      if (scripts['watch:server']) {
+        delete scripts['watch:server']
+        scripts['dev:server'] = TS_SCRIPTS['dev:server']
+        message('dev:server')
+        modified = true
+      }
+      if (scripts['dev:server'] != TS_SCRIPTS['dev:server']) {
+        scripts['dev:server'] = TS_SCRIPTS['dev:server']
+        message('dev:server')
+        modified = true
+      }
+    }
   }
 
+  // Exit code
   if (modified && !fix) {
     throw new Error(errorMessage)
   }
@@ -103,39 +150,41 @@ module.exports = async ({ package: packageFile, versions, fix }) => {
   }
 }
 
+// REFERENCES
+
+// React
+// DONE react-paws-for-effect
+// "start": "ts-node server/index.ts",
+// "build": "npm run webpack --mode production",
+// "dev": "run-p dev:server dev:client",
+// "dev:client": "npm run webpack -- --watch",
+// "dev:server": "nodemon server/index.ts",
+// "webpack": "webpack --config ./client/webpack.config.js",
+
+// => TODO DB Knex Scripts for Production
+
+// "db-reset": "run-s db:*",
+// "db:delete": "rm server/db/dev.sqlite3",
+// "db:migrate": "npm run knex migrate:latest",
+// "db:seed": "npm run knex seed:run",
+
 // I am going to attempt to have a script working for both Express and Knex projects
 
 // DONE Every Repo
 // "test": "jest --watchAll",
 // "lint": "eslint --ext .js,.jsx,.ts,.tsx .",
 
-// EXPRESS
+// DONE EXPRESS
 // express-server
 // "start": "node index",
 
-// server-side-rendering & pupparazzi
+// DONE server-side-rendering & pupparazzi
 // "start": "node index",
 // "dev": "nodemon index",
 
-// KNEX
+// DONE KNEX
 // knex-todo-cli
 // "knex": "knex",
 
-// todo-full-stack, knex-join-stories, dreamfest, boilerplate-express-api & boilerplate-fullstack
+// DONE todo-full-stack, knex-join-stories, dreamfest, boilerplate-express-api & boilerplate-fullstack
 // "knex": "knex --knexfile ./server/db/knexfile.js",
-
-// React
-// react-paws-for-effect
-// "start": "ts-node server/index.ts",
-// "build": "npm run webpack --mode production",
-
-// "dev": "run-p dev:server dev:client",
-// "dev:client": "npm run webpack -- --watch",
-// "dev:server": "nodemon server/index.ts",
-// "webpack": "webpack --config ./client/webpack.config.js",
-
-// charlottes-web-log
-// "start": "run-p webpack build",
-// "build": "ts-node server/index.ts",
-
-// same as lines 48-51
