fd5e10df38394af262129a38754009a0932fb5f8
Gerard Paapu <gerard@gerardpaapu.com>
2023-01-11 15:56:40 +1300

    update async-redux-stories with thunk types

--- packages/async-redux-stories/client/store.ts
+++ packages/async-redux-stories/client/store.ts
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
+import type { ThunkAction as BaseThunkAction } from 'redux-thunk'
+import type { AnyAction } from 'redux'
 
 import reducers from './reducers'
 
@@ -13,5 +15,9 @@ export default store
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type ThunkAction<T = void> = BaseThunkAction<
+  Promise<T>,
+  RootState,
+  void,
+  AnyAction
+>
