40a09d2f46bb5e273446d0fc45fdc86c17b7953c
Kelly Keating <keating.kel@gmail.com>
2023-01-27 10:50:34 +1300

    fix indenting

--- packages/charlottes-web-log-api/server/db/db.ts
+++ packages/charlottes-web-log-api/server/db/db.ts
@@ -1,11 +1,13 @@
 import connection from './connection'
  
 export function getAllPosts(db = connection) {
-   return
- }
+  return
+}
  
-export function getPost( db = connection) {
- }
+export function getPost(db = connection) {
+  return
+}
+
 // TODO: rather than making a second database call to fetch the newly-created
 // (or newly-updated) record, a more efficient approach would be to reconstruct
 // the record based on the details passed in, plus the id returned from the first
@@ -18,7 +20,6 @@ export function updatePost(db = connection) {
   return 
 }
 
-// TODO: when deleting a post, also delete its comments
 export function deletePost(db = connection) {
   return 
 }
