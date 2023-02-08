daa38c3e8ed5b1ea2a9a11b7a5a03ae67b6cbf17
krissy-legeyt <krissylegeyt@gmail.com>
2023-02-08 16:37:30 +1300

    test: db & routes

--- packages/react-to-web-api-solution/server/db/db.test.ts
+++ packages/react-to-web-api-solution/server/db/db.test.ts
@@ -3,19 +3,11 @@ import config from './knexfile'
 
 const testDb = knex(config.test)
 
-import { getWidgets } from './db'
+import { addWidget, delWidget, getWidgets, updateWidget } from './db'
 
-beforeAll(() => {
-  return testDb.migrate.latest()
-})
-
-beforeEach(() => {
-  return testDb.seed.run()
-})
-
-afterAll(() => {
-  testDb.destroy()
-})
+beforeAll(() => testDb.migrate.latest())
+beforeEach(() => testDb.seed.run())
+afterAll(() => testDb.destroy())
 
 describe('getWidgets', () => {
   it('returns the correct widgets array', () => {
@@ -24,5 +16,51 @@ describe('getWidgets', () => {
       expect(widgets[0]).toHaveProperty('mfg')
       expect(widgets[1].inStock).toBe(8)
     })
+  })
+})
+
+describe('addWidget', () => {
+  it('adds a widget to the database', () => {
+    const newWidget = {
+      name: 'Test Widget',
+      price: 99.99,
+      mfg: 'Test Mfg',
+      inStock: 1,
+      rating: 5,
+    }
+    return addWidget(newWidget, testDb)
+      .then(() => getWidgets(testDb))
+      .then((widgets) => {
+        expect(widgets).toHaveLength(4)
+        expect(widgets[3]).toMatchObject(newWidget)
+      })
+  })
+})
+
+describe('delWidget', () => {
+  it('deletes a widget from the database', () => {
+    return delWidget(1, testDb)
+      .then(() => getWidgets(testDb))
+      .then((widgets) => {
+        expect(widgets).toHaveLength(2)
+        expect(widgets[0].id).not.toBe(1)
+      })
+  })
+})
+
+describe('updateWidget', () => {
+  it('updates a widget in the database', () => {
+    const updatedWidget = {
+      name: 'Updated Widget',
+      price: 99.99,
+      mfg: 'Updated Mfg',
+      inStock: 1,
+      rating: 5,
+    }
+    return updateWidget(1, updatedWidget, testDb)
+      .then(() => getWidgets(testDb))
+      .then((widgets) => {
+        expect(widgets[0]).toMatchObject(updatedWidget)
+      })
   })
 })
