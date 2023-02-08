dcbc22949d139cfb0c96335cb0b7f510a0c0aa15
clementine-smart <cmwsmart@gmail.com>
2023-02-08 16:24:52 +1300

    Add types to db.orders

--- packages/sweet-as-organics-api-rtk/server/db/orders.ts
+++ packages/sweet-as-organics-api-rtk/server/db/orders.ts
@@ -2,6 +2,12 @@ import connection from './connection'
 import { FormattedOrder } from '../../common/Order'
 
 import { formatOrder, formatOrderList } from '../formatter'
+import { CartProduct } from '../../common/Cart'
+
+interface OrderItem {
+  id: number
+  quantity: number
+}
 
 export function listOrders(db = connection): Promise<FormattedOrder[]> {
   return db('orders_products')
@@ -18,9 +24,9 @@ export function listOrders(db = connection): Promise<FormattedOrder[]> {
     .then((orderLines) => formatOrderList(orderLines))
 }
 
-export function addOrder(orderRequest, db = connection) {
+export function addOrder(orderRequest: CartProduct[], db = connection) {
   // remove item names from order (we have the id)
-  const order = orderRequest.map((item) => {
+  const order: OrderItem[] = orderRequest.map((item) => {
     return {
       id: item.id,
       quantity: item.quantity,
@@ -43,7 +49,7 @@ export function addOrder(orderRequest, db = connection) {
     .then(([id]) => addOrderLines(id, order, db))
 }
 
-export function addOrderLines(id, order, db = connection) {
+export function addOrderLines(id: number, order: OrderItem[], db = connection) {
   const orderLines = order.map((item) => {
     return {
       order_id: id,
@@ -56,7 +62,11 @@ export function addOrderLines(id, order, db = connection) {
     .then(() => null)
 }
 
-function editOrderStatus(id, newStatus, db = connection) {
+export function editOrderStatus(
+  id: number,
+  newStatus: string,
+  db = connection
+) {
   return orderExists(id, db)
     .then(() => {
       return db('orders').update({ status: newStatus }).where('id', id)
@@ -64,7 +74,7 @@ function editOrderStatus(id, newStatus, db = connection) {
     .then(() => findOrderById(id, db))
 }
 
-function orderExists(id, db = connection) {
+function orderExists(id: number, db = connection) {
   return db('orders')
     .where('id', id)
     .first()
@@ -73,7 +83,7 @@ function orderExists(id, db = connection) {
     })
 }
 
-function findOrderById(id, db = connection) {
+function findOrderById(id: number, db = connection) {
   return db('orders_products')
     .join('orders', 'orders_products.order_id', 'orders.id')
     .join('products', 'orders_products.product_id', 'products.id')
