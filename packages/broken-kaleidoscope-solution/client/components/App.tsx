5b0e9eb3d3e0b46bd3057984ced769b05ce97d15
krissy-legeyt <krissylegeyt@gmail.com>
2023-02-09 14:16:08 +1300

    update solution to createRoot

--- packages/broken-kaleidoscope-solution/client/components/App.tsx
+++ packages/broken-kaleidoscope-solution/client/components/App.tsx
@@ -1,7 +1,13 @@
 import Pixel from './Pixel'
 
 function App() {
-  return Array.from({ length: 100 }).map((_, i) => <Pixel key={i} />)
+  return (
+    <>
+      {Array.from({ length: 100 }).map((_, i) => (
+        <Pixel key={i} />
+      ))}
+    </>
+  )
 }
 
 export default App
