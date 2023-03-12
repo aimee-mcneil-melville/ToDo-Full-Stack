340325ae8b2c6e333300368d0f8779e88f42ff89
Olivia Clarke-Edwards <short.livi@gmail.com>
2023-03-12 16:17:37 +1300

    check that element is not null before accessing keys

--- tests/3-interfaces/getUser.test.ts
+++ tests/3-interfaces/getUser.test.ts
@@ -1,9 +1,13 @@
-import { getUser } from "../../functions/3-interfaces"
+import { getUser } from '../../functions/3-interfaces'
 
 const users = {
   1: { name: 'John', verified: true, picture: 'https://example.com/john.jpg' },
   2: { name: 'Batman', verified: false },
-  3: { name: 'Minnie Mouse', verified: true, email: 'thebaddestintown@dis.ney' },
+  3: {
+    name: 'Minnie Mouse',
+    verified: true,
+    email: 'thebaddestintown@dis.ney',
+  },
   4: { name: 'Kermit the Frog', verified: true },
 }
 
@@ -14,10 +18,12 @@ describe('getUser', () => {
 
   it('returns the correct user', () => {
     const actual = getUser(3, users)
-    expect(actual.name).toBe('Minnie Mouse')
-    expect(actual.verified).toBe(true)
-    expect(actual.email).toBe('thebaddestintown@dis.ney')
-    expect(actual.picture).toBeUndefined()
+    if (actual !== null) {
+      expect(actual.name).toBe('Minnie Mouse')
+      expect(actual.verified).toBe(true)
+      expect(actual.email).toBe('thebaddestintown@dis.ney')
+      expect(actual.picture).toBeUndefined()
+    }
   })
 
   it('returns null if the user does not exist', () => {
