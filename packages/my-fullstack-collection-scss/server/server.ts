5d6c840430026bb2b3b3600e6b078a0061aebc73
Gerard Paapu <gerard@gerardpaapu.com>
2023-05-01 17:04:49 +1200

    feat: migrate my-fullstack-collection

--- packages/my-fullstack-collection-scss/server/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import path from 'path'
-
-const server = express()
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
-
-export default server
