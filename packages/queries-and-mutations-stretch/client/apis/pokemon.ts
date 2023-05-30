3b8e5d8475a453a23eeb5943441333621d978c1b
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-05-30 15:10:50 +1200

    wip: init queries-and-mutations

--- packages/queries-and-mutations-stretch/client/apis/pokemon.ts
+++ packages/queries-and-mutations-stretch/client/apis/pokemon.ts
@@ -1,10 +1,10 @@
 import request from 'superagent'
-import type { Pokemon } from '../../models/pokemon'
+import type { Pokemon, PartialPokemon } from '../../models/pokemon'
 
 export async function fetchAllPokemon(generation: number) {
   return request
     .get(`/api/v1/pokemon/${generation}`)
-    .then((res) => res.body as Pokemon[])
+    .then((res) => res.body as PartialPokemon[])
 }
 
 export async function fetchPokemonByName(name: string) {
