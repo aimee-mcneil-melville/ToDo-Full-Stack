5a458ac5e9219e4efa4b8cf27a3f3a0e836d8522
Gerard Paapu <gerard@gerardpaapu.com>
2023-07-05 15:15:13 +1200

    feat: converted exercise-arrays

--- packages/exercise-arrays/lib.test.js
+++ packages/exercise-arrays/lib.test.js
@@ -1,2 +1,5 @@
+import { test, expect } from 'vitest'
+
 // extract four core functions from your index.js file into lib.js for addOneToArray, filterEvenNumbers, findFirstOverFour and sumArray
-// write at least one test for each function using jest
+// write at least one test for each function using vitest
+
