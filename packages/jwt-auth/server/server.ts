f017e3b34d4fe1a06bf75e05b025f846891984df
Jen Hurley <j.s.hurley00@gmail.com>
2023-07-28 11:47:04 +1200

    remove comments in jwt-auth

--- packages/jwt-auth/server/server.ts
+++ packages/jwt-auth/server/server.ts
@@ -7,13 +7,9 @@ import fruitRoutes from './routes/fruits.ts'
 const server = express()
 
 server.use(express.json())
-// server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
 
-// server.get('*', (req, res) => {
-//   res.sendFile(join(__dirname, 'public/index.html'))
-// })
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
