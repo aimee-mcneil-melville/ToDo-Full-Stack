056832ef5f50ea09f1e60a7553783229839d7793
Gerard Paapu <gerard@gerardpaapu.com>
2023-08-08 07:31:51 +1200

    feat: convert boilerplate-react-api to esm

--- packages/boilerplate-react-api/client/components/App.tsx
+++ packages/boilerplate-react-api/client/components/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient'
+import { getGreeting } from '../apiClient.ts'
 
 const App = () => {
   const [greeting, setGreeting] = useState('')
