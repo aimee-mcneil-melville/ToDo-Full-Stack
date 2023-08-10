abcf631fe2885baf9e9fc2a58bf25c7b755f17e9
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-11 09:27:00 +1200

    update render for DOM content for charlottes-web-log-api-solution

--- packages/charlottes-web-log-api-solution/client/index.tsx
+++ packages/charlottes-web-log-api-solution/client/index.tsx
@@ -1,13 +1,12 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+  createRoot(document.getElementById('app') as HTMLElement).render(
     <Router>
       <App />
-    </Router>,
-    document.getElementById('root')
+    </Router>
   )
 })
