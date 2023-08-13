896683d5fc9edc48f5e0984e53bf379bc754f3f5
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-14 11:57:41 +1200

    shuffle imports around for tidiness for consuming-external-apis

--- packages/react-to-web-api-solution/server/server.ts
+++ packages/react-to-web-api-solution/server/server.ts
@@ -1,6 +1,12 @@
 import { join } from 'node:path'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
+
 import express from 'express'
 import widgets from './routes/widgets.ts'
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 const server = express()
 server.use(express.json())
