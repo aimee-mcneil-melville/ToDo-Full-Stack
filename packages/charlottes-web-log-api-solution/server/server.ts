6b569e4a52a957d0641a9f1035f33ad5bfccb56a
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-14 11:29:53 +1200

    update server.ts with workaround for __dirname for charlottes-web-log-api

--- packages/charlottes-web-log-api-solution/server/server.ts
+++ packages/charlottes-web-log-api-solution/server/server.ts
@@ -1,5 +1,9 @@
-import * as path from 'node:path'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
 import express from 'express'
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 import posts from './routes/posts.ts'
 import comments from './routes/comments.ts'
@@ -12,9 +16,9 @@ server.use('/api/v1/comments', comments)
 server.use('/api/v1/*', (req, res) => res.sendStatus(404))
 
 if (process.env.NODE_ENV === 'production') {
-  server.use('/assets', express.static(path.resolve(__dirname, '../assets')))
+  server.use('/assets', express.static(Path.resolve(__dirname, '../assets')))
   server.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../index.html'))
+    res.sendFile(Path.resolve(__dirname, '../index.html'))
   })
 }
 
