b2ad8257951e0aa8c6f920c30cf74dd584d83a15
Gerard Paapu <gerard@gerardpaapu.com>
2023-08-16 10:21:42 +1200

    feat: upgrade vitest and related dependencies

--- packages/sweet-as-beers/client/test-setup.tsx
+++ packages/sweet-as-beers/client/test-setup.tsx
@@ -1,8 +1,7 @@
 import { expect, beforeEach } from 'vitest'
 import { render, cleanup } from '@testing-library/react'
-import matchers, {
-  TestingLibraryMatchers,
-} from '@testing-library/jest-dom/matchers'
+import matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 import userEvent from '@testing-library/user-event'
 import App from './components/App'
 import { Provider } from 'react-redux'
@@ -11,13 +10,6 @@ import { initialiseStore } from './store'
 beforeEach(cleanup)
 expect.extend(matchers)
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace jest {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    interface Matchers<R = void, T = {}> extends TestingLibraryMatchers<T, R> {}
-  }
-}
 export default function setupApp() {
   const user = userEvent.setup()
   const store = initialiseStore()
