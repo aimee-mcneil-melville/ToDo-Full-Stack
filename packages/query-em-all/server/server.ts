c960a938f38ff3a380deca4d418bd9f4a22dd165
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-16 11:02:59 +1200

    update __dirname in server.ts

--- packages/query-em-all/server/server.ts
+++ packages/query-em-all/server/server.ts
@@ -1,11 +1,16 @@
 import { join } from 'node:path'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
 import express from 'express'
 
 import pokemonRoutes from './routes/pokemon.ts'
 
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
 const server = express()
 
-server.use(express.static(join(__dirname, 'public')))
+server.use(express.static(Path.join(__dirname, 'public')))
 server.use(express.json())
 
 server.use('/api/v1/pokemon', pokemonRoutes)
