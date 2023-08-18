7053b52d9c6d60e2a008645e57efd36142e80b3d
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-18 12:04:04 +1200

    update importing for knexfile for queries-and-mutations-stretch

--- packages/queries-and-mutations-stretch/server/db/knexfile.ts
+++ packages/queries-and-mutations-stretch/server/db/knexfile.ts
@@ -1,11 +1,14 @@
-import path from 'node:path'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 import type { Knex } from 'knex'
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, './dev.sqlite3'),
+      filename: Path.resolve(__dirname, './dev.sqlite3'),
     },
     useNullAsDefault: true,
   },
