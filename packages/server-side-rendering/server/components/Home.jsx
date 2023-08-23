a1e7a86f71f3d80234c16164ffc0686a9efe8b03
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-24 07:53:03 +1200

    migrate to jsx

---
+++ packages/server-side-rendering/server/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <p>
+      Hello <code>jsx</code>
+    </p>
+  )
+}
+
+export default Home
--- packages/server-side-rendering/server/index.js
+++ packages/server-side-rendering/server/index.js
@@ -1,6 +1,6 @@
 import server from './server.jsx'
 
-const port = 4000
+const port = 3000
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
