da5aa7bb8b5355905597913d515c7a62a10e0a83
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-24 08:21:42 +1200

    remove handlebars

--- packages/pupparazzi-solution/server/server.js
+++ packages/pupparazzi-solution/server/server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import handlebars from 'express-handlebars'
 import * as Path from 'node:path'
 
 import routes from './routes.js'
@@ -10,11 +9,6 @@ const server = express()
 const publicFolder = Path.resolve('public')
 server.use(express.static(publicFolder))
 server.use(express.urlencoded({ extended: false }))
-
-// Handlebars configuration
-server.engine('hbs', handlebars.engine({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
-server.set('views', Path.resolve('server/views'))
 
 // Routes
 server.use('/', routes)
