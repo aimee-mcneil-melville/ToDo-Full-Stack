2214d33999cd93d3c1bdd6b1411a0323759c900d
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-24 16:29:42 +1200

    delete challenge

--- packages/server-side-rendering-jsx/server/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <p>
-      Hello <code>jsx</code>
-    </p>
-  )
-}
-
-export default Home
--- packages/server-side-rendering-jsx/server/index.js
+++
@@ -1,8 +0,0 @@
-import server from './server.jsx'
-
-const port = 3000
-
-server.listen(port, function () {
-  // eslint-disable-next-line no-console
-  console.log('Server is listening on http://localhost:' + port)
-})
