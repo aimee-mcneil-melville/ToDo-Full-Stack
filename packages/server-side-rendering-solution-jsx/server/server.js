0223b3327efba99429198b470e53f1802189a12c
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-24 16:12:29 +1200

    use es6 import

--- packages/server-side-rendering-solution-jsx/server/server.js
+++ packages/server-side-rendering-solution-jsx/server/server.js
@@ -1,16 +1,14 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const art = require('./data/art.json')
+import express from 'express'
 
 const server = express()
-module.exports = server
+export default server
 
 // Middleware
 server.engine(
   'hbs',
   hbs.engine({
     extname: 'hbs',
-  })
+  }),
 )
 server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
