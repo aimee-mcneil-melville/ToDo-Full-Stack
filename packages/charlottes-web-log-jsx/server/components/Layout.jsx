edcbd946203ac1c39c5f9f92473202fb3cf660ae
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-24 16:59:22 +1200

    repurpose charlottes-web-log for jsx

--- packages/charlottes-web-log-jsx/server/components/Layout.jsx
+++ packages/charlottes-web-log-jsx/server/components/Layout.jsx
@@ -1,5 +1,18 @@
-function Laytout() {
-  return <h1>The future home of Charlotte&apos;s web log!</h1>
+function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/main.css" rel="stylesheet" />
+        <link rel="icon" href="data:," />
+        <title>Charlottes Web Log</title>
+      </head>
+
+      <body>{children}</body>
+    </html>
+  )
 }
 
-export default Laytout
+export default Layout
