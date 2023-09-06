34fc078a53e1e480dafe579f696fee6d74672013
Gerard Paapu <gerard@gerardpaapu.com>
2023-09-06 17:29:04 +1200

    feat: added a test

--- packages/worldwide-routing-solution/client/index.tsx
+++ packages/worldwide-routing-solution/client/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import { routes } from './routes.tsx'
 
+const router = createBrowserRouter(routes)
+
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <RouterProvider router={routes} />
+    <RouterProvider router={router} />
   )
 })
