9b1ee7ceb80f07c9cccdfbe4930cbd58d4cd5724
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-09-29 08:34:21 +1300

    migrate to es

--- packages/dreamfest-solution-jsx/server/db/migrations/20220423010742_locations.js
+++ packages/dreamfest-solution-jsx/server/db/migrations/20220423010742_locations.js
@@ -1,11 +1,11 @@
-export async function up (knex) {
-  await knex.schema.createTable('locations', (table) => {
+export function up(knex) {
+  return knex.schema.createTable('locations', (table) => {
     table.increments('id').primary()
     table.string('name')
     table.string('description')
   })
 }
 
-export async function down (knex) {
-  await knex.schema.dropTable('locations')
+export function down(knex) {
+  return knex.schema.dropTable('locations')
 }
--- packages/dreamfest-solution-jsx/server/db/migrations/20220423010746_events.js
+++ packages/dreamfest-solution-jsx/server/db/migrations/20220423010746_events.js
@@ -1,5 +1,5 @@
-export async function up (knex) {
-  await knex.schema.createTable('events', (table) => {
+export function up(knex) {
+  return knex.schema.createTable('events', (table) => {
     table.increments('id').primary()
     table.integer('location_id').references('locations.id')
     table.string('day')
@@ -9,6 +9,6 @@ export async function up (knex) {
   })
 }
 
-export async function down (knex) {
-  await knex.schema.dropTable('events')
+export function down(knex) {
+  return knex.schema.dropTable('events')
 }
