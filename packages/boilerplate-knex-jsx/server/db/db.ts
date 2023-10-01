8ba0247bb8cae91028cdec2bd86a2159a17a79ce
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-10-02 07:26:17 +1300

    migrate to jsx

--- packages/boilerplate-knex-jsx/server/db/db.ts
+++ packages/boilerplate-knex-jsx/server/db/db.ts
@@ -5,10 +5,16 @@ const environment = process.env.NODE_ENV || 'development'
 const config = knexfile[environment]
 export const connection = knex(config)
 
+export interface User {
+  id: number
+  name: string
+  email: string
+}
+
 export async function getUsers() {
   return connection('users').select()
 }
 
-export async function getUser(id) {
+export async function getUser(id: number) {
   return connection('users').where('id', id).first()
 }
