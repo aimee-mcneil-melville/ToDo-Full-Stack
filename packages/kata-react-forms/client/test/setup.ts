e51c38f30ff12a1506e33954531d4d02486ba146
Michael Reisch <michael.reisch@devacademy.co.nz>
2023-11-01 10:00:43 +1300

    fix test.js matchers import statement

--- packages/kata-react-forms/client/test/setup.ts
+++ packages/kata-react-forms/client/test/setup.ts
@@ -1,6 +1,6 @@
 import { beforeEach, expect } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import matchers from '@testing-library/jest-dom/matchers'
+import * as matchers from '@testing-library/jest-dom/matchers'
 import '@testing-library/jest-dom/vitest'
 
 beforeEach(cleanup)
