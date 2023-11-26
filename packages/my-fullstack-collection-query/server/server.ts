245720ead9034951174260eebedaac988073cd9d
Elise Wei <1482850+EliseWei@users.noreply.github.com>
2023-11-27 09:59:39 +1300

    Update server.ts
    Important to add API routes above the prod routes

--- packages/my-fullstack-collection-query/server/server.ts
+++ packages/my-fullstack-collection-query/server/server.ts
@@ -5,6 +5,8 @@ import express from 'express'
 const server = express()
 server.use(express.json())
 
+// ADD YOUR API ROUTES HERE
+
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
