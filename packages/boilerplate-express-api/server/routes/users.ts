762c34433ec708e4bc9e88bc4a65ba1f41c6edb7
David Kavenga <david.kavenga@gmail.com>
2024-01-17 16:51:46 +1300

    chore: update boilerplate deployment setup

--- packages/boilerplate-express-api/server/routes/users.ts
+++ packages/boilerplate-express-api/server/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import * as db from '../db/db.js'
+import * as db from '../db/db.ts'
 
 const router = Router()
 
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
     const users = await db.getUsers()
     res.json({ users: users })
-  } catch (e) {
+  } catch (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
 })
