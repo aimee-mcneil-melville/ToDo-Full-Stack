530560a6be98a654c3c336d382da69e5539617ac
Gerard Paapu <gerard@gerardpaapu.com>
2024-02-05 18:36:00 +1300

    wip: art-gallery updates

--- packages/art-gallery/server/server.ts
+++ packages/art-gallery/server/server.ts
@@ -2,21 +2,6 @@ import express from 'express'
 import data from './data/art.ts'
 
 const server = express()
-
-server.get('/api/v1/artwork', (req, res) => {
-  res.json(data)
-})
-
-server.get('/api/v1/artwork/:id', (req, res) => {
-  const artwork = data.find((aw) => aw.id === Number(req.params.id))
-  if (!artwork) {
-    res.sendStatus(404)
-    return
-  }
-
-  res.json(artwork)
-})
-
 export default server
 
 // Routes
